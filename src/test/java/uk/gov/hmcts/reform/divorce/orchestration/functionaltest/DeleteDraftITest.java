64c8d6b08f22146486a35fffb893044ca159fedd
niyioyelade <oyeniyi.oyelade>
2018-09-02 18:32:36 +0100

    adding delete draft functional tests

---
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/functionaltest/DeleteDraftITest.java
@@ -0,0 +1,95 @@
+package uk.gov.hmcts.reform.divorce.orchestration.functionaltest;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.reform.divorce.orchestration.OrchestrationServiceApplication;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = OrchestrationServiceApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@PropertySource(value = "classpath:application.yml")
+@AutoConfigureMockMvc
+public class DeleteDraftITest {
+    private static final String API_URL = "/drafts";
+    private static final String CMS_CONTEXT_PATH = "/casemaintenance/version/1/drafts";
+
+    private static final String USER_TOKEN = "Some JWT Token";
+
+
+
+    @Autowired
+    private MockMvc webClient;
+
+    @ClassRule
+    public static WireMockClassRule cmsServiceServer = new WireMockClassRule(4010);
+
+
+
+
+    @Test
+    public void givenJWTTokenIsNull_whenDeleteDraft_thenReturnBadRequest()
+            throws Exception {
+        webClient.perform(delete(API_URL)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void givenThereIsAConnectionError_whenDeleteDraft_thenReturnBadGateway()
+            throws Exception {
+        final String errorMessage = "some error message";
+
+        stubCmsServerEndpoint(HttpStatus.BAD_GATEWAY, errorMessage);
+
+        webClient.perform(delete(API_URL)
+                .header(AUTHORIZATION, USER_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadGateway())
+                .andExpect(content().string(containsString(errorMessage)));
+    }
+
+
+    @Test
+    public void givenEverythingWorksAsExpected_whenCmsCalled_thenDeleteDraft()
+            throws Exception {
+
+
+        stubCmsServerEndpoint(HttpStatus.OK, "");
+
+        webClient.perform(delete(API_URL)
+                .header(AUTHORIZATION, USER_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(""));
+    }
+
+    private void stubCmsServerEndpoint(HttpStatus status, String body)
+            throws Exception {
+        cmsServiceServer.stubFor(WireMock.delete(CMS_CONTEXT_PATH)
+                .willReturn(aResponse()
+                        .withStatus(status.value())
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_UTF8_VALUE)
+                        .withBody(body)));
+    }
+}
