cef33a1b18ae0f78d82e95b3af2f323b5ad35f37
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-09-03 09:42:57 +0100

    fix ccd callback

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/CaseDataFormatter.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/CaseDataFormatter.java
@@ -8,7 +8,6 @@ import uk.gov.hmcts.reform.divorce.orchestration.domain.model.documentgeneration
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.documentgeneration.GeneratedDocumentInfo;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
-import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
 
 import java.util.Map;
 
@@ -27,14 +26,11 @@ public class CaseDataFormatter implements Task<Map<String, Object>> {
     @Override
     public Map<String, Object> execute(TaskContext context,
                                        Map<String, Object> caseData,
-                                       Object... params) throws TaskException {
+                                       Object... params) {
         GeneratedDocumentInfo miniPetition
-                = (GeneratedDocumentInfo) caseData.get(MINI_PETITION_TEMPLATE_NAME);
+                = (GeneratedDocumentInfo) caseData.remove(MINI_PETITION_TEMPLATE_NAME);
         GeneratedDocumentInfo respondentInvitation
-                = (GeneratedDocumentInfo) caseData.get(RESPONDENT_INVITATION_TEMPLATE_NAME);
-
-        caseData.remove(MINI_PETITION_TEMPLATE_NAME);
-        caseData.remove(RESPONDENT_INVITATION_TEMPLATE_NAME);
+                = (GeneratedDocumentInfo) caseData.remove(RESPONDENT_INVITATION_TEMPLATE_NAME);
 
         return caseFormatterClient.addDocuments(
                 DocumentUpdateRequest.builder()
