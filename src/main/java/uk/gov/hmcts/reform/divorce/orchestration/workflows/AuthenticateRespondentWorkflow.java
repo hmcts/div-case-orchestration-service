a2a581d1e54767139d4f391088e8bb5ddc287f43
tony.chow <tony.chow@hmcts.net>
2018-09-03 17:12:25 +0100

    Rebased to latest master

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/AuthenticateRespondentWorkflow.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/AuthenticateRespondentWorkflow.java
@@ -1,11 +1,14 @@
 package uk.gov.hmcts.reform.divorce.orchestration.workflows;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.DefaultWorkflow;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.WorkflowException;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.AuthenticateRespondent;
+
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AUTH_TOKEN_JSON_KEY;
 
 @Component
 public class AuthenticateRespondentWorkflow extends DefaultWorkflow<Boolean> {
@@ -19,6 +22,8 @@ public class AuthenticateRespondentWorkflow extends DefaultWorkflow<Boolean> {
     public Boolean run(String authToken) throws WorkflowException {
         return this.execute(new Task[] {
             authenticateRespondent,
-        }, null, authToken);
+        }, null,
+            new ImmutablePair<>(AUTH_TOKEN_JSON_KEY, authToken)
+        );
     }
 }
