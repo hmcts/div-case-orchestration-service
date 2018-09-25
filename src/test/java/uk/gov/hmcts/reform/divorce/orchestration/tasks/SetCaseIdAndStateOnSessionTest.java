4c5722e3f30c941bc32f28c02a4e5f078499ce6b
tony.chow <tony.chow@hmcts.net>
2018-09-25 22:54:33 +0100

    Return caseId and state on retrieve draft if exists

---
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/SetCaseIdAndStateOnSessionTest.java
@@ -0,0 +1,51 @@
+package uk.gov.hmcts.reform.divorce.orchestration.tasks;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.courts.CourtEnum;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.DefaultTaskContext;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+import static org.junit.Assert.assertEquals;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_STATE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_ID_JSON_KEY;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_STATE_JSON_KEY;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CREATED_DATE_JSON_KEY;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DIVORCE_CENTRE_SITEID_JSON_KEY;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DIVORCE_UNIT_JSON_KEY;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SetCaseIdAndStateOnSessionTest {
+
+    @InjectMocks
+    SetCaseIdAndStateOnSession setCaseIdAndStateOnSession;
+
+    private Map<String, Object> testData;
+    private TaskContext context;
+
+    @Before
+    public void setup() {
+        testData = new HashMap<>();
+        context = new DefaultTaskContext();
+        context.setTransientObject(CASE_ID_JSON_KEY, TEST_CASE_ID);
+        context.setTransientObject(CASE_STATE_JSON_KEY, TEST_STATE);
+    }
+
+    @Test
+    public void executeShouldSetCaseIdAndStateOnExecute() {
+        Map<String, Object> resultData = new HashMap<>();
+        resultData.put(CASE_ID_JSON_KEY, TEST_CASE_ID);
+        resultData.put(CASE_STATE_JSON_KEY, TEST_STATE);
+
+        assertEquals(resultData, setCaseIdAndStateOnSession.execute(context, testData));
+    }
+}
