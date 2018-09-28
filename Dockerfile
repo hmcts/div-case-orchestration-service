hmcts/div-case-maintenance-service

enable aks on build pipeline

Dockerfile
@@ -1,11 +1,15 @@
-FROM openjdk:8-jre-alpine
+FROM hmcts/cnp-java-base:openjdk-jre-8-alpine-1.1
 
-COPY build/install/div-case-maintenance-service /opt/app/
+ENV APP div-case-maintenance-service.jar
+ENV APPLICATION_TOTAL_MEMORY 512M
+ENV APPLICATION_SIZE_ON_DISK_IN_MB 53
+
+COPY build/libs/$APP /opt/app/
 
 WORKDIR /opt/app
 
-HEALTHCHECK --interval=100s --timeout=100s --retries=10 CMD http_proxy="" wget -q http://localhost:4009/health || exit 1
+HEALTHCHECK --interval=100s --timeout=100s --retries=10 CMD http_proxy="" wget -q http://localhost:4010/health || exit 1
 
-EXPOSE 4009
+EXPOSE 4010
 
-ENTRYPOINT ["/opt/app/bin/div-case-maintenance-service"]
+CMD ["sh", "-c", "java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap ${JAVA_OPTS} -jar /opt/app/$APP"

hmcts/div-case-orchestration-service

Dockerfile
@@ -1,11 +1,15 @@
-FROM openjdk:8-jre-alpine
+FROM hmcts/cnp-java-base:openjdk-jre-8-alpine-1.1
 
-COPY build/install/div-case-orchestration-service /opt/app/
+ENV APP div-case-orchestration-service.jar
+ENV APPLICATION_TOTAL_MEMORY 512M
+ENV APPLICATION_SIZE_ON_DISK_IN_MB 53
+
+COPY build/libs/$APP /opt/app/
 
 WORKDIR /opt/app
 
-HEALTHCHECK --interval=100s --timeout=100s --retries=10 CMD http_proxy="" wget -q http://localhost:4007/status/health || exit 1
+HEALTHCHECK --interval=100s --timeout=100s --retries=10 CMD http_proxy="" wget -q http://localhost:4012/health || exit 1
 
-EXPOSE 4007
+EXPOSE 4012
 
-ENTRYPOINT ["/opt/app/bin/div-case-orchestration-service"]
+CMD ["sh", "-c", "java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap ${JAVA_OPTS} -jar /opt/app/$APP"