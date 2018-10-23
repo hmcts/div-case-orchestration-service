0663279b52e1b94ee1a0901e0c4d2eef2f3bf745
Ganeshraja Ramu <ganeshraja.ramu@hmcts.net>
2018-10-23 16:35:30 +0100

    updated send letter to http addresss to fix failing build

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,7 +12,7 @@ locals {
   service_auth_provider_baseurl     = "http://rpe-service-auth-provider-${local.local_env}.service.core-compute-${local.local_env}.internal"
   petitioner_fe_baseurl             = "https://div-pfe-${local.local_env}.service.core-compute-${local.local_env}.internal"
   feature_toggle_baseurl            = "http://rpe-feature-toggle-api-${local.local_env}.service.core-compute-${local.local_env}.internal"
-  send_letter_service_baseurl      = "https://rpe-send-letter-service-${local.local_env}.service.core-compute-${local.local_env}.internal"
+  send_letter_service_baseurl       = "http://rpe-send-letter-service-${local.local_env}.service.core-compute-${local.local_env}.internal"
   previewVaultName = "${var.raw_product}-aat"
   nonPreviewVaultName = "${var.raw_product}-${var.env}"
   vaultName = "${(var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName}"
