a0e915913dc79ef3d569bac8245769e29a6b46c9
J. Victor Ferreira <victorf@kainos.com>
2018-11-21 11:41:33 +0000

    Updated the petitioner submission notification e-mail to cater for CTSC scenario

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/SendPetitionerSubmissionNotificationEmail.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/SendPetitionerSubmissionNotificationEmail.java
@@ -3,13 +3,13 @@ package uk.gov.hmcts.reform.divorce.orchestration.tasks;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.divorce.orchestration.domain.model.courts.CourtEnum;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.courts.Court;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
 import uk.gov.hmcts.reform.divorce.orchestration.service.EmailService;
 
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_ID_JSON_KEY;
@@ -24,13 +24,16 @@ public class SendPetitionerSubmissionNotificationEmail implements Task<Map<Strin
 
     private final EmailService emailService;
 
+    private final TaskCommons taskCommons;
+
     @Autowired
-    public SendPetitionerSubmissionNotificationEmail(EmailService emailService) {
+    public SendPetitionerSubmissionNotificationEmail(EmailService emailService, TaskCommons taskCommons) {
         this.emailService = emailService;
+        this.taskCommons = taskCommons;
     }
 
     @Override
-    public Map<String, Object> execute(TaskContext context, Map<String, Object> caseData) {
+    public Map<String, Object> execute(TaskContext context, Map<String, Object> caseData) throws TaskException {
 
         String petitionerEmail = (String) caseData.get(D_8_PETITIONER_EMAIL);
 
@@ -40,9 +43,11 @@ public class SendPetitionerSubmissionNotificationEmail implements Task<Map<Strin
             templateVars.put("email address", petitionerEmail);
             templateVars.put("first name", (String) caseData.get(D_8_PETITIONER_FIRST_NAME));
             templateVars.put("last name", (String) caseData.get(D_8_PETITIONER_LAST_NAME));
-            templateVars.put("RDC name", CourtEnum.valueOf(
-                caseData.get(DIVORCE_UNIT_JSON_KEY).toString().toUpperCase(Locale.UK)).getDisplayName()
-            );
+
+            String divorceUnitKey = caseData.get(DIVORCE_UNIT_JSON_KEY).toString();
+            Court court = taskCommons.getCourt(divorceUnitKey);
+            templateVars.put("RDC name", court.getIdentifiableCentreName());
+
             String caseId = (String) context.getTransientObject(CASE_ID_JSON_KEY);
             templateVars.put("CCD reference", formatCaseIdToReferenceNumber(caseId));
 
