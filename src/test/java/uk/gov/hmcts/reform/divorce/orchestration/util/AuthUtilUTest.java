22f8a5607ef5bf5b76262a7eac2ab62dac08aeec
Ganeshraja Ramu <ganeshraja.ramu@hmcts.net>
2018-12-05 22:57:01 +0000

    code coverage fixes

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/util/AuthUtilUTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/util/AuthUtilUTest.java
@@ -1,14 +1,60 @@
 package uk.gov.hmcts.reform.divorce.orchestration.util;
 
+import org.junit.Before;
 import org.junit.Test;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Modifier;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.verification.VerificationMode;
+import org.springframework.test.util.ReflectionTestUtils;
+import uk.gov.hmcts.reform.divorce.orchestration.client.IdamClient;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.idam.AuthenticateUserResponse;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.idam.TokenExchangeResponse;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AuthUtilUTest {
+
+    @Mock
+    IdamClient idamClient;
+
+    @InjectMocks
+    private AuthUtil authUtil;
+
+    @Before
+    public void setup() {
+        setField(authUtil, "citizenUserName", "citizenUsername");
+        setField(authUtil, "citizenPassword", "password");
+        setField(authUtil, "authRedirectUrl", "redirectUrl");
+        setField(authUtil, "authClientId", "authClientId");
+        setField(authUtil, "authClientSecret", "authClientSecret");
+    }
+
+
+    @Test
+    public void testGetCitizenToken() {
+        AuthenticateUserResponse authenticateResponse = AuthenticateUserResponse.builder().build();
+        authenticateResponse.setCode("mycode");
+
+        when(idamClient.authenticateUser(any(), any(), any(), any()))
+            .thenReturn(authenticateResponse);
+
+        TokenExchangeResponse tokenExchangeResponse = spy(TokenExchangeResponse.builder().build());
+        when(idamClient.exchangeCode(eq("mycode"), anyString(), anyString(), anyString(), anyString()))
+            .thenReturn(tokenExchangeResponse);
+        String token = authUtil.getCitizenToken();
+        assertTrue(token.startsWith("Bearer"));
+    }
 
 
     @Test
