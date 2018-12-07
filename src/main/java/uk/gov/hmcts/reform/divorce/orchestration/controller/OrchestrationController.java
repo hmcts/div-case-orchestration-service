74405b58305657deb10cc660baf7ac9666a63c18
Ganeshraja Ramu <ganeshraja.ramu@hmcts.net>
2018-12-07 19:47:59 +0000

    checkstyle fixes

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/controller/OrchestrationController.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/controller/OrchestrationController.java
@@ -42,6 +42,10 @@ import javax.ws.rs.core.MediaType;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.core.MediaType;
+
 
 import static java.util.Collections.singletonList;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.BULK_PRINT_ERROR_KEY;
