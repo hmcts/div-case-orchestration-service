7b6129227692439902d1cef8127ee63fd66b8191
Hosain <hosain.alahmad@hmcts.net>
2018-12-12 11:07:44 +0000

    DIV-3710 - added UnusedImports checkstyle and removed unused imports
    As it's checked in Sonar already so we should fail earlier

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/SubmitAosCase.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/SubmitAosCase.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.divorce.orchestration.tasks;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.orchestration.client.CaseMaintenanceClient;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CaseDetails;
