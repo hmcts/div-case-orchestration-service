95ff4f29035a6018c234006bccadd46bdd3fb14d
tony.chow <tony.chow@hmcts.net>
2019-01-11 12:26:00 +0000

    Add integration test for PaymentUpdate

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/callback/PaymentUpdateCallbackTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/callback/PaymentUpdateCallbackTest.java
@@ -3,13 +3,18 @@ package uk.gov.hmcts.reform.divorce.callback;
 import io.restassured.response.Response;
 import org.apache.http.entity.ContentType;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import uk.gov.hmcts.reform.divorce.context.IntegrationTest;
+import uk.gov.hmcts.reform.divorce.model.UserDetails;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.payment.PaymentUpdate;
+import uk.gov.hmcts.reform.divorce.support.CcdClientSupport;
 import uk.gov.hmcts.reform.divorce.util.ResourceLoader;
 import uk.gov.hmcts.reform.divorce.util.RestUtil;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,30 +24,41 @@ import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.Orchestrati
 
 public class PaymentUpdateCallbackTest extends IntegrationTest {
 
-    private static final String DATA_KEY = "data";
-    private static final String PAYLOAD_CONTEXT_PATH = "fixtures/solicitor/";
+    private static final String PAYLOAD_CONTEXT_PATH = "fixtures/callback/";
+    private static final String SUBMIT_PAYLOAD_CONTEXT_PATH = "fixtures/maintenance/submit/";
 
-    @Value("${case.orchestration.solicitor.process-pba-payment.context-path}")
+    @Value("${case.orchestration.payment-update.context-path}")
     private String contextPath;
 
+    @Autowired
+    private CcdClientSupport ccdClientSupport;
+
     @Test
-    public void givenCreateEvent_whenProcessPbaPayment_thenReturnDataWithNoErrors() throws Exception {
+    public void givenValidPaymentRequest_whenPaymentUpdate_thenReturnStatusOkWithNoErrors() throws Exception {
         final Map<String, Object> headers = new HashMap<>();
         headers.put(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.toString());
-        headers.put(HttpHeaders.AUTHORIZATION, createCaseWorkerUser().getAuthToken());
+        headers.put(HttpHeaders.AUTHORIZATION, createCitizenUser().getAuthToken());
+
+        UserDetails citizenUser = createCitizenUser();
+
+        String caseId = ccdClientSupport.submitCase(
+                ResourceLoader.loadJsonToObject(SUBMIT_PAYLOAD_CONTEXT_PATH + "submit-case-data.json", Map.class),
+                citizenUser
+        ).getId().toString();
+
+        PaymentUpdate paymentUpdate = ResourceLoader.loadJsonToObject(
+                PAYLOAD_CONTEXT_PATH + "paymentUpdate.json", PaymentUpdate.class
+        );
+
+        paymentUpdate.setCcdCaseNumber(caseId);
 
         Response response = RestUtil.postToRestService(
                 serverUrl + contextPath,
-                headers,
-                ResourceLoader.loadJson(PAYLOAD_CONTEXT_PATH + "solicitor-request-data.json")
+                Collections.singletonMap(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.toString()),
+                ResourceLoader.objectToJson(paymentUpdate)
         );
 
         assertEquals(HttpStatus.OK.value(), response.getStatusCode());
-
-        Map<String, Object> responseData = response.getBody().path(DATA_KEY);
-
-        // There will be an error if PBA payment is unsuccessful
-        assertNotNull(responseData.get(SOLICITOR_FEE_ACCOUNT_NUMBER_JSON_KEY));
     }
 
 }
