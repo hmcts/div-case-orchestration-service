10a53559b8ec59072dbfc40d0831ee003a793e77
Qiang Zhou <qiang.zhou@hmcts.net>
2019-01-14 17:25:58 +0000

    Added healthcheck timeout

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/management/monitoring/health/PaymentServiceHealthCheck.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/management/monitoring/health/PaymentServiceHealthCheck.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.divorce.orchestration.management.monitoring.health;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -8,8 +9,9 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class PaymentServiceHealthCheck extends WebServiceHealthCheck {
     @Autowired
-    public PaymentServiceHealthCheck(HttpEntityFactory httpEntityFactory, RestTemplate restTemplate,
-                                             @Value("${payment.service.api.baseurl}/health") String uri) {
+    public PaymentServiceHealthCheck(HttpEntityFactory httpEntityFactory,
+                                     @Qualifier("healthCheckRestTemplate") RestTemplate restTemplate,
+                                     @Value("${payment.service.api.baseurl}/health") String uri) {
         super(httpEntityFactory, restTemplate, uri);
     }
 }
