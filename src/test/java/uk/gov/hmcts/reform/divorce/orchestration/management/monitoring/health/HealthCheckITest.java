3bd02b48aa244049d8858a9e64e5e1a8f19234d7
Qiang Zhou <qiang.zhou@hmcts.net>
2019-01-15 09:00:45 +0000

    Fixed test

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/management/monitoring/health/HealthCheckITest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/management/monitoring/health/HealthCheckITest.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -74,6 +75,7 @@ public class HealthCheckITest {
     private String sendLetterHealthUrl;
 
     @Autowired
+    @Qualifier("healthCheckRestTemplate")
     private RestTemplate restTemplate;
 
     private String healthUrl;
