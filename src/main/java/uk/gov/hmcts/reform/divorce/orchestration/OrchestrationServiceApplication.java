95187e8716609e9f66aeaf0d6ca02bc281a42c3a
Qiang Zhou <qiang.zhou@hmcts.net>
2019-01-17 21:52:06 +0000

    Fix context

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/OrchestrationServiceApplication.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/OrchestrationServiceApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import uk.gov.hmcts.reform.authorisation.healthcheck.ServiceAuthHealthIndicator;
 import uk.gov.hmcts.reform.sendletter.SendLetterAutoConfiguration;
+import uk.gov.hmcts.reform.sendletter.healthcheck.SendLetterHealthIndicator;
 
-@SpringBootApplication(scanBasePackages = "uk.gov.hmcts.reform",
-    exclude = {ServiceAuthHealthIndicator.class, SendLetterAutoConfiguration.class})
+@SpringBootApplication(exclude = {ServiceAuthHealthIndicator.class, SendLetterAutoConfiguration.class})
 @EnableFeignClients(basePackages = {"uk.gov.hmcts.reform.divorce", "uk.gov.hmcts.reform.sendletter"})
 public class OrchestrationServiceApplication {
     public static void main(String[] args) {
