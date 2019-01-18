856594c6c9645ab0c4524b42a88b6983793a6ea0
Qiang Zhou <qiang.zhou@hmcts.net>
2019-01-18 00:13:28 +0000

    Fix health mocks

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/management/monitoring/health/HealthCheckITest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/management/monitoring/health/HealthCheckITest.java
@@ -12,7 +12,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpMethod;
@@ -23,9 +25,14 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationHealthApi;
+import uk.gov.hmcts.reform.divorce.orchestration.client.EmailClient;
+import uk.gov.hmcts.reform.sendletter.healthcheck.InternalHealth;
+import uk.gov.hmcts.reform.sendletter.healthcheck.SendLetterHealthApi;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.ExpectedCount.once;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
@@ -73,6 +80,12 @@ public class HealthCheckITest {
     @Value("${send-letter.url}")
     private String sendLetterHealthUrl;
 
+    @MockBean
+    private SendLetterHealthApi sendLetterHealthApi;
+
+    @MockBean
+    private ServiceAuthorisationHealthApi serviceAuthorisationHealthApi;
+
     @Autowired
     private RestTemplate restTemplate;
 
@@ -112,6 +125,9 @@ public class HealthCheckITest {
         mockEndpointAndResponse(paymentServiceHealthUrl, true);
         mockEndpointAndResponse(sendLetterHealthUrl, true);
         mockEndpointAndResponse(serviceAuthHealthUrl, true);
+        when(sendLetterHealthApi.health()).thenReturn(new InternalHealth(Status.UP));
+        when(serviceAuthorisationHealthApi.health())
+            .thenReturn(new uk.gov.hmcts.reform.authorisation.healthcheck.InternalHealth(Status.UP));
 
         HttpResponse response = getHealth();
         String body = EntityUtils.toString(response.getEntity());
