f6a4d63e17239f070b9b5ea18e9c11d0d0ae9728
J. Victor Ferreira <victorf@kainos.com>
2019-01-21 14:36:13 +0000

    Testing with full list of fields

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/maintenance/CoRespondentCaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/maintenance/CoRespondentCaseTest.java
@@ -1,18 +1,20 @@
 package uk.gov.hmcts.reform.divorce.maintenance;
 
 import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
-import org.springframework.http.HttpStatus;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.divorce.model.UserDetails;
 import uk.gov.hmcts.reform.divorce.support.RetrieveAosCaseSupport;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import static org.springframework.http.HttpStatus.OK;
 import static uk.gov.hmcts.reform.divorce.context.ServiceContextConfiguration.OBJECT_MAPPER;
 import static uk.gov.hmcts.reform.divorce.util.ResourceLoader.loadJson;
 
+@Slf4j
 public class CoRespondentCaseTest extends RetrieveAosCaseSupport {
 
     private static final String PAYLOAD_CONTEXT_PATH = "fixtures/maintenance/co-respondent/";
@@ -23,15 +25,16 @@ public class CoRespondentCaseTest extends RetrieveAosCaseSupport {
     public void givenNoConsentAndNoDefend_whenSubmitAos_thenProceedAsExpected() throws Exception {
         final UserDetails userDetails = createCitizenUser();
         final CaseDetails caseDetails = submitCase("submit-complete-case.json", userDetails);
+        log.info("Case " + caseDetails.getId() + " created.");
         updateCaseForCitizen(String.valueOf(caseDetails.getId()), null, TEST_AOS_STARTED_EVENT_ID, userDetails);
 
         String requestBodyJson = loadJson(PAYLOAD_CONTEXT_PATH + "co-respondent-answers.json");
         updateCaseWithCoRespondentAnswers(userDetails, caseDetails, requestBodyJson);
 
         Response cosResponse = retrieveAosCase(userDetails.getAuthToken());
-        assertEquals(HttpStatus.OK.value(), cosResponse.getStatusCode());
-        assertEquals(String.valueOf(caseDetails.getId()), cosResponse.path(CASE_ID_KEY));
-        assertEquals("DNAwaiting", cosResponse.path(STATE_KEY));
+        assertThat(cosResponse.getStatusCode(), is(OK.value()));
+        assertThat(cosResponse.path(CASE_ID_KEY), is(String.valueOf(caseDetails.getId())));
+        assertThat(cosResponse.path(STATE_KEY), is("DNAwaiting"));
 
         String requestCoRespondentAnswersJson = OBJECT_MAPPER.readTree(requestBodyJson)
                 .get("coRespondentAnswers")
@@ -47,9 +50,9 @@ public class CoRespondentCaseTest extends RetrieveAosCaseSupport {
     private void updateCaseWithCoRespondentAnswers(UserDetails userDetails, CaseDetails caseDetails, String requestBody) {
         Response cosResponse = submitAosCase(userDetails.getAuthToken(), caseDetails.getId(), requestBody);
 
-        assertEquals(OK.value(), cosResponse.getStatusCode());
-        assertEquals(caseDetails.getId(), cosResponse.path("id"));
-        assertEquals("AwaitingDecreeNisi", cosResponse.path(STATE_KEY));
+        assertThat(cosResponse.getStatusCode(), is(OK.value()));
+        assertThat(cosResponse.path("id"), is(caseDetails.getId()));
+        assertThat(cosResponse.path(STATE_KEY), is("AwaitingDecreeNisi"));
     }
 
 }
