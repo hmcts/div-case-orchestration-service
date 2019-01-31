9a720c7745201af91d4642b3898d0b2c996593ad
J. Victor Ferreira <victorf@kainos.com>
2019-01-31 10:35:06 +0000

    Address code review comments

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/courtallocation/CourtAllocationConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/courtallocation/CourtAllocationConfiguration.java
@@ -1,16 +1,14 @@
 package uk.gov.hmcts.reform.divorce.orchestration.courtallocation;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
-@EqualsAndHashCode
 public class CourtAllocationConfiguration {
 
-    private List<CourtWeight> courtsWeightedDistribution = new ArrayList<>();
-    private List<CourtAllocationPerReason> courtsForSpecificReasons = new ArrayList<>();
+    private Set<CourtWeight> courtsWeightedDistribution = new HashSet<>();
+    private Set<CourtAllocationPerReason> courtsForSpecificReasons = new HashSet<>();
 
 }
