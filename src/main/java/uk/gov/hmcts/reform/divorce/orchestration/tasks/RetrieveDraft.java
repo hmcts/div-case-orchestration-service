f8e535ab656a1976acf0eb25b29b0b42cd1c2b42
Qiang Zhou <qiang.zhou@hmcts.net>
2019-02-11 09:42:17 +0000

    Added missing attribute in IT

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/RetrieveDraft.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/RetrieveDraft.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.divorce.orchestration.tasks;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -10,6 +9,7 @@ import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
 
 import java.util.Map;
+import java.util.Optional;
 
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AUTH_TOKEN_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_ID_JSON_KEY;
@@ -38,9 +38,10 @@ public class RetrieveDraft implements Task<Map<String, Object>> {
                 .retrievePetition(context.getTransientObject(AUTH_TOKEN_JSON_KEY).toString(), getCheckCcd(context));
 
         if (cmsContent != null && cmsContent.getCaseData() != null && !cmsContent.getCaseData().isEmpty()) {
-            boolean isDraft = StringUtils.isEmpty(cmsContent.getCaseId());
             caseData = cmsContent.getCaseData();
-            caseData.put(IS_DRAFT_KEY, isDraft);
+            Boolean isDraft = Optional.ofNullable(caseData.get(IS_DRAFT_KEY))
+                                .map(Boolean.class::cast)
+                                .orElse(false);
 
             if (!isDraft) {
                 context.setTransientObject(CASE_ID_JSON_KEY, cmsContent.getCaseId());
