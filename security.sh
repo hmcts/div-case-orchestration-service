5911bf330eebd8527af3fee3c0699121a8769912
Adeleke Osinloye <michael@Adelekes-MacBook-Pro.local>
2019-02-11 11:36:16 +0000

    set up divorce cos security scan

--- security.sh
+++ security.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
-zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001
+zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -u ${SecurityRules} -P 1001
 cat zap.out
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
 cp /zap/api-report.html functional-output/
