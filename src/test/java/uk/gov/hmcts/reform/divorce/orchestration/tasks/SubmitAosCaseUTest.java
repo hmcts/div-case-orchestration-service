74731628878da12a3cf40f2b8779944a96cab90b
John O'Neill <j.oneill@kainos.com>
2019-02-12 14:40:36 +0000

    DIV-4120: Fixed failing functional tests

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/SubmitAosCaseUTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/SubmitAosCaseUTest.java
@@ -19,18 +19,14 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.AUTH_TOKEN;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_CASE_ID;
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.ADULTERY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AUTH_TOKEN_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AWAITING_ANSWER_AOS_EVENT_ID;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AWAITING_DN_AOS_EVENT_ID;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_ID_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CCD_CASE_DATA_FIELD;
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.COMPLETED_AOS_EVENT_ID;
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D_8_REASON_FOR_DIVORCE;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.NO_VALUE;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.RESP_ADMIT_OR_CONSENT_TO_FACT;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.RESP_WILL_DEFEND_DIVORCE;
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.SEPARATION2YRS;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.YES_VALUE;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -38,7 +34,6 @@ public class SubmitAosCaseUTest {
     private static final Map<String, Object> EXPECTED_OUTPUT = Collections.emptyMap();
     private static final Map<String, Object> CASE_UPDATE_RESPONSE = new HashMap<>();
     private static final TaskContext TASK_CONTEXT = new DefaultTaskContext();
-    private static Map<String, Object> DIVORCE_SESSION = new HashMap<>();
 
     @Mock
     private CaseMaintenanceClient caseMaintenanceClient;
@@ -54,70 +49,54 @@ public class SubmitAosCaseUTest {
     }
 
     @Test
-    public void givenDefendAndConsent_whenExecute_thenReturnAosSubmittedDefended() {
-        DIVORCE_SESSION = getCaseData(YES_VALUE, true);
-        TASK_CONTEXT.setTransientObject(D_8_REASON_FOR_DIVORCE, SEPARATION2YRS);
+    public void givenConsentAndDefend_whenExecute_thenProceedAsExpected() {
+        final Map<String, Object> divorceSession = getCaseData(YES_VALUE, true);
 
-        when(caseMaintenanceClient.updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_ANSWER_AOS_EVENT_ID, DIVORCE_SESSION))
+        when(caseMaintenanceClient.updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_ANSWER_AOS_EVENT_ID, divorceSession))
             .thenReturn(CASE_UPDATE_RESPONSE);
 
-        assertEquals(EXPECTED_OUTPUT, classUnderTest.execute(TASK_CONTEXT, DIVORCE_SESSION));
+        assertEquals(EXPECTED_OUTPUT, classUnderTest.execute(TASK_CONTEXT, divorceSession));
 
         verify(caseMaintenanceClient)
-            .updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_ANSWER_AOS_EVENT_ID, DIVORCE_SESSION);
+            .updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_ANSWER_AOS_EVENT_ID, divorceSession);
     }
 
     @Test
-    public void givenDefendAndNoConsent_whenExecute_thenReturnAosSubmittedDefended() {
-        DIVORCE_SESSION = getCaseData(NO_VALUE, true);
-        TASK_CONTEXT.setTransientObject(D_8_REASON_FOR_DIVORCE, SEPARATION2YRS);
+    public void givenNoConsentAndDefend_whenExecute_thenProceedAsExpected() {
+        final Map<String, Object> divorceSession = getCaseData(NO_VALUE, true);
 
-        when(caseMaintenanceClient.updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_ANSWER_AOS_EVENT_ID, DIVORCE_SESSION))
+        when(caseMaintenanceClient.updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_ANSWER_AOS_EVENT_ID, divorceSession))
             .thenReturn(CASE_UPDATE_RESPONSE);
 
-        assertEquals(EXPECTED_OUTPUT, classUnderTest.execute(TASK_CONTEXT, DIVORCE_SESSION));
+        assertEquals(EXPECTED_OUTPUT, classUnderTest.execute(TASK_CONTEXT, divorceSession));
 
         verify(caseMaintenanceClient)
-            .updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_ANSWER_AOS_EVENT_ID, DIVORCE_SESSION);
+            .updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_ANSWER_AOS_EVENT_ID, divorceSession);
     }
 
     @Test
-    public void givenNoDefendAndNoConsentToAdultery_whenExecute_thenReturnAosCompleted() {
-        DIVORCE_SESSION = getCaseData(NO_VALUE, false);
-        TASK_CONTEXT.setTransientObject(D_8_REASON_FOR_DIVORCE, ADULTERY);
+    public void givenBehaviourNoConsentAndNoDefend_whenExecute_thenProceedAsExpected() {
+        final Map<String, Object> divorceSession = getCaseData(NO_VALUE, false);
 
-        when(caseMaintenanceClient.updateCase(AUTH_TOKEN, TEST_CASE_ID, COMPLETED_AOS_EVENT_ID, DIVORCE_SESSION))
+        when(caseMaintenanceClient.updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_DN_AOS_EVENT_ID, divorceSession))
                 .thenReturn(CASE_UPDATE_RESPONSE);
 
-        assertEquals(EXPECTED_OUTPUT, classUnderTest.execute(TASK_CONTEXT, DIVORCE_SESSION));
+        assertEquals(EXPECTED_OUTPUT, classUnderTest.execute(TASK_CONTEXT, divorceSession));
 
-        verify(caseMaintenanceClient).updateCase(AUTH_TOKEN, TEST_CASE_ID, COMPLETED_AOS_EVENT_ID, DIVORCE_SESSION);
+        // we let the case proceed to awaiting DN for unreasonable behaviour regardless of whether they admit or not
+        verify(caseMaintenanceClient).updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_DN_AOS_EVENT_ID, divorceSession);
     }
 
     @Test
-    public void givenNoDefendAndConsentsToAdultery_whenExecute_thenReturnAosSubmittedUndefended() {
-        DIVORCE_SESSION = getCaseData(YES_VALUE, false);
-        TASK_CONTEXT.setTransientObject(D_8_REASON_FOR_DIVORCE, ADULTERY);
+    public void givenConsentAndNoDefend_whenExecute_thenProceedAsExpected() {
+        final Map<String, Object> divorceSession = getCaseData(YES_VALUE, false);
 
-        when(caseMaintenanceClient.updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_DN_AOS_EVENT_ID, DIVORCE_SESSION))
-                .thenReturn(CASE_UPDATE_RESPONSE);
+        when(caseMaintenanceClient.updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_DN_AOS_EVENT_ID, divorceSession))
+            .thenReturn(CASE_UPDATE_RESPONSE);
 
-        assertEquals(EXPECTED_OUTPUT, classUnderTest.execute(TASK_CONTEXT, DIVORCE_SESSION));
+        assertEquals(EXPECTED_OUTPUT, classUnderTest.execute(TASK_CONTEXT, divorceSession));
 
-        verify(caseMaintenanceClient).updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_DN_AOS_EVENT_ID, DIVORCE_SESSION);
-    }
-
-    @Test
-    public void givenNoDefendAndConsentsWhenNotAdultery_whenExecute_thenReturnAosSubmittedUndefended() {
-        DIVORCE_SESSION = getCaseData(YES_VALUE, false);
-        TASK_CONTEXT.setTransientObject(D_8_REASON_FOR_DIVORCE, SEPARATION2YRS);
-
-        when(caseMaintenanceClient.updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_DN_AOS_EVENT_ID, DIVORCE_SESSION))
-                .thenReturn(CASE_UPDATE_RESPONSE);
-
-        assertEquals(EXPECTED_OUTPUT, classUnderTest.execute(TASK_CONTEXT, DIVORCE_SESSION));
-
-        verify(caseMaintenanceClient).updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_DN_AOS_EVENT_ID, DIVORCE_SESSION);
+        verify(caseMaintenanceClient).updateCase(AUTH_TOKEN, TEST_CASE_ID, AWAITING_DN_AOS_EVENT_ID, divorceSession);
     }
 
     private Map<String, Object> getCaseData(String consent, boolean defended) {
