28c1deb4e26978c046fcceceb217327be668586b
Qiang Zhou <qiang.zhou@hmcts.net>
2019-05-01 09:29:27 +0100

    Set fixed date on test

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/SetCoRespondentAnswerReceivedTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/SetCoRespondentAnswerReceivedTest.java
@@ -1,10 +1,10 @@
 package uk.gov.hmcts.reform.divorce.orchestration.tasks;
 
 import com.google.common.collect.ImmutableMap;
+import org.joda.time.DateTimeUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.divorce.orchestration.util.CcdUtil;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,11 +16,14 @@ import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.Orchestrati
 @Component
 public class SetCoRespondentAnswerReceivedTest {
 
+    private static final String CURRENT_DATE = "2018-01-01";
+
     private SetCoRespondentAnswerReceived classToTest;
 
     @Before
     public void setUp() {
         classToTest = new SetCoRespondentAnswerReceived();
+        DateTimeUtils.setCurrentMillisFixed(new org.joda.time.LocalDate(CURRENT_DATE).toDate().getTime());
     }
 
     @Test
@@ -28,7 +31,7 @@ public class SetCoRespondentAnswerReceivedTest {
         Map<String, Object> caseDataResponse = classToTest.execute(null, new HashMap<>());
 
         Map<String, Object> expectedCaseData = ImmutableMap.of(CO_RESPONDENT_ANSWER_RECEIVED, YES_VALUE,
-                CO_RESPONDENT_ANSWER_RECEIVED_DATE, CcdUtil.getCurrentDate());
+                CO_RESPONDENT_ANSWER_RECEIVED_DATE, CURRENT_DATE);
         assertEquals(expectedCaseData, caseDataResponse);
     }
 }
