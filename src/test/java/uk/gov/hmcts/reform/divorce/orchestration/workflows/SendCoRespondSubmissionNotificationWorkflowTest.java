838b502ab1c9786a347120f33269f004e0ac6996
Chris Iliev <c.iliev@kainos.com>
2019-05-14 12:29:59 +0100

    Checkstyle fixes

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/SendCoRespondSubmissionNotificationWorkflowTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/SendCoRespondSubmissionNotificationWorkflowTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.divorce.orchestration.workflows;
 
 import com.google.common.collect.ImmutableMap;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
