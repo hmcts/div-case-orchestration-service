d8afe982cb0626a8b6c6af60d69ef32406682f05
tony.chow <tony.chow@hmcts.net>
2019-05-16 10:21:10 +0100

    Add Integration Test for CoE Generation

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/callback/GenerateCertificateOfEntitlementTest.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.reform.divorce.callback;
+
+import io.restassured.response.Response;
+import org.apache.http.entity.ContentType;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import uk.gov.hmcts.reform.divorce.context.IntegrationTest;
+import uk.gov.hmcts.reform.divorce.util.ResourceLoader;
+import uk.gov.hmcts.reform.divorce.util.RestUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class GenerateCertificateOfEntitlementTest extends IntegrationTest {
+
+    private static final String DATA_KEY = "data";
+    private static final String PAYLOAD_CONTEXT_PATH = "fixtures/callback/";
+
+    @Value("${case.orchestration.generate-certificate-of-entitlement.context-path}")
+    private String contextPath;
+
+    @Test
+    public void givenCallbackRequest_whenGenerateCertificateOfEntitlement_thenReturnUpdatedData() throws Exception {
+        final Map<String, Object> headers = new HashMap<>();
+        headers.put(HttpHeaders.AUTHORIZATION, createCaseWorkerUser().getAuthToken());
+        headers.put(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.toString());
+
+        Response response = RestUtil.postToRestService(
+                serverUrl + contextPath,
+                headers,
+                ResourceLoader.loadJson(PAYLOAD_CONTEXT_PATH + "basic-ccd-callback-request-no-docs.json")
+        );
+
+        assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+
+        Map<String, Object> responseData = response.getBody().path(DATA_KEY);
+
+        assertNotNull(responseData.get("D8DocumentsGenerated"));
+    }
+}
