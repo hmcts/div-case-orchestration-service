d9b973595fb65fbea83776ebf13b7548bccb3592
J. Victor Ferreira <jvictor.ferreira@jworks.eu>
2019-05-29 12:58:47 +0100

    DIV-4237: Returning court information when case is submitted. (#280)

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/functionaltest/SubmitCaseTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/functionaltest/SubmitCaseTest.java
@@ -4,6 +4,7 @@ import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import com.github.tomakehurst.wiremock.matching.EqualToPattern;
 import com.google.common.collect.ImmutableMap;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,9 +18,12 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.reform.divorce.orchestration.OrchestrationServiceApplication;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.courts.Court;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.validation.ValidationRequest;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.validation.ValidationResponse;
+import uk.gov.hmcts.reform.divorce.orchestration.service.impl.CourtLookupService;
 
 import java.util.Collections;
 import java.util.List;
@@ -31,10 +35,12 @@ import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.matchingJsonPath;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasNoJsonPath;
 import static com.jayway.jsonpath.matchers.JsonPathMatchers.isJson;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
@@ -45,6 +51,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AWAITING_PAYMENT;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_STATE_JSON_KEY;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DIVORCE_UNIT_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.ID;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.SUCCESS_STATUS;
 import static uk.gov.hmcts.reform.divorce.orchestration.testutil.ObjectMapperTestUtil.convertObjectToJsonString;
@@ -74,11 +81,14 @@ public class SubmitCaseTest {
     private static final Map<String, Object> CASE_DATA = Collections.emptyMap();
 
     private static final ValidationRequest validationRequest = ValidationRequest.builder()
-            .data(CASE_DATA)
-            .formId(FORM_ID)
-            .build();
+        .data(CASE_DATA)
+        .formId(FORM_ID)
+        .build();
 
     private static final ValidationResponse validationResponse = ValidationResponse.builder().build();
+
+    @Autowired
+    private CourtLookupService courtLookupService;
 
     @Autowired
     private MockMvc webClient;
@@ -100,62 +110,88 @@ public class SubmitCaseTest {
         stubMaintenanceServerEndpointForSubmit(Collections.singletonMap(ID, TEST_CASE_ID));
         stubMaintenanceServerEndpointForDeleteDraft(HttpStatus.OK);
 
-        webClient.perform(post(API_URL)
-                .header(AUTHORIZATION, AUTH_TOKEN)
-                .content(convertObjectToJsonString(CASE_DATA))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(content().string(allOf(
-                    isJson(),
-                    hasJsonPath("$.caseId", equalTo(TEST_CASE_ID)),
-                    hasJsonPath("$.status", equalTo(SUCCESS_STATUS)),
-                    hasJsonPath("$.allocatedCourt.courtId", is(notNullValue()))
-                )));
+        MvcResult result = webClient.perform(post(API_URL)
+            .header(AUTHORIZATION, AUTH_TOKEN)
+            .content(convertObjectToJsonString(CASE_DATA))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().is2xxSuccessful())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andReturn();
+
+        String responseBody = result.getResponse().getContentAsString();
+        assertThat(responseBody, allOf(
+            isJson(),
+            hasJsonPath("$.caseId", equalTo(TEST_CASE_ID)),
+            hasJsonPath("$.status", equalTo(SUCCESS_STATUS)),
+            hasJsonPath("$.allocatedCourt",
+                hasJsonPath("courtId", is(notNullValue()))
+            )
+        ));
+
+        String allocatedCourtId = JsonPath.read(responseBody, "$.allocatedCourt.courtId");
+        Court allocatedCourt = courtLookupService.getCourtByKey(allocatedCourtId);
+        assertThat(responseBody, hasJsonPath("$.allocatedCourt", allOf(
+            hasJsonPath("courtId", is(allocatedCourtId)),
+            hasJsonPath("serviceCentreName", is(allocatedCourt.getServiceCentreName())),
+            hasJsonPath("divorceCentre", is(allocatedCourt.getDivorceCentreName())),
+            hasJsonPath("divorceCentreAddressName", is(allocatedCourt.getDivorceCentreAddressName())),
+            hasJsonPath("street", is(allocatedCourt.getStreet())),
+            hasJsonPath("courtCity", is(allocatedCourt.getCourtCity())),
+            hasJsonPath("poBox", is(allocatedCourt.getPoBox())),
+            hasJsonPath("postCode", is(allocatedCourt.getPostCode())),
+            hasJsonPath("openingHours", is(allocatedCourt.getOpeningHours())),
+            hasJsonPath("email", is(allocatedCourt.getEmail())),
+            hasJsonPath("phoneNumber", is(allocatedCourt.getPhoneNumber())),
+            hasJsonPath("siteId", is(allocatedCourt.getSiteId())),
+            hasNoJsonPath("formattedAddress")
+        )));
     }
 
     @Test
     public void givenCaseAlreadyExists_whenCaseDataIsSubmitted_thenReturnSuccess() throws Exception {
         Map<String, Object> retrieveCaseResponse = ImmutableMap.<String, Object>builder()
-                .put(ID, TEST_CASE_ID)
-                .put(CASE_STATE_JSON_KEY, AWAITING_PAYMENT)
-                .put("case_data", Collections.singletonMap("D8DivorceUnit", "some-court"))
-                .build();
+            .put(ID, TEST_CASE_ID)
+            .put(CASE_STATE_JSON_KEY, AWAITING_PAYMENT)
+            .put("case_data", Collections.singletonMap(DIVORCE_UNIT_JSON_KEY, "serviceCentre"))
+            .build();
 
         stubMaintenanceServerEndpointForRetrieve(HttpStatus.OK, retrieveCaseResponse);
 
         webClient.perform(post(API_URL)
-                .header(AUTHORIZATION, AUTH_TOKEN)
-                .content(convertObjectToJsonString(CASE_DATA))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(content().string(allOf(
-                        isJson(),
-                        hasJsonPath("$.caseId", equalTo(TEST_CASE_ID)),
-                        hasJsonPath("$.status", equalTo(SUCCESS_STATUS)),
-                        hasJsonPath("$.allocatedCourt.courtId", equalTo("some-court"))
-                )));
+            .header(AUTHORIZATION, AUTH_TOKEN)
+            .content(convertObjectToJsonString(CASE_DATA))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().is2xxSuccessful())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(content().string(allOf(
+                isJson(),
+                hasJsonPath("$.caseId", equalTo(TEST_CASE_ID)),
+                hasJsonPath("$.status", equalTo(SUCCESS_STATUS)),
+                hasJsonPath("$.allocatedCourt", allOf(
+                    hasJsonPath("courtId", equalTo("serviceCentre")),
+                    hasJsonPath("serviceCentreName", equalTo("Courts and Tribunals Service Centre"))
+                ))
+            )));
     }
 
     @Test
     public void givenNoPayload_whenCaseDataIsSubmitted_thenReturnBadRequest() throws Exception {
         webClient.perform(post(API_URL)
-                .header(AUTHORIZATION, AUTH_TOKEN)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
+            .header(AUTHORIZATION, AUTH_TOKEN)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest());
     }
 
     @Test
     public void givenNoAuthToken_whenCaseDataIsSubmitted_thenReturnBadRequest() throws Exception {
         webClient.perform(post(API_URL)
-                .content(convertObjectToJsonString(CASE_DATA))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
+            .content(convertObjectToJsonString(CASE_DATA))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest());
     }
 
     @Test
@@ -167,54 +203,54 @@ public class SubmitCaseTest {
         stubValidationServerEndpoint();
 
         webClient.perform(post(API_URL)
-                .header(AUTHORIZATION, AUTH_TOKEN)
-                .content(convertObjectToJsonString(CASE_DATA))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is4xxClientError());
+            .header(AUTHORIZATION, AUTH_TOKEN)
+            .content(convertObjectToJsonString(CASE_DATA))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().is4xxClientError());
     }
 
     private void stubFormatterServerEndpoint() {
         formatterServiceServer.stubFor(WireMock.post(CCD_FORMAT_CONTEXT_PATH)
-                .withRequestBody(matchingJsonPath(COURT_ID_JSON_PATH))
-                .willReturn(aResponse()
-                        .withStatus(HttpStatus.OK.value())
-                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_UTF8_VALUE)
-                        .withBody(convertObjectToJsonString(CASE_DATA))));
+            .withRequestBody(matchingJsonPath(COURT_ID_JSON_PATH))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withHeader(CONTENT_TYPE, APPLICATION_JSON_UTF8_VALUE)
+                .withBody(convertObjectToJsonString(CASE_DATA))));
     }
 
     private void stubValidationServerEndpoint() {
         validationServiceServer.stubFor(WireMock.post(VALIDATION_CONTEXT_PATH)
-                .withRequestBody(equalToJson(convertObjectToJsonString(validationRequest)))
-                .willReturn(aResponse()
-                        .withStatus(HttpStatus.OK.value())
-                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_UTF8_VALUE)
-                        .withBody(convertObjectToJsonString(validationResponse))));
+            .withRequestBody(equalToJson(convertObjectToJsonString(validationRequest)))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withHeader(CONTENT_TYPE, APPLICATION_JSON_UTF8_VALUE)
+                .withBody(convertObjectToJsonString(validationResponse))));
     }
 
     private void stubMaintenanceServerEndpointForSubmit(Map<String, Object> response) {
         maintenanceServiceServer.stubFor(WireMock.post(SUBMISSION_CONTEXT_PATH)
-                .withRequestBody(equalToJson(convertObjectToJsonString(CASE_DATA)))
-                .withHeader(AUTHORIZATION, new EqualToPattern(AUTH_TOKEN))
-                .willReturn(aResponse()
-                        .withStatus(HttpStatus.OK.value())
-                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_UTF8_VALUE)
-                        .withBody(convertObjectToJsonString(response))));
+            .withRequestBody(equalToJson(convertObjectToJsonString(CASE_DATA)))
+            .withHeader(AUTHORIZATION, new EqualToPattern(AUTH_TOKEN))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withHeader(CONTENT_TYPE, APPLICATION_JSON_UTF8_VALUE)
+                .withBody(convertObjectToJsonString(response))));
     }
 
     private void stubMaintenanceServerEndpointForRetrieve(HttpStatus status, Map<String, Object> response) {
         maintenanceServiceServer.stubFor(get(urlEqualTo(RETRIEVE_CASE_CONTEXT_PATH))
-                .willReturn(aResponse()
-                        .withStatus(status.value())
-                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_UTF8_VALUE)
-                        .withBody(convertObjectToJsonString(response))));
+            .willReturn(aResponse()
+                .withStatus(status.value())
+                .withHeader(CONTENT_TYPE, APPLICATION_JSON_UTF8_VALUE)
+                .withBody(convertObjectToJsonString(response))));
     }
 
     private void stubMaintenanceServerEndpointForDeleteDraft(HttpStatus status) {
         maintenanceServiceServer.stubFor(WireMock.delete(DELETE_DRAFT_CONTEXT_PATH)
-                .withHeader(AUTHORIZATION, new EqualToPattern(AUTH_TOKEN))
-                .willReturn(aResponse()
-                        .withStatus(status.value())));
+            .withHeader(AUTHORIZATION, new EqualToPattern(AUTH_TOKEN))
+            .willReturn(aResponse()
+                .withStatus(status.value())));
     }
 
 }
