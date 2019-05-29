a3e35d42503580a2220437cb8a95c26bbbc94360
Qiang Zhou <qiang.zhou@hmcts.net>
2019-05-29 15:14:14 +0100

    Add await on integration test

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/maintenance/CreateBulkCaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/maintenance/CreateBulkCaseTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.divorce.maintenance;
 import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.entity.ContentType;
+import org.awaitility.core.ConditionTimeoutException;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -13,14 +14,17 @@ import uk.gov.hmcts.reform.divorce.support.CcdSubmissionSupport;
 import uk.gov.hmcts.reform.divorce.util.RestUtil;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.springframework.http.HttpStatus.OK;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.BulkCaseConstants.BULK_CASE_LIST_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AWAITING_DN_AOS_EVENT_ID;
@@ -35,7 +39,8 @@ public class CreateBulkCaseTest extends CcdSubmissionSupport {
     private static final String TEST_AOS_STARTED_EVENT_ID = "testAosStarted";
     private static final String  BULK_CASE_PATH = "/bulk/case";
 
-    private static final int  WAITING_TIME_IN_MILLIS = 2000;
+    private static final int  MAX_WAITING_TIME_IN_SECONDS = 30;
+    private static final int  POOL_INTERVAL_IN_MILLIS = 500;
 
     @Value("${case.orchestration.maintenance.submit.context-path}")
     private String caseCreationContextPath;
@@ -48,31 +53,29 @@ public class CreateBulkCaseTest extends CcdSubmissionSupport {
         final UserDetails user1 = createCitizenUser();
         final UserDetails user2 = createCitizenUser();
 
-        CaseDetails case1 = createAwaitingPronouncementCase(user1);
-        CaseDetails case2 = createAwaitingPronouncementCase(user2);
-        waitToProcess();
+        createAwaitingPronouncementCase(user1);
+        createAwaitingPronouncementCase(user2);
 
-        createBulkCase()
-            .then()
+        Response response  = createBulkCase();
+        response.then()
             .assertThat()
             .statusCode(is(HttpStatus.OK.value()))
             .body(BULK_CASE_LIST_KEY, is(not(empty())));
 
-        waitToProcess();
-
-        CaseDetails case1updated = retrieveCase(user1, case1.getId().toString());
-        CaseDetails case2updated = retrieveCase(user2, case2.getId().toString());
-
-        assertThat(case1updated.getData().get(BULK_LISTING_CASE_ID_FIELD), is(notNullValue()));
-        assertThat(case2updated.getData().get(BULK_LISTING_CASE_ID_FIELD), is(notNullValue()));
-
+        List<String> casesList = response.jsonPath().get("BulkCases.case_data.CaseAcceptedList[0].value.CaseReference");
+        final UserDetails caseworker = createCaseWorkerUser();
+        casesList.forEach( caseId ->{
+            validateWithAwaitingTime(caseworker, caseId);
+        });
     }
 
-    private void waitToProcess() {
+    private void validateWithAwaitingTime(UserDetails user, String caseId) {
         try {
-            Thread.sleep(WAITING_TIME_IN_MILLIS);
-        } catch (InterruptedException e) {
-            log.info("Error  waiting", e);
+            await().pollInterval(POOL_INTERVAL_IN_MILLIS, MILLISECONDS)
+                .atMost(MAX_WAITING_TIME_IN_SECONDS, SECONDS)
+                .until(() -> retrieveCaseForCaseworker(user, caseId).getData().get(BULK_LISTING_CASE_ID_FIELD) != null);
+        } catch (ConditionTimeoutException exception) {
+            fail(String.format("Waiting time exhausted but case with id %s not updated yet", caseId));
         }
     }
 
@@ -83,7 +86,6 @@ public class CreateBulkCaseTest extends CcdSubmissionSupport {
         updateCaseForCitizen(String.valueOf(caseDetails.getId()), null, TEST_AOS_STARTED_EVENT_ID, userDetails);
         updateCaseForCitizen(String.valueOf(caseDetails.getId()), null, AWAITING_DN_AOS_EVENT_ID, userDetails);
 
-
         Response cosResponse = submitDnCase(userDetails.getAuthToken(), caseDetails.getId(),
             "dn-submit.json");
         assertEquals(OK.value(), cosResponse.getStatusCode());
@@ -92,7 +94,6 @@ public class CreateBulkCaseTest extends CcdSubmissionSupport {
         updateCase(String.valueOf(caseDetails.getId()), null, "refertoLegalAdvisor");
         return updateCase(String.valueOf(caseDetails.getId()), null, "entitlementGranted");
     }
-
 
     private Response submitDnCase(String userToken, Long caseId, String filePath) throws Exception {
         final Map<String, Object> headers = new HashMap<>();
