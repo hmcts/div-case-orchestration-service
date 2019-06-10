1d91709d43dd638608552842cb8fa970ba76a864
Qiang Zhou <qiang.zhou@hmcts.net>
2019-06-10 12:41:14 +0100

    Keep default constructor for quartz

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/job/CreateBulkCaseJob.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/job/CreateBulkCaseJob.java
@@ -5,14 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.WorkflowException;
 import uk.gov.hmcts.reform.divorce.orchestration.service.CaseOrchestrationService;
 
 @Slf4j
-@RequiredArgsConstructor
 public class CreateBulkCaseJob implements Job {
 
-    private final CaseOrchestrationService orchestrationService;
+    @Autowired
+    private CaseOrchestrationService orchestrationService;
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
