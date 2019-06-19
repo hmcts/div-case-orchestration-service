84847c93d647f344a93d548b5ce4eae38c2b7439
Patryk Jar <jar.patryk@gmail.com>
2019-06-19 13:12:05 +0200

    DIV-4420: refactored int tests

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/callback/ProcessPbaPaymentTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/callback/ProcessPbaPaymentTest.java
@@ -1,20 +1,14 @@
 package uk.gov.hmcts.reform.divorce.callback;
 
 import io.restassured.response.Response;
-import org.apache.http.entity.ContentType;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import uk.gov.hmcts.reform.divorce.context.IntegrationTest;
-import uk.gov.hmcts.reform.divorce.util.ResourceLoader;
-import uk.gov.hmcts.reform.divorce.util.RestUtil;
 
-import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static uk.gov.hmcts.reform.divorce.callback.SolicitorCreateAndUpdateTest.postWithDataAndValidateResponse;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.SOLICITOR_FEE_ACCOUNT_NUMBER_JSON_KEY;
 
 public class ProcessPbaPaymentTest extends IntegrationTest {
@@ -27,17 +21,11 @@ public class ProcessPbaPaymentTest extends IntegrationTest {
 
     @Test
     public void givenCallbackRequest_whenProcessPbaPayment_thenReturnDataWithNoErrors() throws Exception {
-        final Map<String, Object> headers = new HashMap<>();
-        headers.put(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.toString());
-        headers.put(HttpHeaders.AUTHORIZATION, createCaseWorkerUser().getAuthToken());
-
-        Response response = RestUtil.postToRestService(
+        Response response = postWithDataAndValidateResponse(
                 serverUrl + contextPath,
-                headers,
-                ResourceLoader.loadJson(PAYLOAD_CONTEXT_PATH + "solicitor-request-data.json")
+                PAYLOAD_CONTEXT_PATH + "solicitor-request-data.json",
+                createCaseWorkerUser().getAuthToken()
         );
-
-        assertEquals(HttpStatus.OK.value(), response.getStatusCode());
 
         Map<String, Object> responseData = response.getBody().path(DATA_KEY);
 
