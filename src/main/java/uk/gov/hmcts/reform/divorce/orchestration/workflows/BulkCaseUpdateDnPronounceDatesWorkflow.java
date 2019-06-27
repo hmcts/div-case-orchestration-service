3e0fac1cb94e4c0e6c07ed0f9ed46f053ca516d3
tony.chow <tony.chow@hmcts.net>
2019-06-28 00:01:18 +0100

    Fixed checkstyle issue

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/BulkCaseUpdateDnPronounceDatesWorkflow.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/BulkCaseUpdateDnPronounceDatesWorkflow.java
@@ -30,7 +30,7 @@ public class BulkCaseUpdateDnPronounceDatesWorkflow extends DefaultWorkflow<Map<
 
         return this.execute(
                 new Task[] {
-                        setDnPronouncementDetails,
+                    setDnPronouncementDetails,
                     updateDivorceCasePronouncementDateWithinBulk
                 },
                 caseDetails.getCaseData(),
