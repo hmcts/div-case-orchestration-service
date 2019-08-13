0ea9ab2b369b24f6967aab979458b0511ac6dc37
Qiang Zhou <qiang.zhou@hmcts.net>
2019-08-13 10:19:18 +0100

    Add judge conditional

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/ListForPronouncementDocGenerationWorkflow.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/ListForPronouncementDocGenerationWorkflow.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.divorce.orchestration.workflows;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CcdCallbackRequest;
@@ -12,6 +13,8 @@ import uk.gov.hmcts.reform.divorce.orchestration.tasks.DocumentGenerationTask;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.SetFormattedDnCourtDetails;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.SyncBulkCaseListTask;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AUTH_TOKEN_JSON_KEY;
@@ -19,14 +22,16 @@ import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.Orchestrati
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_FILENAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_TEMPLATE_ID;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_TYPE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.PRONOUNCEMENT_JUDGE_CCD_FIELD;
 
 @Component
 @RequiredArgsConstructor
 public class ListForPronouncementDocGenerationWorkflow extends DefaultWorkflow<Map<String, Object>> {
 
-    public static final String templateId = "FL-DIV-GNO-ENG-00059.docx";
-    public static final String  documentType = "caseListForPronouncement";
-    public static final String  filename = "caseListForPronouncement";
+    private static final String LIST_FOR_PRONOUNCEMENT_TEMPLATE_ID = "FL-DIV-GNO-ENG-00059.docx";
+    private static final String LIST_FOR_PRONOUNCEMENT_DOCUMENT_TYPE = "caseListForPronouncement";
+    private static final String LIST_FOR_PRONOUNCEMENT_FILE_NAME = "caseListForPronouncement";
+
     private final SetFormattedDnCourtDetails setFormattedDnCourtDetails;
 
     private final DocumentGenerationTask documentGenerationTask;
@@ -35,18 +40,26 @@ public class ListForPronouncementDocGenerationWorkflow extends DefaultWorkflow<M
 
     private final SyncBulkCaseListTask syncBulkCaseListTask;
 
-
     public Map<String, Object> run(final CcdCallbackRequest ccdCallbackRequest, final String authToken) throws WorkflowException {
 
+        String judgeName = (String) ccdCallbackRequest.getCaseDetails().getCaseData().get(PRONOUNCEMENT_JUDGE_CCD_FIELD);
+        List<Task> taskList = new ArrayList<>();
+        taskList.add(syncBulkCaseListTask);
+        if (StringUtils.isNotBlank(judgeName)) {
+            taskList.add(setFormattedDnCourtDetails);
+            taskList.add(documentGenerationTask);
+            taskList.add(caseFormatterAddDocuments);
+        }
+
+        Task[] taskArr = new Task[taskList.size()];
         return this.execute(
-            new Task[] {syncBulkCaseListTask, setFormattedDnCourtDetails, documentGenerationTask, caseFormatterAddDocuments},
+            taskList.toArray(taskArr),
             ccdCallbackRequest.getCaseDetails().getCaseData(),
             ImmutablePair.of(AUTH_TOKEN_JSON_KEY, authToken),
             ImmutablePair.of(CASE_DETAILS_JSON_KEY, ccdCallbackRequest.getCaseDetails()),
-            ImmutablePair.of(DOCUMENT_TYPE, documentType),
-            ImmutablePair.of(DOCUMENT_TEMPLATE_ID, templateId),
-            ImmutablePair.of(DOCUMENT_FILENAME, filename)
+            ImmutablePair.of(DOCUMENT_TYPE, LIST_FOR_PRONOUNCEMENT_DOCUMENT_TYPE),
+            ImmutablePair.of(DOCUMENT_TEMPLATE_ID, LIST_FOR_PRONOUNCEMENT_TEMPLATE_ID),
+            ImmutablePair.of(DOCUMENT_FILENAME, LIST_FOR_PRONOUNCEMENT_FILE_NAME)
         );
     }
-
 }
