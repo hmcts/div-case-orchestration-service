b5ae08beaf917873a3a3806216481b9de50ce82f
Patryk Jar <jar.patryk@gmail.com>
2019-08-29 17:18:36 +0200

    updated interface with ValidationService

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/ValidateCaseDataTask.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/ValidateCaseDataTask.java
@@ -1,16 +1,15 @@
 package uk.gov.hmcts.reform.divorce.orchestration.tasks;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.divorce.models.request.ValidationRequest;
+import uk.gov.hmcts.reform.divorce.models.request.CoreCaseData;
 import uk.gov.hmcts.reform.divorce.models.response.ValidationResponse;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
 import uk.gov.hmcts.reform.divorce.validation.service.ValidationService;
 
 import java.util.Map;
-
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.FORM_ID;
 
 @Component
 public class ValidateCaseDataTask implements Task<Map<String, Object>> {
@@ -23,12 +22,10 @@ public class ValidateCaseDataTask implements Task<Map<String, Object>> {
 
     @Override
     public Map<String, Object> execute(TaskContext context, Map<String, Object> caseData) {
-        ValidationResponse validationResponse =
-            validationService.validate(
-                ValidationRequest.builder()
-                    .data(caseData)
-                    .formId(FORM_ID)
-                    .build());
+        ObjectMapper mapper = new ObjectMapper();
+        CoreCaseData coreCaseData = mapper.convertValue(caseData, CoreCaseData.class);
+
+        ValidationResponse validationResponse = validationService.validate(coreCaseData);
 
         if (!validationResponse.isValid()) {
             context.setTaskFailed(true);
