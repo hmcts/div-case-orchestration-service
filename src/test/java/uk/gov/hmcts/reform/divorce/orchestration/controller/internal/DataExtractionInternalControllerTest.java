f51598817e7559b71758543c52b36e2a6d474398
Chris Iliev <c.iliev@kainos.com>
2019-09-10 12:01:20 +0100

    Integration test calls listener test fixed

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/controller/internal/DataExtractionInternalControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/controller/internal/DataExtractionInternalControllerTest.java
@@ -2,28 +2,42 @@ package uk.gov.hmcts.reform.divorce.orchestration.controller.internal;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import uk.gov.hmcts.reform.divorce.orchestration.service.CaseOrchestrationServiceException;
-import uk.gov.hmcts.reform.divorce.orchestration.service.DataExtractionService;
+import uk.gov.hmcts.reform.divorce.orchestration.event.domain.DataExtractionRequest;
+import uk.gov.hmcts.reform.divorce.orchestration.event.listener.DataExtractionRequestListener;
 
+import java.time.LocalDate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.divorce.orchestration.event.domain.DataExtractionRequest.Status.DA;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DataExtractionInternalControllerTest {
 
     @Mock
-    private DataExtractionService dataExtractionService;
+    private DataExtractionRequestListener listener;
+
+    @Captor
+    private ArgumentCaptor<DataExtractionRequest> dataExtractionRequestArgumentCaptor;
 
     @InjectMocks
     private DataExtractionInternalController classUnderTest;
 
     @Test
-    public void shouldRequestDataExtractionForFamilyMan() throws CaseOrchestrationServiceException {
+    public void shouldCallJob_ForFamilyManDataExtraction() {
         classUnderTest.startDataExtractionToFamilyMan();
 
-        verify(dataExtractionService).requestDataExtractionForPreviousDay();
+        verify(listener).onApplicationEvent(dataExtractionRequestArgumentCaptor.capture());
+        DataExtractionRequest event = dataExtractionRequestArgumentCaptor.getValue();
+        assertThat(event.getStatus(), is(DA));
+        LocalDate yesterday = LocalDate.now().minusDays(1);
+        assertThat(event.getDate(), is(yesterday));
     }
 
 }
