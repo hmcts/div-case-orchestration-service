6f96c4c092c3699c5736b72e30c430736b38ed64
Piyush Kumar <piyushk@Blanka-2.local>
2019-09-12 18:44:36 +0100

    cleanup ase after migration to aks

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -9,35 +9,10 @@ output "vaultUri" {
 output "test_environment" {
   value = "${local.local_env}"
 }
-
-output "idam_api_baseurl" {
-  value = "${var.idam_api_baseurl}"
-}
-
 output "service_auth_provider_url" {
   value = "${local.idam_s2s_url}"
 }
 
 output "case_maintenance_service_api_baseurl" {
   value = "${local.case_maintenance_service_baseurl}"
-}
-
-output "aos_responded_days_to_complete" {
-  value = "${var.aos_responded_days_to_complete}"
-}
-
-output "aos_responded_awaiting_answer_days_to_respond" {
-  value = "${var.aos_responded_awaiting_answer_days_to_respond}"
-}
-
-output "scheudler_schedules_create_bulk_cases_cron" {
-  value = "${var.scheudler_schedules_create_bulk_cases_cron}"
-}
-
-output "feature_resp_solicitor_details" {
-  value = "${var.feature_resp_solicitor_details}"
-}
-
-output "documentation_swagger_enabled" {
-  value = "${var.documentation_swagger_enabled}"
 }
