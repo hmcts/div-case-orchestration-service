fa645d91f0a0b38ca4e57e104c19581c11e5ac87
J. Victor Ferreira <victor.ferreira@hmcts.net>
2019-09-13 15:23:11 +0100

    Factory unit tested

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/dataextraction/CSVExtractorFactory.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/dataextraction/CSVExtractorFactory.java
@@ -1,17 +1,42 @@
 package uk.gov.hmcts.reform.divorce.orchestration.tasks.dataextraction;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.divorce.orchestration.event.domain.DataExtractionRequest;
+import uk.gov.hmcts.reform.divorce.orchestration.event.domain.DataExtractionRequest.Status;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import static java.lang.String.format;
+import static uk.gov.hmcts.reform.divorce.orchestration.event.domain.DataExtractionRequest.Status.DA;
 
 @Component
 public class CSVExtractorFactory {
 
-    public CSVExtractor getCSVExtractorForStatus(DataExtractionRequest.Status status) {
-        return null;
+    @Autowired
+    private DecreeAbsoluteDataExtractor decreeAbsoluteDataExtractor;
+
+    private Map<Status, CSVExtractor> csvExtractorMap = new EnumMap<>(Status.class);
+
+    @PostConstruct
+    public void init() {
+        csvExtractorMap.put(DA, decreeAbsoluteDataExtractor);
     }
 
-    public boolean hasCSVExtractorForStatus(DataExtractionRequest.Status status) {
-        return false;
+    public CSVExtractor getCSVExtractorForStatus(Status status) {
+        CSVExtractor csvExtractor = csvExtractorMap.get(status);
+
+        if (csvExtractor == null) {
+            throw new UnsupportedOperationException(format("CSVExtractor for %s not implemented.", status));
+        }
+
+        return csvExtractor;
+    }
+
+    public boolean hasCSVExtractorForStatus(Status status) {
+        return csvExtractorMap.containsKey(status);
     }
 
 }
--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/dataextraction/CSVExtractorFactoryTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/dataextraction/CSVExtractorFactoryTest.java
@@ -1,17 +1,57 @@
 package uk.gov.hmcts.reform.divorce.orchestration.tasks.dataextraction;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.rules.ExpectedException.none;
+import static uk.gov.hmcts.reform.divorce.orchestration.event.domain.DataExtractionRequest.Status.AOS;
+import static uk.gov.hmcts.reform.divorce.orchestration.event.domain.DataExtractionRequest.Status.DA;
+import static uk.gov.hmcts.reform.divorce.orchestration.event.domain.DataExtractionRequest.Status.DN;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CSVExtractorFactoryTest {
 
+    @Rule
+    public ExpectedException expectedException = none();
+
+    @Mock
+    private DecreeAbsoluteDataExtractor decreeAbsoluteDataExtractor;
+
+    @InjectMocks
+    private CSVExtractorFactory csvExtractorFactory;
+
+    @Before
+    public void setUp(){
+        csvExtractorFactory.init();
+    }
+
     @Test
-    public void getCSVExtractorForStatus() {
+    public void getRightCSVExtractor() {
+        assertThat(csvExtractorFactory.getCSVExtractorForStatus(DA), is(decreeAbsoluteDataExtractor));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenTryingToRetrieveUnimplementedCSVExtractor() {
+        expectedException.expect(UnsupportedOperationException.class);
+        expectedException.expectMessage("CSVExtractor for AOS not implemented.");
+
+        csvExtractorFactory.getCSVExtractorForStatus(AOS);
+        csvExtractorFactory.getCSVExtractorForStatus(DN);
     }
 
     @Test
     public void hasCSVExtractorForStatus() {
+        assertThat(csvExtractorFactory.hasCSVExtractorForStatus(DA), is(true));
+        assertThat(csvExtractorFactory.hasCSVExtractorForStatus(DN), is(false));
+        assertThat(csvExtractorFactory.hasCSVExtractorForStatus(AOS), is(false));
     }
 
 }
