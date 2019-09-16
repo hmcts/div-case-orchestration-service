236a7e011938567c0caa504e8e88eb18db858604
John O'Neill <j.oneill@kainos.com>
2019-09-16 17:40:11 +0100

    Checkstyle fix

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/DecreeAbsoluteAboutToBeGrantedWorkflowTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/DecreeAbsoluteAboutToBeGrantedWorkflowTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.divorce.orchestration.workflows;
 
-import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InOrder;
