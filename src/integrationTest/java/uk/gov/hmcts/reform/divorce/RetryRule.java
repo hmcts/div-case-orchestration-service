533924c88498a73179db9bb5a5e35dd99e851089
Hosain <hosainnet@users.noreply.github.com>
2019-09-23 11:09:41 +0100

    DIV-5762 - added disableLegacyDeployment() to switch deployment to AKS (#495)
    * DIV-5762 - added disableLegacyDeployment() to switch deployment to AKS
    This will disable the legacy Azure deployment and switch over the DNS
    To the new K8S prod cluster
    Also added retry for integration tests due to flaky services

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/RetryRule.java
@@ -0,0 +1,40 @@
+package uk.gov.hmcts.reform.divorce;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public class RetryRule implements TestRule {
+    private int retryCount;
+
+    public RetryRule(int retryCount) {
+        this.retryCount = retryCount;
+    }
+
+    public Statement apply(Statement base, Description description) {
+        return statement(base, description);
+    }
+
+    private Statement statement(final Statement base, final Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                Throwable caughtThrowable = null;
+
+                for (int i = 0; i < retryCount; i++) {
+                    try {
+                        base.evaluate();
+                        return;
+                    } catch (Throwable t) {
+                        caughtThrowable = t;
+                        System.err.println(
+                                description.getDisplayName() + ": run " + (i + 1) + " failed. - " + t.getMessage()
+                        );
+                    }
+                }
+                System.err.println(description.getDisplayName() + ": giving up after " + retryCount + " failures.");
+                throw caughtThrowable;
+            }
+        };
+    }
+}
