e500f9d199588bf06b1bc5bc435f37b6eb70366e
Qiang Zhou <qiang.zhou@hmcts.net>
2019-09-23 23:38:36 +0100

    DIV-3934 Added DN rejection notification

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/notification/NotifyForRefusalOrderTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/notification/NotifyForRefusalOrderTaskTest.java
@@ -27,13 +27,16 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_NISI_GRANTED_CCD_FIELD;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DN_REFUSED_REJECT_OPTION;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D_8_CASE_REFERENCE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D_8_INFERRED_PETITIONER_GENDER;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D_8_PETITIONER_EMAIL;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D_8_PETITIONER_FIRST_NAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D_8_PETITIONER_LAST_NAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.NOTIFICATION_ADDRESSEE_FIRST_NAME_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.NOTIFICATION_ADDRESSEE_LAST_NAME_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.NOTIFICATION_CASE_NUMBER_KEY;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.NOTIFICATION_HUSBAND_OR_WIFE;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.NO_VALUE;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.REFUSAL_DECISION_CCD_FIELD;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.REFUSAL_DECISION_MORE_INFO_VALUE;
@@ -42,29 +45,32 @@ import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.Orchestrati
 @RunWith(MockitoJUnitRunner.class)
 public class NotifyForRefusalOrderTaskTest {
 
+    private static final String MALE_GENDER = "male";
+    private static final String TEST_CASE_REFERENCE = "TEST_CASE_REFERENCE";
+    private static final String PETITIONER_EMAIL = "applicant@divorce.gov.uk";
+    private static final String PETITIONER_FIRST_NAME = "Jerry";
+    private static final String PETITIONER_LAST_NAME = "Johnson";
+    private static final String RELEATION = "husband";
+
     private Map<String, Object> incomingPayload;
     private TaskContext taskContext;
 
-    private String testCaseReference = "testCaseReference";
-    private String petitionerEmail = "applicant@divorce.gov.uk";
-    private String petitionerFirstName = "Jerry";
-    private String petitionerLastName = "Johnson";
 
     @Mock
     private EmailService emailService;
 
     @InjectMocks
-    NotifyForRefusalOrderTask notifyForRefusalOrderTask;
+    private NotifyForRefusalOrderTask notifyForRefusalOrderTask;
 
 
     @Before
     public void setUp() {
         incomingPayload = new HashMap<>();
-        incomingPayload.put(D_8_PETITIONER_EMAIL, petitionerEmail);
-        incomingPayload.put(D_8_PETITIONER_FIRST_NAME, petitionerFirstName);
-        incomingPayload.put(D_8_PETITIONER_LAST_NAME, petitionerLastName);
-        incomingPayload.put(D_8_CASE_REFERENCE, testCaseReference);
-
+        incomingPayload.put(D_8_PETITIONER_EMAIL, PETITIONER_EMAIL);
+        incomingPayload.put(D_8_PETITIONER_FIRST_NAME, PETITIONER_FIRST_NAME);
+        incomingPayload.put(D_8_PETITIONER_LAST_NAME, PETITIONER_LAST_NAME);
+        incomingPayload.put(D_8_CASE_REFERENCE, TEST_CASE_REFERENCE);
+        incomingPayload.put(D_8_INFERRED_PETITIONER_GENDER, MALE_GENDER);
         taskContext = new DefaultTaskContext();
     }
 
@@ -75,13 +81,35 @@ public class NotifyForRefusalOrderTaskTest {
         Map returnedPayload = notifyForRefusalOrderTask.execute(taskContext, incomingPayload);
 
         assertThat(returnedPayload, equalTo(incomingPayload));
-        verify(emailService).sendEmail(eq(petitionerEmail),
+        verify(emailService).sendEmail(eq(PETITIONER_EMAIL),
             eq(EmailTemplateNames.DECREE_NISI_REFUSAL_ORDER_CLARIFICATION.name()),
             argThat(new HamcrestArgumentMatcher<>(
                 allOf(
-                    hasEntry(NOTIFICATION_CASE_NUMBER_KEY, testCaseReference),
-                    hasEntry(NOTIFICATION_ADDRESSEE_FIRST_NAME_KEY, petitionerFirstName),
-                    hasEntry(NOTIFICATION_ADDRESSEE_LAST_NAME_KEY, petitionerLastName)
+                    hasEntry(NOTIFICATION_CASE_NUMBER_KEY, TEST_CASE_REFERENCE),
+                    hasEntry(NOTIFICATION_ADDRESSEE_FIRST_NAME_KEY, PETITIONER_FIRST_NAME),
+                    hasEntry(NOTIFICATION_ADDRESSEE_LAST_NAME_KEY, PETITIONER_LAST_NAME),
+                    hasEntry(NOTIFICATION_ADDRESSEE_LAST_NAME_KEY, PETITIONER_LAST_NAME)
+                )
+            )),
+            anyString()
+        );
+    }
+
+    @Test
+    public void testNotificationServiceIsCalledWithExpectedParametersWhenRefusalDecisionIsRejection() throws TaskException {
+        incomingPayload.put(DECREE_NISI_GRANTED_CCD_FIELD, NO_VALUE);
+        incomingPayload.put(REFUSAL_DECISION_CCD_FIELD, DN_REFUSED_REJECT_OPTION);
+        Map returnedPayload = notifyForRefusalOrderTask.execute(taskContext, incomingPayload);
+
+        assertThat(returnedPayload, equalTo(incomingPayload));
+        verify(emailService).sendEmail(eq(PETITIONER_EMAIL),
+            eq(EmailTemplateNames.DECREE_NISI_REFUSAL_ORDER_REJECTION.name()),
+            argThat(new HamcrestArgumentMatcher<>(
+                allOf(
+                    hasEntry(NOTIFICATION_CASE_NUMBER_KEY, TEST_CASE_REFERENCE),
+                    hasEntry(NOTIFICATION_ADDRESSEE_FIRST_NAME_KEY, PETITIONER_FIRST_NAME),
+                    hasEntry(NOTIFICATION_ADDRESSEE_LAST_NAME_KEY, PETITIONER_LAST_NAME),
+                    hasEntry(NOTIFICATION_HUSBAND_OR_WIFE, RELEATION)
                 )
             )),
             anyString()
@@ -93,7 +121,7 @@ public class NotifyForRefusalOrderTaskTest {
         Map returnedPayload = notifyForRefusalOrderTask.execute(taskContext, incomingPayload);
 
         assertThat(returnedPayload, equalTo(incomingPayload));
-        verify(emailService, never()).sendEmail(eq(petitionerEmail),
+        verify(emailService, never()).sendEmail(eq(PETITIONER_EMAIL),
             eq(EmailTemplateNames.DECREE_NISI_REFUSAL_ORDER_CLARIFICATION.name()),
             anyMap(),
             anyString()
@@ -108,7 +136,7 @@ public class NotifyForRefusalOrderTaskTest {
         Map returnedPayload = notifyForRefusalOrderTask.execute(taskContext, incomingPayload);
 
         assertThat(returnedPayload, equalTo(incomingPayload));
-        verify(emailService, never()).sendEmail(eq(petitionerEmail),
+        verify(emailService, never()).sendEmail(eq(PETITIONER_EMAIL),
             eq(EmailTemplateNames.DECREE_NISI_REFUSAL_ORDER_CLARIFICATION.name()),
             anyMap(),
             anyString()
