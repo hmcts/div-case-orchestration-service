a7b9c86cbdebd25f254877ae57598a2c738d7ebc
Chris Iliev <iichr@users.noreply.github.com>
2019-09-23 16:38:23 +0100

    Implemented AOS offline data extraction to CSV (#479)

--- infrastructure/prod.tfvars
+++ infrastructure/prod.tfvars
@@ -9,3 +9,4 @@ scheduler_make_cases_eligible_da_enabled = "true"
 scheduler_send_updated_cases_to_robotics_enabled = "true"
 
 dataextraction_status_da_email_to = "StokeCTSC4.Auto@justice.gov.uk"
+dataextraction_status_aos_email_to = "Auto.StokeCTSC2@justice.gov.uk"
--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/controller/internal/DataExtractionInternalControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/controller/internal/DataExtractionInternalControllerTest.java
@@ -8,17 +8,24 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.divorce.orchestration.event.domain.DataExtractionRequest;
+import uk.gov.hmcts.reform.divorce.orchestration.event.domain.DataExtractionRequest.Status;
 import uk.gov.hmcts.reform.divorce.orchestration.event.listener.DataExtractionRequestListener;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.divorce.orchestration.event.domain.DataExtractionRequest.Status.AOS;
 import static uk.gov.hmcts.reform.divorce.orchestration.event.domain.DataExtractionRequest.Status.DA;
+import static uk.gov.hmcts.reform.divorce.orchestration.event.domain.DataExtractionRequest.Status.DN;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DataExtractionInternalControllerTest {
+
+    private final LocalDate yesterday = LocalDate.now().minusDays(1);
 
     @Mock
     private DataExtractionRequestListener listener;
@@ -33,11 +40,17 @@ public class DataExtractionInternalControllerTest {
     public void shouldCallJob_ForFamilyManDataExtraction() {
         classUnderTest.startDataExtractionToFamilyMan();
 
-        verify(listener).onApplicationEvent(dataExtractionRequestArgumentCaptor.capture());
-        DataExtractionRequest event = dataExtractionRequestArgumentCaptor.getValue();
-        assertThat(event.getStatus(), is(DA));
-        LocalDate yesterday = LocalDate.now().minusDays(1);
+        verify(listener, times(3)).onApplicationEvent(dataExtractionRequestArgumentCaptor.capture());
+        List<DataExtractionRequest> allEvents = dataExtractionRequestArgumentCaptor.getAllValues();
+
+        verifyEvent(allEvents.get(0), DA);
+        verifyEvent(allEvents.get(1), AOS);
+        verifyEvent(allEvents.get(2), DN);
+    }
+
+    private void verifyEvent(DataExtractionRequest event, Status expectedStatus) {
         assertThat(event.getDate(), is(yesterday));
+        assertThat(event.getStatus(), is(expectedStatus));
     }
 
 }
