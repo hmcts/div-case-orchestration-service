6431548abac58df756f976c6f18f05fb08903bd4
tony.chow <tony.chow@hmcts.net>
2019-09-26 10:21:51 +0100

    Adjusted to existing Refusal Order documents

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/DecreeNisiRefusalDocumentGeneratorTask.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/DecreeNisiRefusalDocumentGeneratorTask.java
@@ -9,19 +9,31 @@ import uk.gov.hmcts.reform.divorce.orchestration.domain.model.documentgeneration
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static java.lang.String.format;
 import static java.util.Collections.singletonMap;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AUTH_TOKEN_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_DETAILS_JSON_KEY;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D8DOCUMENTS_GENERATED;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_NISI_REFUSAL_DOCUMENT_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_NISI_REFUSAL_DOCUMENT_NAME_OLD;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_NISI_REFUSAL_ORDER_CLARIFICATION_TEMPLATE_ID;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_NISI_REFUSAL_ORDER_DOCUMENT_TYPE;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_CASE_DETAILS_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_COLLECTION;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_FILENAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_FILENAME_FMT;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_TYPE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_TYPE_OTHER;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.REFUSAL_DECISION_CCD_FIELD;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.REFUSAL_DECISION_MORE_INFO_VALUE;
 
@@ -29,7 +41,10 @@ import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.Orchestrati
 @AllArgsConstructor
 public class DecreeNisiRefusalDocumentGeneratorTask implements Task<Map<String, Object>> {
 
+    private static final String VALUE_KEY = "value";
+
     private final DocumentGeneratorClient documentGeneratorClient;
+    private final Clock clock;
 
     @Override
     public Map<String, Object> execute(final TaskContext context, final Map<String, Object> caseData) {
@@ -39,6 +54,22 @@ public class DecreeNisiRefusalDocumentGeneratorTask implements Task<Map<String, 
             new LinkedHashSet<>());
 
         if (REFUSAL_DECISION_MORE_INFO_VALUE.equalsIgnoreCase((String) caseData.get(REFUSAL_DECISION_CCD_FIELD))) {
+            // Rename and set previous refusal order documents to other type so it won't get overwritten
+            List<Map<String, Object>> d8DocumentsGenerated =
+                Optional.ofNullable((List<Map<String, Object>>) caseData.get(D8DOCUMENTS_GENERATED))
+                    .orElse(new ArrayList<>());
+
+            d8DocumentsGenerated.stream().filter(collectionMember -> {
+                Map<String, Object> document = (Map<String, Object>) collectionMember.get(VALUE_KEY);
+                return DECREE_NISI_REFUSAL_ORDER_DOCUMENT_TYPE.equals(document.get(DOCUMENT_TYPE));
+            }).forEach(collectionMember -> {
+                Map<String, Object> document = (Map<String, Object>) collectionMember.get(VALUE_KEY);
+                document.put(DOCUMENT_TYPE, DOCUMENT_TYPE_OTHER);
+                document.put(DOCUMENT_FILENAME,
+                    format(DOCUMENT_FILENAME_FMT, DECREE_NISI_REFUSAL_DOCUMENT_NAME_OLD,
+                        caseDetails.getCaseId() + "-" + Instant.now(clock).toEpochMilli()));
+            });
+
             GeneratedDocumentInfo generatedDocumentInfo = generatePdfDocument(
                 DECREE_NISI_REFUSAL_ORDER_CLARIFICATION_TEMPLATE_ID,
                 DECREE_NISI_REFUSAL_ORDER_DOCUMENT_TYPE,
