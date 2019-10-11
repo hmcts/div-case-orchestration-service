ffbd7c0df9d3983f9ff7adc2b09344b486cd72b4
qzhou-hmcts <42572376+qzhou-hmcts@users.noreply.github.com>
2019-10-11 13:49:40 +0100

    DIV-5773 Add AwaitingAdminClarification state (#529)

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/SetDNDecisionStateTask.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/SetDNDecisionStateTask.java
@@ -10,11 +10,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.Features.DN_REFUSAL;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AWAITING_ADMIN_CLARIFICATION;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AWAITING_CLARIFICATION;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AWAITING_PRONOUNCEMENT;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_NISI_GRANTED_CCD_FIELD;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DN_REFUSED;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DN_REFUSED_ADMIN_ERROR_OPTION;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DN_REFUSED_REJECT_OPTION;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.EMPTY_STRING;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.REFUSAL_DECISION_CCD_FIELD;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.STATE_CCD_FIELD;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.YES_VALUE;
@@ -36,17 +39,21 @@ public class SetDNDecisionStateTask implements Task<Map<String, Object>> {
 
         } else {
             if (featureToggleService.isFeatureEnabled(DN_REFUSAL)) {
-                if (isDnRejected(caseData)) {
-                    newCaseEndState = DN_REFUSED;
+                String refusalDecision = (String) caseData.getOrDefault(REFUSAL_DECISION_CCD_FIELD, EMPTY_STRING);
+                switch (refusalDecision) {
+                    case DN_REFUSED_REJECT_OPTION:
+                        newCaseEndState = DN_REFUSED;
+                        break;
+                    case DN_REFUSED_ADMIN_ERROR_OPTION:
+                        newCaseEndState = AWAITING_ADMIN_CLARIFICATION;
+                        break;
+                    default:
+                        newCaseEndState = AWAITING_CLARIFICATION;
                 }
             }
         }
         payload.put(STATE_CCD_FIELD, newCaseEndState);
         return payload;
-    }
-
-    private boolean isDnRejected(Map<String, Object> caseData) {
-        return DN_REFUSED_REJECT_OPTION.equalsIgnoreCase((String) caseData.get(REFUSAL_DECISION_CCD_FIELD));
     }
 
     private boolean isDnGranted(Map<String, Object> caseData) {
