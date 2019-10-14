9439b0521bcfbd93d43af89cb8a3b989a61fc713
J. Victor Ferreira <victorf@kainos.com>
2019-10-14 09:38:32 +0100

    DIV-5860: Enabling Integration test for data migration to run wi… (#543)
    * Integration test now using local SMTP server
    
    * Asserting e-mail is sent (locally)
    
    * Resetting local SMTP after test

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/dataextraction/FamilyManDataExtractionIT.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/dataextraction/FamilyManDataExtractionIT.java
@@ -1,37 +1,54 @@
 package uk.gov.hmcts.reform.divorce.dataextraction;
 
+import com.dumbster.smtp.SimpleSmtpServer;
 import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Value;
 import uk.gov.hmcts.reform.divorce.support.cos.RetrieveCaseSupport;
 import uk.gov.hmcts.reform.divorce.util.RestUtil;
 
+import java.io.IOException;
+
 import static java.util.Collections.emptyMap;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 import static org.springframework.http.HttpStatus.OK;
 
 /**
  * This is going to be used to assert that the listener side of the data extraction job is called and runs successfully.
- * Ideally, we'd be testing that the e-mail was sent as expected, but as of now, we have no way of asserting that.
- *
- * <p>To run this test locally, you'll need MailHog to be running locally. Here are some instructions to have this done using Docker:
- * #Download image
- * docker pull mailhog/mailhog
- *
- * <p>#Run new container (if it's the first time you do this)
- * docker run -d -p 32773:1025 -p 32772:8025 --name localMailHog mailhog/mailhog
- *
- * <p>#Start existing container (if it's not the first time you do this)
- * docker start localMailHog
- *
- * <p>#Log into MailHog to assert the e-mails sent by this test.
- * http://localhost:32772
  */
+@Slf4j
 public class FamilyManDataExtractionIT extends RetrieveCaseSupport {
+
+    private static final int LOCAL_SMTP_SERVER_PORT = 32773;
+    private static final String LOCAL_ENVIRONMENT_NAME = "local";
+
+    private static SimpleSmtpServer simpleSmtpServer;
+
+    @Value("${env}")
+    private String environment;
 
     @Value("${case.orchestration.jobScheduler.extract-data-to-family-man.context-path}")
     private String testDataExtractionEndPoint;
+
+    @Before
+    public void setUp() throws IOException {
+        /*
+         * Set up a local SMTP server when running this test locally. In the pipeline, we'll use the actual SMTP server.
+         * The reason for this is because a firewall prevents us from connecting to the actual SMTP server from our local machines.
+         */
+        if (isTestRunningLocally()) {
+            setUpLocalEmailServer();
+            log.info("Starting local SMTP server");
+        } else {
+            log.info("Using actual SMTP server");
+        }
+    }
 
     /**
      * Please look into class-level comment if this test is not passing locally.
@@ -45,6 +62,7 @@ public class FamilyManDataExtractionIT extends RetrieveCaseSupport {
         );
 
         assertThat(response.getStatusCode(), is(OK.value()));
+        checkEmailHasBeenSent();
     }
 
     /**
@@ -59,6 +77,30 @@ public class FamilyManDataExtractionIT extends RetrieveCaseSupport {
         );
 
         assertThat(response.getStatusCode(), is(OK.value()));
+    }
+
+    @AfterClass
+    public static void cleanUp() {
+        if (simpleSmtpServer != null) {
+            simpleSmtpServer.close();
+        }
+    }
+
+    private boolean isTestRunningLocally() {
+        return LOCAL_ENVIRONMENT_NAME.equalsIgnoreCase(environment);
+    }
+
+    private static void setUpLocalEmailServer() throws IOException {
+        if (simpleSmtpServer == null) {
+            simpleSmtpServer = SimpleSmtpServer.start(LOCAL_SMTP_SERVER_PORT);
+        }
+    }
+
+    private void checkEmailHasBeenSent() {
+        if (isTestRunningLocally()) {
+            assertThat(simpleSmtpServer.getReceivedEmails(), hasSize(greaterThan(0)));
+            simpleSmtpServer.reset();
+        }
     }
 
 }
