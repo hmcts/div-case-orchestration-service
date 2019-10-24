29d0673092d83969e02c00e466243d08df62ccd2
Tony Chow <36265570+tchow8@users.noreply.github.com>
2019-10-24 15:44:48 +0100

    Send Amend Notification If Previous Case Id Exists (#553)

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/OrchestrationConstants.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/OrchestrationConstants.java
@@ -115,6 +115,7 @@ public class OrchestrationConstants {
     public static final String SOL_SERVICE_METHOD_CCD_FIELD = "SolServiceMethod";
     public static final String PERSONAL_SERVICE_VALUE = "personalService";
     public static final String NOT_DEFENDING_NOT_ADMITTING = "NoNoAdmission";
+    public static final String PREVIOUS_CASE_ID_CCD_KEY = "PreviousCaseId";
 
     public static final String PET_SOL_EMAIL = "PetitionerSolicitorEmail";
     public static final String PET_SOL_NAME = "PetitionerSolicitorName";
