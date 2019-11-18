5e87dacf28a3989310dc33c4f4a88842fd04409c
vianvi <30834324+vianvi@users.noreply.github.com>
2019-11-18 17:42:18 +0000

    Nightly tests are failing for python2 and python3 compatibility Applied Suggested Solution.

--- security.sh
+++ security.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -u ${SecurityRules} -P 1001 -l FAIL
 cat zap.out
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
