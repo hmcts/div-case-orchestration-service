dfb13ccb06bcd029e0aaf0b4e06fc492252f809c
vianvi <30834324+vianvi@users.noreply.github.com>
2019-11-19 12:10:39 +0000

    add HTML report fro ZAP scan

--- security.sh
+++ security.sh
@@ -5,6 +5,7 @@ export LANG=C.UTF-8
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -u ${SecurityRules} -P 1001 -l FAIL
 cat zap.out
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
+cp /zap/api-report.html functional-output/
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 cp *.* functional-output/
 zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l High --exit-code False
