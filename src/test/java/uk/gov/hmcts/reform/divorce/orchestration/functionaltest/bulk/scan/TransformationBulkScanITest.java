1170672fc39b05a6663a4c5f1e14860d50da7e4e
John O'Neill <j.oneill@kainos.com>
2019-11-20 11:04:04 +0000

    BSP-77: Implemented logic to transform small amount of excela data to CCD (#596)

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/functionaltest/bulk/scan/TransformationBulkScanITest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/functionaltest/bulk/scan/TransformationBulkScanITest.java
@@ -16,10 +16,9 @@ import static com.jayway.jsonpath.matchers.JsonPathMatchers.isJson;
 import static java.util.Collections.emptyList;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.ResultMatcher.matchAll;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,23 +31,38 @@ import static uk.gov.hmcts.reform.divorce.orchestration.testutil.ResourceLoader.
 @RunWith(SpringRunner.class)
 public class TransformationBulkScanITest {
 
+    private static final String EXCEPTION_RECORD_JSON_PATH = "jsonExamples/payloads/bulk/scan/basicForm.json";
+    private static final String TRANSFORMATION_URL = "/transform-exception-record";
+
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void shouldReturnSuccessResponseForTransformationEndpoint() throws Exception {
-        String formToValidate = loadResourceAsString("jsonExamples/payloads/bulk/scan/basicForm.json");
+        String formToValidate = loadResourceAsString(EXCEPTION_RECORD_JSON_PATH);
 
-        mockMvc.perform(post("/transform-exception-record")
+        mockMvc.perform(post(TRANSFORMATION_URL)
             .contentType(APPLICATION_JSON)
-            .content(formToValidate)
-        ).andExpect(matchAll(
-            status().isOk(),
-            content().string(allOf(
-                isJson(),
-                hasJsonPath("$.warnings", equalTo(emptyList())),
-                hasJsonPath("$.case_creation_details", is(notNullValue()))
-            ))
-        ));
+            .content(formToValidate))
+            .andExpect(status().isOk())
+            .andExpect(content().string(
+                allOf(
+                    isJson(),
+                    hasJsonPath("$.warnings", equalTo(emptyList())),
+                    hasJsonPath("$.case_creation_details.*", hasSize(3)),
+                    hasJsonPath("$.case_creation_details", allOf(
+                        hasJsonPath("case_type_id", is("DIVORCE")),
+                        hasJsonPath("event_id", is("EVENT_ID")),
+                        hasJsonPath("case_data.*", hasSize(6)),
+                        hasJsonPath("case_data", allOf(
+                            hasJsonPath("D8FirstName", is("Christopher")),
+                            hasJsonPath("D8LastName", is("O'John")),
+                            hasJsonPath("D8PetitionerEmail", is("test.testerson@mailinator.com")),
+                            hasJsonPath("D8legalProcess", is("Divorce")),
+                            hasJsonPath("D8ScreenHasMarriageCert", is("True")),
+                            hasJsonPath("D8CertificateInEnglish", is("True"))
+                        ))
+                    ))
+                )));
     }
 }
