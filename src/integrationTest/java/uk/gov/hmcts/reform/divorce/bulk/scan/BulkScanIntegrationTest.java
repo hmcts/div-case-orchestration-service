39afd9f94447808ca441f5b2fb600381c20cb352
vianvi <30834324+vianvi@users.noreply.github.com>
2019-12-05 17:53:48 +0000

    add Integration Tests

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/bulk/scan/BulkScanIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/bulk/scan/BulkScanIntegrationTest.java
@@ -1,45 +1,122 @@
 package uk.gov.hmcts.reform.divorce.bulk.scan;
 
+import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
+import net.serenitybdd.rest.SerenityRest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import uk.gov.hmcts.reform.divorce.context.IntegrationTest;
-import uk.gov.hmcts.reform.divorce.orchestration.domain.model.bulk.scan.validation.in.OcrDataField;
-import uk.gov.hmcts.reform.divorce.orchestration.domain.model.bulk.scan.validation.in.OcrDataValidationRequest;
-import uk.gov.hmcts.reform.divorce.support.cos.CosApiClient;
-
-import static java.util.Collections.singletonList;
-import static org.junit.Assert.fail;
+import uk.gov.hmcts.reform.divorce.support.IdamUtils;
 import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.scan.BulkScanForms.NEW_DIVORCE_CASE;
+import static uk.gov.hmcts.reform.divorce.orchestration.testutil.ResourceLoader.loadResourceAsString;
+import static uk.gov.hmcts.reform.divorce.orchestration.controller.BulkScanController.SERVICE_AUTHORISATION_HEADER;
 
+@Slf4j
 public class BulkScanIntegrationTest extends IntegrationTest {
 
     @Autowired
-    @Qualifier("bulkScanValidatorTokenGenerator")
-    private AuthTokenGenerator bulkScanTokenGenerator;
+    private IdamUtils idamUtilsS2SAuthorization;
 
-    @Autowired
-    @Qualifier("ccdSubmissionTokenGenerator")
-    private AuthTokenGenerator wrongTokenGenerator;
+    @Value("${auth.provider.bulkscan.validate.microservice}")
+    private String bulkScanValidationMicroService;
 
-    @Autowired
-    private CosApiClient cosApiClient;
+    @Value("${auth.provider.bulkscan.update.microservice}")
+    private String bulkScanTransformationAndUpdateMicroService;
+
+    @Value("${case.orchestration.service.base.uri}")
+    private String cosBaseURL;
+
+
+    private static final String FULL_D8_FORM_JSON_PATH = "jsonExamples/payloads/bulk/scan/fullD8Form.json";
+    private static String VALID_BODY;
 
     @Test
-    public void shouldGetSuccessfulResponsesWhenUsingWhitelistedService() {
-        String rightToken = "Bearer " + bulkScanTokenGenerator.generate();
-        cosApiClient.validateBulkScannedFields(
-            rightToken, //TODO - look into bearer class (BearerTokenGenerator)
-            NEW_DIVORCE_CASE, new OcrDataValidationRequest(singletonList(new OcrDataField("name", "value"))));
+    public void shouldGetSuccessfulResponsesWhenUsingWhitelistedServiceForValidationEndPoint()  throws Exception {
+        String token = idamUtilsS2SAuthorization.generateUserTokenWithValidMicroService(bulkScanValidationMicroService);
+        VALID_BODY = loadResourceAsString(FULL_D8_FORM_JSON_PATH);
+
+        Response forValidationEndpoint = responseForValidationEndpoint(token,"/forms/{form-type}/validate-ocr", NEW_DIVORCE_CASE);
+
+
+        assert forValidationEndpoint.getStatusCode() == 200 : "Service is not authorised to OCR validation " + forValidationEndpoint.getStatusCode();
+
     }
 
     @Test
-    public void shouldGetServiceDeniedWhenUsingNonWhitelistedService() {
-        String wrongToken = "Bearer " + wrongTokenGenerator.generate();
-        cosApiClient.validateBulkScannedFields(
-            wrongToken, //TODO - look into bearer class (BearerTokenGenerator)
-            NEW_DIVORCE_CASE, new OcrDataValidationRequest(singletonList(new OcrDataField("name", "value"))));
+    public void shouldGetServiceDeniedWhenUsingNonWhitelistedServiceForValidationEndPoint()  throws Exception {
+        String token = idamUtilsS2SAuthorization.generateUserTokenWithValidMicroService(bulkScanTransformationAndUpdateMicroService);
+
+        VALID_BODY = loadResourceAsString(FULL_D8_FORM_JSON_PATH);
+
+        Response forValidationEndpoint = responseForValidationEndpoint(token,"/forms/{form-type}/validate-ocr", NEW_DIVORCE_CASE);
+
+        assert forValidationEndpoint.getStatusCode() == 403 : "Not matching with expected Error code " + forValidationEndpoint.getStatusCode();
     }
 
+    @Test
+    public void shouldGetSuccessfulResponsesWhenUsingWhitelistedServiceForTransformationEndPoint()  throws Exception {
+        String token = idamUtilsS2SAuthorization.generateUserTokenWithValidMicroService(bulkScanTransformationAndUpdateMicroService);
+
+        VALID_BODY = loadResourceAsString(FULL_D8_FORM_JSON_PATH);
+        Response forTransformationEndpoint = responseForEndpoint(token,"/transform-exception-record");
+
+        assert forTransformationEndpoint.getStatusCode() == 200 : "Service is not authorised to transform OCR data to case" + forTransformationEndpoint.getStatusCode();
+
+    }
+
+    @Test
+    public void shouldGetServiceDeniedWhenUsingNonWhitelistedServiceForTransformationEndPoint()  throws Exception {
+        String token = idamUtilsS2SAuthorization.generateUserTokenWithValidMicroService(bulkScanValidationMicroService);
+
+        VALID_BODY = loadResourceAsString(FULL_D8_FORM_JSON_PATH);
+
+        Response forTransformationEndpoint = responseForEndpoint(token,"/transform-exception-record");
+
+        assert forTransformationEndpoint.getStatusCode() == 403 : "Not matching with expected error Code " + forTransformationEndpoint.getStatusCode();
+    }
+
+    @Test
+    public void shouldGetSuccessfulResponsesWhenUsingWhitelistedServiceForUpdateEndPoint()  throws Exception {
+        String token = idamUtilsS2SAuthorization.generateUserTokenWithValidMicroService(bulkScanTransformationAndUpdateMicroService);
+
+        VALID_BODY = loadResourceAsString(FULL_D8_FORM_JSON_PATH);
+        Response forUpdateEndpoint = responseForEndpoint(token,"/update-case");
+
+        assert forUpdateEndpoint.getStatusCode() == 200 : "Service is not authorised to transform OCR data to case" + forUpdateEndpoint.getStatusCode();
+
+    }
+
+    @Test
+    public void shouldGetServiceDeniedWhenUsingNonWhitelistedServiceForUpdateEndPoint()  throws Exception {
+        String token = idamUtilsS2SAuthorization.generateUserTokenWithValidMicroService(bulkScanValidationMicroService);
+
+        VALID_BODY = loadResourceAsString(FULL_D8_FORM_JSON_PATH);
+
+        Response forUpdateEndpoint = responseForEndpoint(token,"/update-case");
+
+        assert forUpdateEndpoint.getStatusCode() == 403 : "Not matching with expected error Code " + forUpdateEndpoint.getStatusCode();
+    }
+
+    private Response responseForEndpoint(String token, String endpointName){
+
+           Response  response = SerenityRest.given()
+            .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+            .header(SERVICE_AUTHORISATION_HEADER, token)
+            .relaxedHTTPSValidation()
+            .body(VALID_BODY)
+            .post(cosBaseURL+ endpointName);
+           return response;
+    }
+    private Response responseForValidationEndpoint(String token, String endpointName, String formType){
+
+        Response  response = SerenityRest.given()
+            .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+            .header(SERVICE_AUTHORISATION_HEADER, token)
+            .relaxedHTTPSValidation()
+            .body(VALID_BODY)
+            .post(cosBaseURL+ endpointName, formType);
+        return response;
+    }
 }
