d02700a25e5847774a36ae5a99bff065fc51dbf4
mseyeni@icloud.com <marc-steevene@Ilya.kainos.com>
2019-12-17 15:13:42 +0000

    section 6, reason for divorce validation and transformation

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/functionaltest/bulk/scan/ValidationBulkScanITest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/functionaltest/bulk/scan/ValidationBulkScanITest.java
@@ -141,7 +141,9 @@ public class ValidationBulkScanITest {
                     "PetitionerSolicitorEmail is not in a valid format",
                     "D8PetitionerCorrespondencePostcode is usually 6 or 7 characters long",
                     "Mandatory field \"D8MarriageCertificateCorrect\" is missing",
-                    "D8FinancialOrder must be \"Yes\" or \"No\""
+                    "D8FinancialOrder must be \"Yes\" or \"No\"",
+                    "D8ReasonForDivorce must be \"unreasonable-behaviour\", \"adultery\", \"desertion\","
+                        + " \"separation-2-years\" or \"separation-5-years\""
                 )),
                 hasJsonPath("$.status", equalTo(WARNINGS_STATUS))
             ))
