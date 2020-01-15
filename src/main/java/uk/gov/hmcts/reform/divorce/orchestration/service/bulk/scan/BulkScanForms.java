21a379621b3fd496da900e23b6c52f29cdf9fd1a
John O'Neill <j.oneill@kainos.com>
2020-01-15 17:39:48 +0000

    BSP-120: AOS Pack 2 - 2 year separation (#653)
    * Added new validation and transformation for the AosPack2 form
    
    * BSP-120: Updated relevant tests for aos pack 2
    
    * Updated test for update bulk scan for aos pack 2
    
    * BSP-120: Added in validation for RespStatementofTruthSignedDate
    
    * Code cleanup
    
    * BSP-120: Added formA to validation endpoint whitelist and updated relevant test
    
    * BSP-120: Updated logic to only validate respStatementOfTruthSignedDate when present
    
    * Bumped dependency check version and added suppression
    
    * Removed redundant comment
    
    * BSP-120: Removed redundant dependency suppressions
    
    * BSP-120: Addressed PR comments
    
    * BSP-120: Addressed PR comments
    
    * Renamed aosPack2 form to aosPackOffline
    
    * BSP-120: Updated test json files to aosPackOffline

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/service/bulk/scan/BulkScanForms.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/service/bulk/scan/BulkScanForms.java
@@ -3,4 +3,7 @@ package uk.gov.hmcts.reform.divorce.orchestration.service.bulk.scan;
 public class BulkScanForms {
 
     public static final String D8_FORM = "d8Form";
+
+    // TODO: Confirm with Jeremy if this matches with the form name in BSP configuration spreadsheet
+    public static final String AOS_PACK_OFFLINE = "aosPackOffline";
 }
