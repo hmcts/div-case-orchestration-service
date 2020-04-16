e7b90d750892aa38546f4c3f472f034655d86efe
JackMurphy <jackmur@kainos.com>
2020-04-16 15:26:24 +0100

    Update tests

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/pay/PaymentItemTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/pay/PaymentItemTest.java
@@ -1,15 +1,18 @@
 package uk.gov.hmcts.reform.divorce.orchestration.domain.model.pay;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.fees.FeeResponse;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.fees.OrderSummary;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.*;
-import static org.hamcrest.core.Is.is;
+import static junit.framework.TestCase.assertNull;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_FEE_AMOUNT;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_FEE_CODE;
@@ -17,13 +20,13 @@ import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_FEE_D
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_FEE_VERSION;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_SOLICITOR_REFERENCE;
 
+@RunWith(MockitoJUnitRunner.class)
 public class PaymentItemTest {
 
     private static PaymentItem paymentItem = new PaymentItem();
     private static final String VALID_STRING = "150.50";
-    private static final String NULL = null;
     private static final String INVALID_STRING_WORD = "NOT_A_NUMBER";
-    private static final String INVALID_DIGIT_EXPONENT = "20e";
+    private static final String INVALID_DIGIT_EXPONENT = "e";
     private static final String INVALID_DIGIT_NEGATIVE = "-5";
 
     @InjectMocks
@@ -52,34 +55,33 @@ public class PaymentItemTest {
     @Test
     public void shouldSetCalculatedAmount_WithValidString() {
         paymentItem.setCalculatedAmount(VALID_STRING);
-        String amount = convertPoundsToPennies(VALID_STRING);
-        assertEquals(paymentItem.getCalculatedAmount(), amount);
+        assertEquals(paymentItem.getCalculatedAmount(), convertPoundsToPennies(VALID_STRING));
     }
 
     @Test
     public void shouldSetCalculatedAmount_AsNull() {
-        paymentItem.setCalculatedAmount(NULL);
-        assertEquals(paymentItem.getCalculatedAmount(), NULL);
+        paymentItem.setCalculatedAmount(null);
+        assertNull(paymentItem.getCalculatedAmount());
+    }
+
+    @Test
+    public void shouldSetCalculatedAmount_WithNegativeString() {
+        paymentItem.setCalculatedAmount(INVALID_DIGIT_NEGATIVE);
+        assertEquals(paymentItem.getCalculatedAmount(), convertPoundsToPennies(INVALID_DIGIT_NEGATIVE));
     }
 
     @Test
     public void shouldThrowException_WhenValueIsNotANumber() {
         thrown.expect(NumberFormatException.class);
-        thrown.expectMessage(containsString("Z"));
+        thrown.expectMessage(is("For input string: \"" + INVALID_STRING_WORD + "\""));
         paymentItem.setCalculatedAmount(INVALID_STRING_WORD);
     }
 
-
-    @Test(expected = NumberFormatException.class)
-    public void shouldNotSetCalculatedAmount_AsExponent() {
-        paymentItem.setCalculatedAmount(INVALID_DIGIT_EXPONENT);
-        assertThat(paymentItem.getCalculatedAmount(), is(INVALID_DIGIT_EXPONENT));
-    }
-
     @Test
-    public void shouldNotSetCalculatedAmount_WithNegativeString() {
-        paymentItem.setCalculatedAmount(INVALID_DIGIT_NEGATIVE);
-        assertNotEquals(paymentItem.getCalculatedAmount(), INVALID_DIGIT_NEGATIVE);
+    public void shouldThrowException_AsExponent() {
+        thrown.expect(NumberFormatException.class);
+        thrown.expectMessage(is("For input string: \"" + INVALID_DIGIT_EXPONENT + "\""));
+        paymentItem.setCalculatedAmount(INVALID_DIGIT_EXPONENT);
     }
 
     private String convertPoundsToPennies(String amount) {
