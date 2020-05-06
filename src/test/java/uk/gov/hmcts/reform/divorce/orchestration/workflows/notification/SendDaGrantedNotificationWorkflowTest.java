cfa38e07c085dd6b28b9f0dd5e1d5ad61dfe804d
Felix Eyetan <peyetan@yahoo.com>
2020-05-06 11:42:05 +0100

    Updated test case for SendDAGrantedNotificationWorkflow, covered more cases Deleted unused test class for newly created send da granted bulkprint task thats no longer required

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/notification/SendDaGrantedNotificationWorkflowTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/notification/SendDaGrantedNotificationWorkflowTest.java
@@ -3,23 +3,30 @@ package uk.gov.hmcts.reform.divorce.orchestration.workflows.notification;
 import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CaseDetails;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CcdCallbackRequest;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.documentgeneration.DocumentGenerationRequest;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.WorkflowException;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.CaseFormatterAddDocuments;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.FetchPrintDocsFromDmStore;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.MultipleDocumentGenerationTask;
-import uk.gov.hmcts.reform.divorce.orchestration.tasks.SendDaGrantedDocumentsToBulkPrintTask;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.SendDaGrantedNotificationEmailTask;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.bulk.printing.BulkPrinter;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNotNull;
@@ -28,6 +35,13 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AUTH_TOKEN_JSON_KEY;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_ABSOLUTE_DOCUMENT_TYPE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_ABSOLUTE_FILENAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_ABSOLUTE_LETTER_DOCUMENT_TYPE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_ABSOLUTE_LETTER_FILENAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_ABSOLUTE_LETTER_TEMPLATE_ID;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_ABSOLUTE_TEMPLATE_ID;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_GENERATION_REQUESTS_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.NO_VALUE;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.RESP_IS_USING_DIGITAL_CHANNEL;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.YES_VALUE;
@@ -39,7 +53,7 @@ public class SendDaGrantedNotificationWorkflowTest {
     private SendDaGrantedNotificationEmailTask sendDaGrantedNotificationEmail;
 
     @Mock
-    private  MultipleDocumentGenerationTask documentsGenerationTask;
+    private MultipleDocumentGenerationTask documentsGenerationTask;
 
     @Mock
     private CaseFormatterAddDocuments caseFormatterAddDocuments;
@@ -53,13 +67,13 @@ public class SendDaGrantedNotificationWorkflowTest {
     @InjectMocks
     private SendDaGrantedNotificationWorkflow sendDaGrantedNotificationWorkflow;
 
+    @Captor
+    private ArgumentCaptor<TaskContext> taskContextArgumentCaptor;
+
     @Test
     public void notifyApplicantCanFinaliseDivorceTaskIsExecuted_send_email_if_is_digital_channel() throws Exception {
-        Map<String, Object> casePayload = new HashMap<>(ImmutableMap.of(RESP_IS_USING_DIGITAL_CHANNEL, YES_VALUE));
-        final CaseDetails caseDetails = CaseDetails.builder()
-            .caseData(casePayload)
-            .build();
-
+        Map<String, Object> casePayload = getCasePayload(YES_VALUE);
+        final CaseDetails caseDetails = getCaseDetails(casePayload);
         final CcdCallbackRequest ccdCallbackRequest = CcdCallbackRequest.builder().caseDetails(caseDetails).build();
 
         when(sendDaGrantedNotificationEmail.execute(isNotNull(), eq(casePayload))).thenReturn(casePayload);
@@ -68,17 +82,14 @@ public class SendDaGrantedNotificationWorkflowTest {
 
         assertEquals(casePayload, result);
 
-        verify(sendDaGrantedNotificationEmail,times(1)).execute(any(TaskContext.class), eq(casePayload));
+        verify(sendDaGrantedNotificationEmail, times(1)).execute(any(TaskContext.class), eq(casePayload));
         verify(documentsGenerationTask, never()).execute(any(TaskContext.class), eq(casePayload));
     }
 
     @Test
     public void notifyApplicantCanFinaliseDivorceTaskIsExecuted_send_email_if_is_not_digital_channel() throws Exception {
-        Map<String, Object> casePayload = new HashMap<>(ImmutableMap.of(RESP_IS_USING_DIGITAL_CHANNEL, NO_VALUE));
-        final CaseDetails caseDetails = CaseDetails.builder()
-            .caseData(casePayload)
-            .build();
-
+        Map<String, Object> casePayload = getCasePayload(NO_VALUE);
+        final CaseDetails caseDetails = getCaseDetails(casePayload);
         final CcdCallbackRequest ccdCallbackRequest = CcdCallbackRequest.builder().caseDetails(caseDetails).build();
 
         when(documentsGenerationTask.execute(isNotNull(), eq(casePayload))).thenReturn(casePayload);
@@ -90,12 +101,49 @@ public class SendDaGrantedNotificationWorkflowTest {
 
         assertEquals(casePayload, result);
 
-        verify(sendDaGrantedNotificationEmail,never()).execute(any(TaskContext.class), eq(casePayload));
+        verify(sendDaGrantedNotificationEmail, never()).execute(any(TaskContext.class), eq(casePayload));
 
         verify(documentsGenerationTask, times(1)).execute(any(TaskContext.class), eq(casePayload));
         verify(caseFormatterAddDocuments, times(1)).execute(any(TaskContext.class), eq(casePayload));
         verify(fetchPrintDocsFromDmStore, times(1)).execute(any(TaskContext.class), eq(casePayload));
         verify(bulkPrinterTask, times(1)).execute(any(TaskContext.class), eq(casePayload));
     }
+
+    @Test
+    public void testThatDocumentsGenerationTask_is_called_with_required_documents() throws TaskException, WorkflowException {
+        Map<String, Object> casePayload = getCasePayload(NO_VALUE);
+        final CaseDetails caseDetails = getCaseDetails(casePayload);
+        final CcdCallbackRequest ccdCallbackRequest = CcdCallbackRequest.builder().caseDetails(caseDetails).build();
+
+        when(documentsGenerationTask.execute(isNotNull(), eq(casePayload))).thenReturn(casePayload);
+
+        sendDaGrantedNotificationWorkflow.run(ccdCallbackRequest.getCaseDetails(), AUTH_TOKEN_JSON_KEY);
+
+        verify(documentsGenerationTask).execute(taskContextArgumentCaptor.capture(), eq(caseDetails.getCaseData()));
+        TaskContext taskContext = taskContextArgumentCaptor.getValue();
+        List<DocumentGenerationRequest> documentGenerationRequestList = taskContext.getTransientObject(DOCUMENT_GENERATION_REQUESTS_KEY);
+
+        assertThat(documentGenerationRequestList.size(), is(2));
+
+        assertThat(documentGenerationRequestList.get(0).getDocumentTemplateId(), is(DECREE_ABSOLUTE_LETTER_TEMPLATE_ID));
+        assertThat(documentGenerationRequestList.get(0).getDocumentFileName(), is(DECREE_ABSOLUTE_LETTER_FILENAME));
+        assertThat(documentGenerationRequestList.get(0).getDocumentType(), is(DECREE_ABSOLUTE_LETTER_DOCUMENT_TYPE));
+
+        assertThat(documentGenerationRequestList.get(1).getDocumentTemplateId(), is(DECREE_ABSOLUTE_TEMPLATE_ID));
+        assertThat(documentGenerationRequestList.get(1).getDocumentFileName(), is(DECREE_ABSOLUTE_FILENAME));
+        assertThat(documentGenerationRequestList.get(1).getDocumentType(), is(DECREE_ABSOLUTE_DOCUMENT_TYPE));
+    }
+
+    private HashMap<String, Object> getCasePayload(String value) {
+        return new HashMap<>(ImmutableMap.of(RESP_IS_USING_DIGITAL_CHANNEL, value));
+    }
+
+    private CaseDetails getCaseDetails(Map<String, Object> casePayload) {
+        return CaseDetails.builder()
+            .caseData(casePayload)
+            .build();
+    }
+
+
 
 }
