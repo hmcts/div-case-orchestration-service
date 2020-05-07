aaf2eb27c9c56ee41b394354e0aad4c0c2e5b6a2
Felix Eyetan <peyetan@yahoo.com>
2020-05-07 08:11:56 +0100

    Clean up

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/bulk/print/DaGrantedLetter.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/bulk/print/DaGrantedLetter.java
@@ -4,11 +4,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.bsp.common.model.document.Addressee;
+import uk.gov.hmcts.reform.bsp.common.model.document.CtscContactDetails;
 
 @Data
-@Builder
-@EqualsAndHashCode(callSuper=false)
+@Getter
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 public class DaGrantedLetter extends DocmosisTemplateVars {
     @JsonProperty("respondentFullName")
     private String respondentFullName;
@@ -18,4 +22,19 @@ public class DaGrantedLetter extends DocmosisTemplateVars {
     private Addressee addressee;
     @JsonProperty("decreeAbsoluteDate")
     private String decreeAbsoluteDate;
+
+    @Builder
+    public DaGrantedLetter(
+        CtscContactDetails ctscContactDetails,
+        String caseReference,
+        String respondentFullName,
+        String petitionerFullName,
+        Addressee addressee,
+        String decreeAbsoluteDate) {
+        super(ctscContactDetails, caseReference);
+        this.respondentFullName = respondentFullName;
+        this.petitionerFullName = petitionerFullName;
+        this.addressee = addressee;
+        this.decreeAbsoluteDate = decreeAbsoluteDate;
+    }
 }
