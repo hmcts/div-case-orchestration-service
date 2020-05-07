2cbb9d6532a22587ed0b0cec6eee124c6c3afa05
Felix Eyetan <peyetan@yahoo.com>
2020-05-07 08:00:13 +0100

    Fixing descrepancies in file

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/bulk/print/DocmosisTemplateVars.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/bulk/print/DocmosisTemplateVars.java
@@ -1,21 +1,6 @@
 package uk.gov.hmcts.reform.divorce.orchestration.domain.model.bulk.print;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-<<<<<<< HEAD
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import uk.gov.hmcts.reform.bsp.common.model.document.CtscContactDetails;
-
-@Data
-@Builder
-@EqualsAndHashCode
-public class DocmosisTemplateVars {
-    @JsonProperty("ctscContactDetails")
-    private CtscContactDetails ctscContactDetails;
-    @JsonProperty("caseReference")
-    private String caseReference;
-=======
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,5 +14,4 @@ public class DocmosisTemplateVars {
     protected CtscContactDetails ctscContactDetails;
     @JsonProperty("caseReference")
     protected String caseReference;
->>>>>>> 7f6ad23fd0b1ed8e947eb6dbf667ccf9dc811c22
 }
