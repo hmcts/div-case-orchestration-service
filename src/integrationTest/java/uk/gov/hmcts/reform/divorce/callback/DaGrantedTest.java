42b48ce0f88540c428e160de2ffe85f839299fb7
Felix Eyetan <peyetan@yahoo.com>
2020-05-12 17:35:58 +0100

    Branch state after rebase

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/callback/DaGrantedTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/callback/DaGrantedTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.reform.divorce.context.IntegrationTest;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CcdCallbackRequest;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CcdCallbackResponse;
 import uk.gov.hmcts.reform.divorce.support.cos.CosApiClient;
 import uk.gov.hmcts.reform.divorce.util.ResourceLoader;
 
@@ -21,7 +22,7 @@ public class DaGrantedTest extends IntegrationTest {
     @Test
     public void givenValidCaseData_whenDaIsGranted_thenReturnDaGrantedData() {
         CcdCallbackRequest ccdCallbackRequest = ResourceLoader.loadJsonToObject(BASE_CASE_RESPONSE, CcdCallbackRequest.class);
-        Map<String, Object> response = cosApiClient.handleDaGranted(
+        CcdCallbackResponse response = cosApiClient.handleDaGranted(
                 createCaseWorkerUser().getAuthToken(),
                 ccdCallbackRequest);
 
