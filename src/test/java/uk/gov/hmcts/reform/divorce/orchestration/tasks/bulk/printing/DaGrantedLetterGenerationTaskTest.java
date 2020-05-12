c61d8e018ef6471ddfd398b5b5407f41648b95ae
Patryk Jar <jar.patryk@gmail.com>
2020-05-12 11:28:08 +0200

    improved unit tests for DaGrantedLetterGenerationTask

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/bulk/printing/DaGrantedLetterGenerationTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/bulk/printing/DaGrantedLetterGenerationTaskTest.java
@@ -3,10 +3,12 @@ package uk.gov.hmcts.reform.divorce.orchestration.tasks.bulk.printing;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.bsp.common.model.document.CtscContactDetails;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.bulk.print.DaGrantedLetter;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.bulk.print.DocmosisTemplateVars;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.documentgeneration.GeneratedDocumentInfo;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.DefaultTaskContext;
@@ -16,6 +18,7 @@ import uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.PdfDocumentG
 import uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.CtscContactDetailsDataProviderService;
 import uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.DaGrantedLetterDataExtractor;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -29,10 +32,22 @@ import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.AUTH_TOKEN;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AUTH_TOKEN_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_ID_JSON_KEY;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.DaGrantedLetterDataExtractor.CaseDataKeys.PETITIONER_FIRST_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.DaGrantedLetterDataExtractor.CaseDataKeys.PETITIONER_LAST_NAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.DaGrantedLetterDataExtractorTest.buildCaseDataWithAddressee;
+import static uk.gov.hmcts.reform.divorce.orchestration.tasks.bulk.printing.DaGrantedLetterGenerationTask.FileMetadata.TEMPLATE_ID;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DaGrantedLetterGenerationTaskTest {
+
+    private static final String PETITIONERS_FIRST_NAME = "Anna";
+    private static final String PETITIONERS_LAST_NAME = "Nowak";
+
+    private static final String CASE_ID = "It's mandatory field in context";
+    private static final String LETTER_DATE = LocalDate.now().toString();
+
+    private static final CtscContactDetails CTSC_CONTACT = CtscContactDetails.builder().build();
+    private static final GeneratedDocumentInfo DOCUMENT = GeneratedDocumentInfo.builder().build();
 
     @Mock
     private CtscContactDetailsDataProviderService ctscContactDetailsDataProviderService;
@@ -45,10 +60,9 @@ public class DaGrantedLetterGenerationTaskTest {
 
     @Before
     public void setup() {
-        when(ctscContactDetailsDataProviderService.getCtscContactDetails()).thenReturn(CtscContactDetails.builder().build());
-        when(pdfDocumentGenerationService.generatePdf(
-            any(DocmosisTemplateVars.class), eq(DaGrantedLetterGenerationTask.FileMetadata.TEMPLATE_ID), eq(AUTH_TOKEN))
-        ).thenReturn(GeneratedDocumentInfo.builder().build());
+        when(ctscContactDetailsDataProviderService.getCtscContactDetails()).thenReturn(CTSC_CONTACT);
+        when(pdfDocumentGenerationService.generatePdf(any(DocmosisTemplateVars.class), eq(TEMPLATE_ID), eq(AUTH_TOKEN)))
+            .thenReturn(DOCUMENT);
     }
 
     @Test
@@ -61,13 +75,27 @@ public class DaGrantedLetterGenerationTaskTest {
             .getTransientObject(PrepareDataForDocumentGenerationTask.ContextKeys.GENERATED_DOCUMENTS);
 
         assertThat(documents.size(), is(1));
+        assertThat(documents.get(0), is(DOCUMENT));
         verify(ctscContactDetailsDataProviderService, times(1)).getCtscContactDetails();
-        verify(pdfDocumentGenerationService, times(1)).generatePdf(any(), any(), any());
+        verifyPdfDocumentGenerationCallIsCorrect();
+    }
+
+    private void verifyPdfDocumentGenerationCallIsCorrect() {
+        final ArgumentCaptor<DaGrantedLetter> daGrantedLetterArgumentCaptor = ArgumentCaptor.forClass(DaGrantedLetter.class);
+        verify(pdfDocumentGenerationService, times(1))
+            .generatePdf(daGrantedLetterArgumentCaptor.capture(), eq(TEMPLATE_ID), eq(AUTH_TOKEN));
+
+        final DaGrantedLetter daGrantedLetter = daGrantedLetterArgumentCaptor.getValue();
+        assertThat(daGrantedLetter.getPetitionerFullName(), is("Anna Nowak"));
+        assertThat(daGrantedLetter.getRespondentFullName(), is("John Smith"));
+        assertThat(daGrantedLetter.getCaseReference(), is(CASE_ID));
+        assertThat(daGrantedLetter.getLetterDate(), is(LETTER_DATE));
+        assertThat(daGrantedLetter.getCtscContactDetails(), is(CTSC_CONTACT));
     }
 
     public static TaskContext prepareTaskContext() {
         TaskContext context = new DefaultTaskContext();
-        context.setTransientObject(CASE_ID_JSON_KEY, "It's mandatory field in context");
+        context.setTransientObject(CASE_ID_JSON_KEY, CASE_ID);
         context.setTransientObject(AUTH_TOKEN_JSON_KEY, AUTH_TOKEN);
 
         return context;
@@ -75,7 +103,10 @@ public class DaGrantedLetterGenerationTaskTest {
 
     private Map<String, Object> buildCaseData() {
         Map<String, Object> caseData = buildCaseDataWithAddressee();
-        caseData.put(DaGrantedLetterDataExtractor.CaseDataKeys.DA_GRANTED_DATE, "201-01-10");
+        caseData.put(DaGrantedLetterDataExtractor.CaseDataKeys.DA_GRANTED_DATE, LETTER_DATE);
+
+        caseData.put(PETITIONER_FIRST_NAME, PETITIONERS_FIRST_NAME);
+        caseData.put(PETITIONER_LAST_NAME, PETITIONERS_LAST_NAME);
 
         return caseData;
     }
