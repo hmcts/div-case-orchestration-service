7d2be2d40b41edcb2a3909544789dc8e49302763
Felix Eyetan <peyetan@yahoo.com>
2020-05-13 10:36:01 +0100

    WIP - Covering DaGranted expected senarios in Int. test

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/callback/DaGrantedTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/callback/DaGrantedTest.java
@@ -5,12 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.reform.divorce.context.IntegrationTest;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CcdCallbackRequest;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CcdCallbackResponse;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CollectionMember;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.Document;
 import uk.gov.hmcts.reform.divorce.support.cos.CosApiClient;
 import uk.gov.hmcts.reform.divorce.util.ResourceLoader;
 
+import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.assertNotNull;
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.isJson;
+import static org.junit.Assert.*;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D8DOCUMENTS_GENERATED;
 
 public class DaGrantedTest extends IntegrationTest {
 
@@ -22,11 +27,19 @@ public class DaGrantedTest extends IntegrationTest {
     @Test
     public void givenValidCaseData_whenDaIsGranted_thenReturnDaGrantedData() {
         CcdCallbackRequest ccdCallbackRequest = ResourceLoader.loadJsonToObject(BASE_CASE_RESPONSE, CcdCallbackRequest.class);
+
         CcdCallbackResponse response = cosApiClient.handleDaGranted(
-                createCaseWorkerUser().getAuthToken(),
-                ccdCallbackRequest);
+            createCaseWorkerUser().getAuthToken(),
+            ccdCallbackRequest);
 
-        System.out.println(response); //TODO ...
+        Map<String, Object> responseData = response.getData();
 
+        assertNotNull(responseData);
+        assertEquals(response.getErrors().size(), 0);
+        assertNoExtraDocumentsWhereGenerated((List) responseData.get(D8DOCUMENTS_GENERATED));
+    }
+
+    private static void assertNoExtraDocumentsWhereGenerated(List<CollectionMember<Document>> documents) {
+        assertEquals(1, documents.size());
     }
 }
