17281c06e9667d4fd1183a92322a3b1c23a6392f
gurarach <gurdeep.araich@hmcts.net>
2020-05-20 13:34:18 +0100

    Tests and controller updated. Still 1 failing test on PaymentUpdateITest

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/util/AuthUtil.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/util/AuthUtil.java
@@ -4,12 +4,21 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
+import uk.gov.hmcts.reform.bsp.common.error.ForbiddenException;
+import uk.gov.hmcts.reform.bsp.common.error.UnauthenticatedException;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
+
+import java.util.List;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 @SuppressWarnings("squid:S1118")
 @Component
 public class AuthUtil {
 
+    private final AuthTokenValidator authTokenValidator;
+    private final List<String> allowedToUpdate;
     private static final String BEARER = "Bearer ";
     private static final String AUTHORIZATION_CODE = "authorization_code";
     private static final String CODE = "code";
@@ -38,8 +47,14 @@ public class AuthUtil {
     private final IdamClient idamClient;
 
     @Autowired
-    public AuthUtil(IdamClient idamClient) {
+    public AuthUtil(
+                    IdamClient idamClient,
+                    AuthTokenValidator authTokenValidator,
+                    @Value("${idam.s2s-auth.services-allowed-to-validate}") List<String> allowedToUpdate
+    ) {
         this.idamClient = idamClient;
+        this.authTokenValidator = authTokenValidator;
+        this.allowedToUpdate = allowedToUpdate;
     }
 
     public String getCitizenToken() {
@@ -60,6 +75,22 @@ public class AuthUtil {
         }
 
         return token.startsWith(BEARER) ? token : BEARER.concat(token);
+    }
+
+    public void assertIsServiceAllowedToUpdate(String token) {
+        String serviceName = this.authenticate(token);
+
+        if (!allowedToUpdate.contains(serviceName)) {
+            throw new ForbiddenException("Service is not authorised to access this endpoint");
+        }
+    }
+
+    private String authenticate(String authHeader) {
+        if (isBlank(authHeader)) {
+            throw new UnauthenticatedException("Provided S2S token is missing or invalid");
+        }
+
+        return authTokenValidator.getServiceName(authHeader);
     }
 
 }
