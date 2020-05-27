55fdec288a3fdd8e544f5f1c1c8f3e7caf4bc339
gurarach <gurdeep.araich@hmcts.net>
2020-05-27 14:41:48 +0100

    Updated the exception handling

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/exception/UnauthenticatedException.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.divorce.orchestration.domain.model.exception;
-
-public class UnauthenticatedException extends RuntimeException {
-
-    public UnauthenticatedException(String message) {
-        super(message);
-    }
-
-}
