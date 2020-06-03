30351e2b2eaf9632066c71d7c86656e30a33b666
Patryk Jar <jar.patryk@gmail.com>
2020-06-03 15:32:52 +0200

    respondent solicitor address D8DerivedRespondentSolicitorAddr

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/service/bulk/print/dataextractor/AddresseeDataExtractorTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/service/bulk/print/dataextractor/AddresseeDataExtractorTest.java
@@ -9,14 +9,16 @@ import java.util.Map;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.AddresseeDataExtractor.CaseDataKeys.RESPONDENT_CORRESPONDENCE_ADDRESS;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.AddresseeDataExtractor.CaseDataKeys.RESPONDENT_SOLICITOR_ADDRESS;
 import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.FullNamesDataExtractorTest.FIRST_NAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.FullNamesDataExtractorTest.LAST_NAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.FullNamesDataExtractorTest.buildCaseDataWithRespondentNames;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.FullNamesDataExtractorTest.buildCaseDataWithRespondentSolicitorNames;
 
 public class AddresseeDataExtractorTest {
 
     public static String FORMATTED_ADDRESS = "this is my address\ntown\ncounty\npostcode";
-    public static String EXPECTED_RESPONDENT_NAME = FIRST_NAME + " " + LAST_NAME;
+    public static String EXPECTED_NAME = FIRST_NAME + " " + LAST_NAME;
 
     @Test
     public void getRespondentShouldReturnValidValues() {
@@ -25,7 +27,7 @@ public class AddresseeDataExtractorTest {
         Addressee addressee = AddresseeDataExtractor.getRespondent(caseData);
 
         assertThat(addressee.getFormattedAddress(), is(FORMATTED_ADDRESS));
-        assertThat(addressee.getName(), is(EXPECTED_RESPONDENT_NAME));
+        assertThat(addressee.getName(), is(EXPECTED_NAME));
     }
 
     @Test(expected = InvalidDataForTaskException.class)
@@ -35,9 +37,33 @@ public class AddresseeDataExtractorTest {
         AddresseeDataExtractor.getRespondent(caseData);
     }
 
+    @Test
+    public void getRespondentSolicitorShouldReturnValidValues() {
+        Map<String, Object> caseData = buildCaseDataWithRespondentSolicitorAsAddressee();
+
+        Addressee addressee = AddresseeDataExtractor.getRespondentSolicitor(caseData);
+
+        assertThat(addressee.getFormattedAddress(), is(FORMATTED_ADDRESS));
+        assertThat(addressee.getName(), is(EXPECTED_NAME));
+    }
+
+    @Test(expected = InvalidDataForTaskException.class)
+    public void getRespondentSolicitorShouldThrowInvalidDataForTaskException() {
+        Map<String, Object> caseData = buildCaseDataWithRespondentSolicitorNames(EXPECTED_NAME);
+
+        AddresseeDataExtractor.getRespondent(caseData);
+    }
+
     public static Map<String, Object> buildCaseDataWithRespondentAsAddressee() {
         Map<String, Object> caseData = buildCaseDataWithRespondentNames(FIRST_NAME, LAST_NAME);
         caseData.put(RESPONDENT_CORRESPONDENCE_ADDRESS, FORMATTED_ADDRESS);
+
+        return caseData;
+    }
+
+    public static Map<String, Object> buildCaseDataWithRespondentSolicitorAsAddressee() {
+        Map<String, Object> caseData = buildCaseDataWithRespondentSolicitorNames(EXPECTED_NAME);
+        caseData.put(RESPONDENT_SOLICITOR_ADDRESS, FORMATTED_ADDRESS);
 
         return caseData;
     }
