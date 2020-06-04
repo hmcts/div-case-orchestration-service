abf9e60a7934b862054dbe0435c6c8d14254f9c7
Felix Eyetan <peyetan@yahoo.com>
2020-06-04 20:19:08 +0100

    Checkstyle fix for unused imports

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/service/bulk/print/DocumentContentFetcherServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/service/bulk/print/DocumentContentFetcherServiceTest.java
@@ -18,7 +18,6 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
