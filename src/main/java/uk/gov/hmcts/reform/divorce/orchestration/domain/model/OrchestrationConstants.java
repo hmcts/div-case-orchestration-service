856942f737a6c0775325f9a788e9ac8e7e227d2d
rishikrsharma <rishikrsharma@users.noreply.github.com>
2020-06-12 11:08:21 +0100

    WLA-561 DIVORCE: added claim cost order auto stop check (#833)

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/OrchestrationConstants.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/OrchestrationConstants.java
@@ -144,6 +144,7 @@ public class OrchestrationConstants {
     public static final String DN_REFUSED_ADMIN_ERROR_OPTION = "adminError";
     public static final String DN_APPLICATION_SUBMITTED_DATE = "DNApplicationSubmittedDate";
     public static final String DN_REFUSAL_DRAFT = "DNRefusalDraft";
+    public static final String ADDITIONAL_INFRORMATION = "additionalInformation";
 
     // CCD Events
     public static final String DN_RECEIVED = "dnReceived";
