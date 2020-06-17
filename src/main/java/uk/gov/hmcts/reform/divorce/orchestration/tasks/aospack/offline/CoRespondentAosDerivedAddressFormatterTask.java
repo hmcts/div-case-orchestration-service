9467932dae1d4b0be688bd3bab1e631dbe3d6d2b
Felix Eyetan <peyetan@yahoo.com>
2020-06-17 08:23:58 +0100

    WIP - Updating test cases to add required formatted addresses

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/aospack/offline/CoRespondentAosDerivedAddressFormatterTask.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/aospack/offline/CoRespondentAosDerivedAddressFormatterTask.java
@@ -1,15 +1,44 @@
 package uk.gov.hmcts.reform.divorce.orchestration.tasks.aospack.offline;
 
+import uk.gov.hmcts.reform.bsp.common.utils.LetterAddressHelper;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
 
 import java.util.Map;
+import java.util.Optional;
+
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D8_CO_RESPONDENT_SOLICITOR_ADDRESS;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D8_REASON_FOR_DIVORCE_ADULTERY_3RD_PARTY_ADDRESS;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.AddresseeDataExtractor.CaseDataKeys.CO_RESPONDENT_ADDRESS;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.AddresseeDataExtractor.CaseDataKeys.CO_RESPONDENT_SOLICITOR_ADDRESS;
+import static uk.gov.hmcts.reform.divorce.orchestration.util.PartyRepresentationChecker.isCoRespondentRepresented;
 
 public class CoRespondentAosDerivedAddressFormatterTask implements Task<Map<String, Object>> {
 
     @Override
     public Map<String, Object> execute(TaskContext context, Map<String, Object> caseData) throws TaskException {
+
+        if (isCoRespondentRepresented(caseData)) {
+            caseData.put(CO_RESPONDENT_SOLICITOR_ADDRESS, formatDerivedCoRespondentSolicitorAddr(caseData));
+        } else {
+            caseData.put(CO_RESPONDENT_ADDRESS, formatDerivedReasonForDivorceAdultery3rdAddress(caseData));
+        }
+
         return caseData;
     }
+
+
+    String formatDerivedCoRespondentSolicitorAddr(Map<String, Object> caseData) {
+        return Optional.ofNullable((Map<String, Object>) caseData.get(D8_CO_RESPONDENT_SOLICITOR_ADDRESS))
+            .map(address -> LetterAddressHelper.formatAddressForLetterPrinting(address))
+            .orElse(null);
+    }
+
+    String formatDerivedReasonForDivorceAdultery3rdAddress(Map<String, Object> caseData) {
+        return Optional.ofNullable((Map<String, Object>) caseData.get(D8_REASON_FOR_DIVORCE_ADULTERY_3RD_PARTY_ADDRESS))
+            .map(address -> LetterAddressHelper.formatAddressForLetterPrinting(address))
+            .orElse(null);
+    }
+
 }
---
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/aospack/offline/CoRespondentAosDerivedAddressFormatterTaskTest.java
@@ -0,0 +1,132 @@
+package uk.gov.hmcts.reform.divorce.orchestration.tasks.aospack.offline;
+
+import org.junit.Before;
+import org.junit.Test;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CO_RESPONDENT_REPRESENTED;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D8_CO_RESPONDENT_SOLICITOR_ADDRESS;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D8_REASON_FOR_DIVORCE_ADULTERY_3RD_PARTY_ADDRESS;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.NO_VALUE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.YES_VALUE;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.AddresseeDataExtractor.CaseDataKeys.CO_RESPONDENT_ADDRESS;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.AddresseeDataExtractor.CaseDataKeys.CO_RESPONDENT_SOLICITOR_ADDRESS;
+
+public class CoRespondentAosDerivedAddressFormatterTaskTest {
+
+    private static final String ADDRESS_LINE_1 = "AddressLine1";
+    private static final String ADDRESS_LINE_2 = "AddressLine2";
+    private static final String ADDRESS_LINE_3 = "AddressLine3";
+    private static final String COUNTY = "County";
+    private static final String COUNTRY = "Country";
+    private static final String POST_TOWN = "PostTown";
+    private static final String POST_CODE = "PostCode";
+    private static final String ADDY_LINE_1 = "CoRespAddyLine1";
+    private static final String ADDY_LINE_2 = "CoRespAddyLine2";
+    private static final String ADDY_LINE_3 = "CoRespAddyLine3";
+    private static final String ADDY_COUNTY = "CoRespCounty";
+    private static final String ADDY_COUNTRY = "CoRespCountry";
+    private static final String ADDY_POST_TOWN = "CoRespPostTown";
+    private static final String ADDY_POSTCODE = "CoRespPostcode";
+    private static final String EXPECTED_DERIVED_ADDRESS = "CoRespAddyLine1\nCoRespAddyLine2\nCoRespAddyLine3\nCoRespCounty\n"
+        + "CoRespCountry\nCoRespPostTown\nCoRespPostcode";
+
+    private static Map<String, Object> BASE_ADDRESS;
+
+    private CoRespondentAosDerivedAddressFormatterTask classUnderTest;
+
+    @Before
+    public void setUp() {
+        classUnderTest = new CoRespondentAosDerivedAddressFormatterTask();
+        BASE_ADDRESS = new HashMap<String, Object>() {
+            {
+                put(ADDRESS_LINE_1, ADDY_LINE_1);
+                put(ADDRESS_LINE_2, ADDY_LINE_2);
+                put(ADDRESS_LINE_3, ADDY_LINE_3);
+                put(COUNTY, ADDY_COUNTY);
+                put(COUNTRY, ADDY_COUNTRY);
+                put(POST_TOWN, ADDY_POST_TOWN);
+                put(POST_CODE, ADDY_POSTCODE);
+            }
+        };
+    }
+
+    @Test
+    public void shouldReturnDataWithCoRespondentSolicitorAddress() throws TaskException {
+        Map<String, Object> caseData = buildCaseWithCoRespondentSolicitorAddress();
+
+        Map<String, Object> returnedData = classUnderTest.execute(null, caseData);
+
+        assertThat(returnedData, hasKey(CO_RESPONDENT_SOLICITOR_ADDRESS));
+        assertThat(returnedData.get(CO_RESPONDENT_SOLICITOR_ADDRESS), is(EXPECTED_DERIVED_ADDRESS));
+    }
+
+    @Test
+    public void shouldReturnNullWhenInvalidDataProvided() throws TaskException {
+        Map<String, Object> caseData = new HashMap() {
+            {
+                put(CO_RESPONDENT_REPRESENTED, YES_VALUE);
+            }
+        };
+
+        Map<String, Object> returnedData = classUnderTest.execute(null, caseData);
+
+        assertThat(returnedData, hasKey(CO_RESPONDENT_SOLICITOR_ADDRESS));
+        assertThat(returnedData.get(CO_RESPONDENT_SOLICITOR_ADDRESS), is(nullValue()));
+    }
+
+    @Test
+    public void shouldReturnDataWithCoRespondentAddress() throws TaskException {
+        Map<String, Object> caseData = buildCaseWithCoRespondentAddress();
+
+        Map<String, Object> returnedData = classUnderTest.execute(null, caseData);
+
+        assertThat(returnedData, hasKey(CO_RESPONDENT_ADDRESS));
+        assertThat(returnedData.get(CO_RESPONDENT_ADDRESS), is(EXPECTED_DERIVED_ADDRESS));
+    }
+
+    @Test
+    public void formatDerivedCoRespondentSolicitorAddr() {
+        Map<String, Object> caseData = buildCaseWithCoRespondentSolicitorAddress();
+
+        String derivedCoRespondentSolicitorAddr = classUnderTest.formatDerivedCoRespondentSolicitorAddr(caseData);
+
+        assertThat(derivedCoRespondentSolicitorAddr, is(EXPECTED_DERIVED_ADDRESS));
+
+    }
+
+    @Test
+    public void formatDerivedReasonForDivorceAdultery3rdAddress() {
+        Map<String, Object> caseData = buildCaseWithCoRespondentAddress();
+
+        String derivedCoRespondentAddr = classUnderTest.formatDerivedReasonForDivorceAdultery3rdAddress(caseData);
+
+        assertThat(derivedCoRespondentAddr, is(EXPECTED_DERIVED_ADDRESS));
+    }
+
+    private Map<String, Object> buildCaseWithCoRespondentSolicitorAddress() {
+        return new HashMap<String, Object>() {
+            {
+                put(D8_CO_RESPONDENT_SOLICITOR_ADDRESS, BASE_ADDRESS);
+                put(CO_RESPONDENT_REPRESENTED, YES_VALUE);
+            }
+        };
+    }
+
+    private Map<String, Object> buildCaseWithCoRespondentAddress() {
+        return new HashMap<String, Object>() {
+            {
+                put(D8_REASON_FOR_DIVORCE_ADULTERY_3RD_PARTY_ADDRESS, BASE_ADDRESS);
+                put(CO_RESPONDENT_REPRESENTED, NO_VALUE);
+            }
+        };
+    }
+
+}
