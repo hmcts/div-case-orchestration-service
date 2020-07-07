60e410959e22104c984aed755f3adbc4153d766c
vianvi <30834324+vianvi@users.noreply.github.com>
2020-07-07 09:00:24 +0100

    fail only high alerts

--- security.sh
+++ security.sh
@@ -2,8 +2,8 @@
 echo ${TEST_URL}
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001
+zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL
 cat zap.out
-curl  http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 cp *.* functional-output/
 zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l High --exit-code False
