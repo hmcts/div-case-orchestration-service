11a1c5a76b1785f7a95eb6e98ac2b4bc2252c01a
J. Victor Ferreira <victor.ferreira@hmcts.net>
2020-07-07 17:56:18 +0100

    Feature-toggling this feature

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/SetOrderSummaryWorkflow.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/SetOrderSummaryWorkflow.java
@@ -5,12 +5,14 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.DefaultWorkflow;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.WorkflowException;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
+import uk.gov.hmcts.reform.divorce.orchestration.service.FeatureToggleService;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.GetAmendPetitionFeeTask;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.GetPetitionIssueFeeTask;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.SetOrderSummary;
 
 import java.util.Map;
 
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.Features.SOLICITOR_DN_REJECT_AND_AMEND;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.PREVIOUS_CASE_ID_CCD_KEY;
 
 @Component
@@ -19,18 +21,22 @@ public class SetOrderSummaryWorkflow extends DefaultWorkflow<Map<String, Object>
     private final Task<?>[] issueTasks;
     private final Task<?>[] amendmentTasks;
 
+    private final FeatureToggleService featureToggleService;
+
     @Autowired
     public SetOrderSummaryWorkflow(GetPetitionIssueFeeTask getPetitionIssueFeeTask,
                                    GetAmendPetitionFeeTask getAmendPetitionFeeTask,
-                                   SetOrderSummary setOrderSummary) {
+                                   SetOrderSummary setOrderSummary,
+                                   FeatureToggleService featureToggleService) {
         issueTasks = new Task<?>[] {getPetitionIssueFeeTask, setOrderSummary};
         amendmentTasks = new Task<?>[] {getAmendPetitionFeeTask, setOrderSummary};
+        this.featureToggleService = featureToggleService;
     }
 
     public Map<String, Object> run(Map<String, Object> payload) throws WorkflowException {
         Task<?>[] tasks;
 
-        if (payload.containsKey(PREVIOUS_CASE_ID_CCD_KEY)) {
+        if (featureToggleService.isFeatureEnabled(SOLICITOR_DN_REJECT_AND_AMEND) && payload.containsKey(PREVIOUS_CASE_ID_CCD_KEY)) {
             tasks = amendmentTasks;
         } else {
             tasks = issueTasks;
--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/SetOrderSummaryTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/SetOrderSummaryTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.fees.FeeItem;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.fees.FeeResponse;
@@ -12,19 +13,24 @@ import uk.gov.hmcts.reform.divorce.orchestration.domain.model.fees.OrderSummary;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.DefaultTaskContext;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
+import uk.gov.hmcts.reform.divorce.orchestration.service.FeatureToggleService;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_FEE_AMOUNT;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_FEE_CODE;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_FEE_DESCRIPTION;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_FEE_VERSION;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.Features.SOLICITOR_DN_REJECT_AND_AMEND;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.PETITION_FEE_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.PETITION_ISSUE_ORDER_SUMMARY_JSON_KEY;
 
@@ -32,6 +38,9 @@ import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.Orchestrati
 public class SetOrderSummaryTest {
 
     private static final String SOL_APPLICATION_FEE_IN_POUNDS = "solApplicationFeeInPounds";
+
+    @Mock
+    private FeatureToggleService featureToggleService;
 
     @InjectMocks
     SetOrderSummary setOrderSummary;
@@ -46,7 +55,8 @@ public class SetOrderSummaryTest {
     }
 
     @Test
-    public void shouldReturnFeeInformation() throws Exception {
+    public void shouldReturnFeeInformation_WhenFeatureSwitchedOn() throws Exception {
+        when(featureToggleService.isFeatureEnabled(SOLICITOR_DN_REJECT_AND_AMEND)).thenReturn(true);
         FeeResponse feeResponse = FeeResponse.builder()
             .amount(TEST_FEE_AMOUNT)
             .feeCode(TEST_FEE_CODE)
@@ -70,7 +80,8 @@ public class SetOrderSummaryTest {
     }
 
     @Test
-    public void shouldReturnFeeInformation_WhenFeeHasPenceValue() throws Exception {
+    public void shouldReturnFeeInformation_WhenFeeHasPenceValue_WhenFeatureSwitchedOn() throws Exception {
+        when(featureToggleService.isFeatureEnabled(SOLICITOR_DN_REJECT_AND_AMEND)).thenReturn(true);
         FeeResponse feeResponse = FeeResponse.builder()
             .amount(85.43)
             .feeCode(TEST_FEE_CODE)
@@ -91,6 +102,31 @@ public class SetOrderSummaryTest {
         assertThat(fee.getFeeDescription(), is(TEST_FEE_DESCRIPTION));
         assertThat(fee.getFeeAmount(), is("8543"));
         assertThat(returnedCaseData, hasEntry(SOL_APPLICATION_FEE_IN_POUNDS, "85.43"));
+    }
+
+    @Test
+    public void shouldReturnFeeInformation_WhenFeatureSwitchedOff() throws Exception {
+        when(featureToggleService.isFeatureEnabled(SOLICITOR_DN_REJECT_AND_AMEND)).thenReturn(false);
+        FeeResponse feeResponse = FeeResponse.builder()
+            .amount(TEST_FEE_AMOUNT)
+            .feeCode(TEST_FEE_CODE)
+            .version(TEST_FEE_VERSION)
+            .description(TEST_FEE_DESCRIPTION)
+            .build();
+        context.setTransientObject(PETITION_FEE_JSON_KEY, feeResponse);
+
+        Map<String, Object> returnedCaseData = setOrderSummary.execute(context, testData);
+
+        OrderSummary returnedOrderSummary = (OrderSummary) returnedCaseData.get(PETITION_ISSUE_ORDER_SUMMARY_JSON_KEY);
+        assertThat(returnedOrderSummary.getPaymentTotal(), is("55000"));
+        List<FeeItem> fees = returnedOrderSummary.getFees();
+        assertThat(fees, hasSize(1));
+        FeeValue fee = fees.get(0).getValue();
+        assertThat(fee.getFeeCode(), is(TEST_FEE_CODE));
+        assertThat(fee.getFeeVersion(), is(TEST_FEE_VERSION.toString()));
+        assertThat(fee.getFeeDescription(), is(TEST_FEE_DESCRIPTION));
+        assertThat(fee.getFeeAmount(), is("55000"));
+        assertThat(returnedCaseData, not(hasKey(SOL_APPLICATION_FEE_IN_POUNDS)));
     }
 
     @Test(expected = TaskException.class)
