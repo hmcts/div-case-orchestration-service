8778a574fc821d338bc42acd23976b2a59f4235d
Patryk Jar <jar.patryk@gmail.com>
2020-07-22 14:36:44 +0200

    DIV-6431: added @RequiredArgsConstructor to ReceivedServiceAddedDateTask class

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/ReceivedServiceAddedDateTask.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/ReceivedServiceAddedDateTask.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.divorce.orchestration.tasks;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
@@ -9,6 +10,7 @@ import java.time.LocalDate;
 import java.util.Map;
 
 @Component
+@RequiredArgsConstructor
 public class ReceivedServiceAddedDateTask implements Task<Map<String, Object>> {
 
     public static final String RECEIVED_SERVICE_ADDED_DATE = "ReceivedServiceAddedDate";
