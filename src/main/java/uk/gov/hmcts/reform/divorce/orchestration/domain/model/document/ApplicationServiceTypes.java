ba2a068103f632f3b3267fab8c0a9e64a58797b1
Patryk Jar <jar.patryk@gmail.com>
2020-08-10 12:22:24 +0200

    DIV-6237: fixed typo 'demeed' -> 'deemed', and add better logs

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/document/ApplicationServiceTypes.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/document/ApplicationServiceTypes.java
@@ -6,5 +6,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApplicationServiceTypes {
     public static final String DISPENSED = "dispensed";
-    public static final String DEEMED = "demeed";
+    public static final String DEEMED = "deemed";
 }
