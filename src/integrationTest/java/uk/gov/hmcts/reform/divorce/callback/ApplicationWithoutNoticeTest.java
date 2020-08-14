c6533905eddededb57fcedee41ac2ee74c7bac11
Patryk Jar <jar.patryk@gmail.com>
2020-08-14 12:55:18 +0200

    refactoring

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/callback/ApplicationWithoutNoticeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/callback/ApplicationWithoutNoticeTest.java
@@ -10,8 +10,10 @@ import uk.gov.hmcts.reform.divorce.support.CcdSubmissionSupport;
 import uk.gov.hmcts.reform.divorce.support.cos.CosApiClient;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
+import static java.lang.String.format;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -19,6 +21,9 @@ import static org.junit.Assert.assertThat;
 
 @Slf4j
 public class ApplicationWithoutNoticeTest extends CcdSubmissionSupport {
+
+    public static final String EXPECTED_AMOUNT = "5000";
+    public static final String EXPECTED_FEE_CODE = "FEE0228";
 
     @Autowired
     private CosApiClient cosApiClient;
@@ -35,26 +40,20 @@ public class ApplicationWithoutNoticeTest extends CcdSubmissionSupport {
         assertNotNull(response);
         assertNull(response.getErrors());
         assertNull(response.getWarnings());
-        assertFeeCodeIsCorrect(response);
-        assertFeeAmountIsCorrect(response);
+        assertFeeCodeIsCorrect(response.getData());
+        assertFeeAmountIsCorrect(response.getData());
     }
 
-    private static void assertFeeAmountIsCorrect(CcdCallbackResponse response) {
-        assertThat(
-            response.getData(),
-            hasJsonPath(
-                "$.generalApplicationWithoutNoticeFeeSummary.Fees[0].value.FeeAmount",
-                is("5000")
-            )
-        );
+    private static void assertFeeCodeIsCorrect(Map<String, Object> caseData) {
+        assertValue(caseData, "FeeCode", EXPECTED_FEE_CODE);
     }
 
-    private static void assertFeeCodeIsCorrect(CcdCallbackResponse response) {
-        assertThat(
-            response.getData(),
-            hasJsonPath(
-                "$.generalApplicationWithoutNoticeFeeSummary.Fees[0].value.FeeCode",
-                is("FEE0228")
-            ));
+    private static void assertFeeAmountIsCorrect(Map<String, Object> caseData) {
+        assertValue(caseData, "FeeAmount", EXPECTED_AMOUNT);
+    }
+
+    private static void assertValue(Map<String, Object> caseData, String field, String value) {
+        String jsonPath = format("$.generalApplicationWithoutNoticeFeeSummary.Fees[0].value.%s", field);
+        assertThat(caseData, hasJsonPath(jsonPath, is(value)));
     }
 }
