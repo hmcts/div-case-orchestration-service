2f22e9fb6d3ed6ce8bbcc541455dc4837054c80a
Felix Eyetan <peyetan@yahoo.com>
2020-08-16 14:16:04 +0100

    Refactoring test code

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/servicejourney/ServiceRefusalDraftRemovalTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/servicejourney/ServiceRefusalDraftRemovalTest.java
@@ -13,6 +13,7 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasKey;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.CcdFields.SERVICE_REFUSAL_DRAFT;
+import static uk.gov.hmcts.reform.divorce.orchestration.tasks.bulk.printing.BulkPrintTestData.prepareTaskContext;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ServiceRefusalDraftRemovalTest {
@@ -27,7 +28,7 @@ public class ServiceRefusalDraftRemovalTest {
         caseData.put("incomingKey", "incomingValue");
         caseData.put(SERVICE_REFUSAL_DRAFT, getDocumentLink());
 
-        Map<String, Object> returnedPayload = classUnderTest.execute(null, caseData);
+        Map<String, Object> returnedPayload = classUnderTest.execute(prepareTaskContext(), caseData);
 
         assertThat(returnedPayload, hasKey("incomingKey"));
         assertThat(returnedPayload, not(hasKey(SERVICE_REFUSAL_DRAFT)));
