39de2ed69e66fe7ad3e636c44dc340638f674b9a
Felix Eyetan <peyetan@yahoo.com>
2020-08-17 18:42:31 +0100

    Fixing code indentation

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/servicejourney/DispensedServiceRefusalOrderTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/servicejourney/DispensedServiceRefusalOrderTaskTest.java
@@ -19,6 +19,6 @@ public class DispensedServiceRefusalOrderTaskTest extends ServiceRefusalOrderGen
 
     @Test
     public void testExecuteShouldGenerateAFile() throws TaskException {
-       executeShouldGenerateAFile();
+        executeShouldGenerateAFile();
     }
 }
