953f2d10aa784605af04e723d129f58417bf1b40
David Gimelle <davidgimelle1@hmcts.net>
2020-08-26 15:01:23 +0100

    Remove Migration Test

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/util/FlywayNoOpStrategy.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/util/FlywayNoOpStrategy.java
@@ -1,12 +1,10 @@
 package uk.gov.hmcts.reform.divorce.orchestration.util;
 
-import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 
 import java.util.stream.Stream;
 
-@Slf4j
 public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
 
     @Override
@@ -15,12 +13,6 @@ public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
             .filter(info -> !info.getState().isApplied())
             .findFirst()
             .ifPresent(info -> {
-                log.warn("info.getDescription: {}", info.getDescription());
-                log.warn("info.getVersion: {}", info.getVersion());
-                log.warn("info.getType: {}", info.getType());
-                log.warn("info.getState: {}", info.getState());
-                log.warn("info.getInstalledOn: {}", info.getInstalledOn());
-                log.warn("info.getInstalledRank: {}", info.getInstalledRank());
             });
     }
 }
