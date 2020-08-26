fb52becbed409cfcc85212eb594bd54a779a90cf
David Gimelle <davidgimelle1@hmcts.net>
2020-08-26 13:38:59 +0100

    Simplify changes

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/util/FlywayNoOpStrategy.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/util/FlywayNoOpStrategy.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.reform.divorce.orchestration.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 
 import java.util.stream.Stream;
 
+@Slf4j
 public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
 
     @Override
@@ -13,7 +15,14 @@ public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
             .filter(info -> !info.getState().isApplied())
             .findFirst()
             .ifPresent(info -> {
+                log.warn("info.getDescription: {}", info.getDescription());
+                log.warn("info.getVersion: {}", info.getVersion());
+                log.warn("info.getType: {}", info.getType());
+                log.warn("info.getState: {}", info.getState());
+                log.warn("info.getInstalledOn: {}", info.getInstalledOn());
+                log.warn("info.getInstalledRank: {}", info.getInstalledRank());
                 throw new IllegalStateException("Newer version of script not applied " + info.getScript());
             });
     }
 }
+
