48234f4f293729180665a1b816251a5bbc86d0e4
TSeelig <thomas.seelig@HMCTS.NET>
2020-08-28 09:58:46 +0100

    removed unused logger

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/util/FlywayNoOpStrategy.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/util/FlywayNoOpStrategy.java
@@ -1,12 +1,10 @@
 package uk.gov.hmcts.reform.divorce.orchestration.util;
 
-import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 
 import java.util.stream.Stream;
 
-@Slf4j
 public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
 
     @Override
