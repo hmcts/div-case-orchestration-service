dc8c19578832599e24b3e4fdf518c998e31d7164
J. Victor Ferreira <victor.ferreira@hmcts.net>
2020-09-03 15:06:52 +0100

    Adding debug logs

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/util/FlywayNoOpStrategy.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/util/FlywayNoOpStrategy.java
@@ -1,14 +1,21 @@
 package uk.gov.hmcts.reform.divorce.orchestration.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
+@Slf4j
 public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
 
     @Override
     public void migrate(Flyway flyway) {
+        log.warn("these are my schemas...");
+        Arrays.stream(flyway.getConfiguration().getSchemas()).forEach(log::warn);
+        log.warn("done with schemas...");
+
         Stream.of(flyway.info().all())
             .filter(info -> !info.getState().isApplied())
             .findFirst()
