831c4e975a826c0e98f958ae3d76f656620fe701
Patryk Jar <jar.patryk@gmail.com>
2020-09-07 11:46:38 +0200

    DIV-6142: spring-functional tests

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/generalorders/GeneralOrderGenerationTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/generalorders/GeneralOrderGenerationTaskTest.java
@@ -14,9 +14,9 @@ import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskExc
 import java.util.Collections;
 import java.util.Map;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
