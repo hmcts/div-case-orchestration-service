73d8102a9de77f72948332e9c0d7d2ad098e04b0
Patryk Jar <jar.patryk@gmail.com>
2020-09-10 17:27:55 +0200

    removed unused imports

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/framework/workflow/task/generics/FieldsRemovalTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/framework/workflow/task/generics/FieldsRemovalTaskTest.java
@@ -13,7 +13,6 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static uk.gov.hmcts.reform.divorce.orchestration.testutil.TaskContextHelper.context;
-import static uk.gov.hmcts.reform.divorce.orchestration.testutil.TaskContextHelper.contextWithToken;
 
 public class FieldsRemovalTaskTest {
 
