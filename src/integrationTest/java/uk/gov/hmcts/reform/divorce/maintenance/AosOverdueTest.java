6a5fa7f9a145516f8b9d86ad8e12f8ad8c4708c0
Patryk Jar <jar.patryk@gmail.com>
2020-09-21 17:12:30 +0200

    More time for elastic search

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/maintenance/AosOverdueTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/maintenance/AosOverdueTest.java
@@ -58,7 +58,7 @@ public class AosOverdueTest extends RetrieveCaseSupport {
             .then()
             .statusCode(HttpStatus.SC_OK);
 
-        await().atMost(60, SECONDS).untilAsserted(() -> {
+        await().atMost(120, SECONDS).untilAsserted(() -> {
             CaseDetails caseDetails = retrieveCase(citizenUser, caseId);
             String state = caseDetails.getState();
             assertThat(state, is(AOS_OVERDUE));
