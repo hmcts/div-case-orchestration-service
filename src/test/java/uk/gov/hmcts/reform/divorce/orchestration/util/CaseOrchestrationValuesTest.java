1d8a8ab0539805eca0b9bbb7b0d75608be9f8fb9
J. Victor Ferreira <victorf@kainos.com>
2020-09-22 15:16:56 +0100

    DIV-6543: Making AOS Overdue grace period configurable (#1020)
    * DIV-6543: Making AOS Overdue grace period configurable
    
    * All my tests are now passing
    
    * Setting up preview environment for demo and tests
    
    * Checkstyle issues
    
    * Fixing preview environment variables.
    
    * Making integration test's error message better.
    
    * Adding logs to tell us the grace period set in the environment
    
    * Disabling job in PR deployment
    
    Co-authored-by: J. Victor Ferreira <victor.ferreira@hmcts.net>

---
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/util/CaseOrchestrationValuesTest.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.divorce.orchestration.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CaseOrchestrationValuesTest {
+
+    @Autowired
+    private CaseOrchestrationValues caseOrchestrationValues;
+
+    @Test
+    public void shouldLoadDefaultValues() {
+        assertThat(caseOrchestrationValues.getAosOverdueGracePeriod(), is("29"));
+    }
+
+}
