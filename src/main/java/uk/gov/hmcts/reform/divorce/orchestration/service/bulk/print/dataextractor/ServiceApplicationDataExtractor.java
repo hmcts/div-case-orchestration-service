e17eeebceebdd6c250f3eea217ae3506b28582e4
Patryk Jar <jar.patryk@gmail.com>
2020-10-07 11:06:28 +0200

    defined a set of fields used by data extractor

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/service/bulk/print/dataextractor/ServiceApplicationDataExtractor.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/service/bulk/print/dataextractor/ServiceApplicationDataExtractor.java
@@ -2,32 +2,36 @@ package uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextract
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.CcdFields;
 
 import java.util.Map;
 
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.CcdFields.SERVICE_APPLICATION_GRANTED;
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.CcdFields.SERVICE_APPLICATION_REFUSAL_REASON;
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.CcdFields.SERVICE_APPLICATION_TYPE;
 import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.helper.ExtractorHelper.getMandatoryStringValue;
 import static uk.gov.hmcts.reform.divorce.orchestration.tasks.util.TaskUtils.getOptionalPropertyValueAsString;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ServiceApplicationDataExtractor {
 
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class CaseDataKeys {
+        public static final String REFUSAL_REASON = CcdFields.SERVICE_APPLICATION_REFUSAL_REASON;
+        public static final String SERVICE_APPLICATION_GRANTED = CcdFields.SERVICE_APPLICATION_GRANTED;
+        public static final String SERVICE_APPLICATION_TYPE = CcdFields.SERVICE_APPLICATION_TYPE;
+    }
+
     public static String getServiceApplicationRefusalReason(Map<String, Object> caseData) {
-        return getMandatoryStringValue(caseData, SERVICE_APPLICATION_REFUSAL_REASON);
+        return getMandatoryStringValue(caseData, CaseDataKeys.REFUSAL_REASON);
     }
 
     public static String getServiceApplicationRefusalReasonOrEmpty(Map<String, Object> caseData) {
-        return getOptionalPropertyValueAsString(caseData, SERVICE_APPLICATION_REFUSAL_REASON, "");
+        return getOptionalPropertyValueAsString(caseData, CaseDataKeys.REFUSAL_REASON, "");
     }
 
     public static String getServiceApplicationGranted(Map<String, Object> caseData) {
-        return getMandatoryStringValue(caseData, SERVICE_APPLICATION_GRANTED);
+        return getMandatoryStringValue(caseData, CaseDataKeys.SERVICE_APPLICATION_GRANTED);
     }
 
     public static String getServiceApplicationType(Map<String, Object> caseData) {
-        return getMandatoryStringValue(caseData, SERVICE_APPLICATION_TYPE);
+        return getMandatoryStringValue(caseData, CaseDataKeys.SERVICE_APPLICATION_TYPE);
     }
-
 }
