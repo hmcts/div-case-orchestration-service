59a9e1282bcc674c17cb66e21577f20a10bade92
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2020-10-13 09:37:39 +0100

    TA-100 Added Test and json for happy Path.

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/callback/MakeCaseEligibleForDATest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/callback/MakeCaseEligibleForDATest.java
@@ -13,19 +13,20 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.fail;
 
 
 public class MakeCaseEligibleForDATest extends IntegrationTest {
 
-    // TODO Change this file.
-    private static final String CCD_CALLBACK_REQUEST = "fixtures/callback/dn-decision-made.json";
+    private static final String CCD_CALLBACK_REQUEST_DECISION_MADE = "fixtures/callback/dn-decision-made.json";
+    private static final String CCD_CALLBACK_REQUEST_CASE_ELIGIBLE_FOR_DA_SUBMITTED  = "fixtures/callback/case-eligible-for-da-submitted";
 
     @Autowired
     private CosApiClient cosApiClient;
 
     @Test
-    public void givenCcdCallbackRequest_whenMakeCaseEligibleForDASubmitted_thenErrorShouldBeRaised() {
-        final CcdCallbackRequest ccdCallbackRequest = ResourceLoader.loadJsonToObject(CCD_CALLBACK_REQUEST, CcdCallbackRequest.class);
+    public void givenCcdCallbackRequest_whenMakeCaseEligibleForDASubmitted_thenErrorsShouldBeRaised() {
+        final CcdCallbackRequest ccdCallbackRequest = ResourceLoader.loadJsonToObject(CCD_CALLBACK_REQUEST_DECISION_MADE, CcdCallbackRequest.class);
         CcdCallbackResponse response = null ;
         try {
             response = cosApiClient.handleMakeCaseEligibleForDASubmitted(ccdCallbackRequest);
@@ -35,4 +36,17 @@ public class MakeCaseEligibleForDATest extends IntegrationTest {
         }
     }
 
+    @Test
+    public void givenCcdCallbackRequest_whenMakeCaseEligibleForDASubmitted_isTriggered_shouldReturnSuccess() {
+        final CcdCallbackRequest ccdCallbackRequest = ResourceLoader
+            .loadJsonToObject(CCD_CALLBACK_REQUEST_CASE_ELIGIBLE_FOR_DA_SUBMITTED, CcdCallbackRequest.class);
+        CcdCallbackResponse response = null ;
+        try {
+            response = cosApiClient.handleMakeCaseEligibleForDASubmitted(ccdCallbackRequest);
+            assertThat(response.getErrors(), is(nullValue()));
+            assertThat(response.getData(), is(notNullValue()));
+        } catch (FeignException e) {
+            fail(e.contentUTF8());
+        }
+    }
 }
