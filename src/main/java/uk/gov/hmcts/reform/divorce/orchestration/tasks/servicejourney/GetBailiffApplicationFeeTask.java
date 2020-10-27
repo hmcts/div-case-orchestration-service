6168b4de706345012f449b1efe9efe22f64a0427
Owen Carleton <owenc@kainos.com>
2020-10-27 11:52:49 +0000

    addng bailiff task

---
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/servicejourney/GetBailiffApplicationFeeTask.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.reform.divorce.orchestration.tasks.servicejourney;
+
+import uk.gov.hmcts.reform.divorce.orchestration.client.FeesAndPaymentsClient;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.fees.OrderSummary;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.generics.FeeLookupWithoutNoticeTask;
+
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.CcdFields.GENERAL_APPLICATION_WITHOUT_NOTICE_FEE_SUMMARY;
+
+public class GetBailiffApplicationFeeTask extends FeeLookupWithoutNoticeTask {
+
+    public GetBailiffApplicationFeeTask(FeesAndPaymentsClient feesAndPaymentsClient) {
+        super(feesAndPaymentsClient);
+    }
+
+    @Override
+    public String getFieldName() {
+        return GENERAL_APPLICATION_WITHOUT_NOTICE_FEE_SUMMARY;
+    }
+
+    @Override
+    protected OrderSummary getOrderSummary() {
+        OrderSummary orderSummary = new OrderSummary();
+        orderSummary.add(feesAndPaymentsClient.getBailiffApplicationFee());
+
+        return orderSummary;
+    }
+}
