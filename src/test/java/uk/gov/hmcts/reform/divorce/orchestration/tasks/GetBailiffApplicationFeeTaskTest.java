4c0987f56a5a71fae8dc3098e458f1cdc17e6017
Owen Carleton <12084873+ocarleton@users.noreply.github.com>
2020-10-30 12:07:14 +0000

    DIV-6536: getting bailiff fee if bailiff service type (#1071)
    * getting bailiff fee if bailiff service type and adding to order summary
    
    Co-authored-by: Owen Carleton <owenc@kainos.com>

---
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/GetBailiffApplicationFeeTaskTest.java
@@ -0,0 +1,63 @@
+package uk.gov.hmcts.reform.divorce.orchestration.tasks;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.divorce.orchestration.client.FeesAndPaymentsClient;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.fees.FeeResponse;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.fees.OrderSummary;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
+import uk.gov.hmcts.reform.divorce.orchestration.tasks.servicejourney.GetBailiffApplicationFeeTask;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_FEE_AMOUNT;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_FEE_CODE;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_FEE_DESCRIPTION;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_FEE_VERSION;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.CcdFields.GENERAL_APPLICATION_WITHOUT_NOTICE_FEE_SUMMARY;
+import static uk.gov.hmcts.reform.divorce.orchestration.testutil.TaskContextHelper.context;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GetBailiffApplicationFeeTaskTest {
+
+    @Mock
+    FeesAndPaymentsClient feesAndPaymentsClient;
+
+    @InjectMocks
+    GetBailiffApplicationFeeTask getBailiffApplicationFeeTask;
+
+    private Map<String, Object> testData;
+    private TaskContext context;
+
+    @Before
+    public void setup() {
+        testData = new HashMap<>();
+    }
+
+    @Test
+    public void executeShouldCallCaseMaintenanceClientSubmitEndpoint() {
+        OrderSummary summary = new OrderSummary();
+        FeeResponse feeResponse = FeeResponse.builder()
+            .amount(TEST_FEE_AMOUNT)
+            .feeCode(TEST_FEE_CODE)
+            .version(TEST_FEE_VERSION)
+            .description(TEST_FEE_DESCRIPTION)
+            .build();
+        summary.add(feeResponse);
+
+        when(feesAndPaymentsClient.getBailiffApplicationFee()).thenReturn(feeResponse);
+
+        assertEquals(testData, getBailiffApplicationFeeTask.execute(context(), testData));
+        assertEquals(summary, testData.get(GENERAL_APPLICATION_WITHOUT_NOTICE_FEE_SUMMARY));
+
+        verify(feesAndPaymentsClient).getBailiffApplicationFee();
+    }
+}
