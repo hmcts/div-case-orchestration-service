ebe7d5cfa550fc99664752be0e2401adc8ba7634
mseyeni@icloud.com <marc-steevene@Ilya.local>
2020-11-03 09:37:52 +0000

    adding tests

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/generalreferral/GeneralReferralSetPreviousCaseStateTask.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/generalreferral/GeneralReferralSetPreviousCaseStateTask.java
@@ -6,22 +6,22 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.CcdFields;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
-import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
 
 import java.util.Map;
 
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_STATE_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.tasks.util.TaskUtils.getCaseId;
+import static uk.gov.hmcts.reform.divorce.orchestration.tasks.util.TaskUtils.getMandatoryContextValue;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public abstract class GeneralReferralSetPreviousCaseStateTask implements Task<Map<String, Object>> {
+public class GeneralReferralSetPreviousCaseStateTask implements Task<Map<String, Object>> {
 
     @Override
-    public Map<String, Object> execute(TaskContext context, Map<String, Object> payload) throws TaskException {
+    public Map<String, Object> execute(TaskContext context, Map<String, Object> payload) {
         final String caseId = getCaseId(context);
-        final String caseState = context.getTransientObject(CASE_STATE_JSON_KEY);
+        final String caseState = getMandatoryContextValue(CASE_STATE_JSON_KEY, context);
 
         log.info("CaseID: {} setting up previous case state to {}", caseId, caseState);
 
