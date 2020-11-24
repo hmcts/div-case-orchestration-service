8167e22ed1f754bc2864a48f394424019f9925f9
JackMurphy <jackmur@kainos.com>
2020-11-24 10:34:02 +0000

    Add MarkJourneyAsServedByAlternativeMethodTask

---
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/alternativeservice/MarkJourneyAsServedByAlternativeMethodTaskTest.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.divorce.orchestration.tasks.alternativeservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.CcdFields;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.YES_VALUE;
+import static uk.gov.hmcts.reform.divorce.orchestration.testutil.TaskContextHelper.context;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MarkJourneyAsServedByAlternativeMethodTaskTest {
+
+    @InjectMocks
+    private MarkJourneyAsServedByAlternativeMethodTask markJourneyAsServedByAlternativeMethodTask;
+
+    @Test
+    public void whenExecute_thenServedByProcessServerIsSetToYes() {
+        Map<String, Object> returnedPayload = markJourneyAsServedByAlternativeMethodTask.execute(context(), new HashMap<>());
+
+        assertThat(returnedPayload, hasEntry(CcdFields.SERVED_BY_ALTERNATIVE_METHOD, YES_VALUE));
+    }
+}
