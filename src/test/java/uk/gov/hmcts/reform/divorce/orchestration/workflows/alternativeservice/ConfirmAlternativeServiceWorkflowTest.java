a96dafb69df4123191b2cb87f0a099e69ac7cb60
mseyeni@icloud.com <marc-steevene@Ilya.local>
2020-11-25 12:03:26 +0000

    DIV-6642: Flag for when Confirm alternative method

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/alternativeservice/ConfirmAlternativeServiceWorkflowTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/alternativeservice/ConfirmAlternativeServiceWorkflowTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CaseDetails;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.alternativeservice.AlternativeServiceDueDateSetterTask;
+import uk.gov.hmcts.reform.divorce.orchestration.tasks.alternativeservice.MarkJourneyAsServedByAlternativeMethodTask;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,7 +15,7 @@ import java.util.Map;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static uk.gov.hmcts.reform.divorce.orchestration.testutil.Verificators.mockTasksExecution;
-import static uk.gov.hmcts.reform.divorce.orchestration.testutil.Verificators.verifyTaskWasCalled;
+import static uk.gov.hmcts.reform.divorce.orchestration.testutil.Verificators.verifyTasksCalledInOrder;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ConfirmAlternativeServiceWorkflowTest {
@@ -22,13 +23,16 @@ public class ConfirmAlternativeServiceWorkflowTest {
     @Mock
     private AlternativeServiceDueDateSetterTask alternativeServiceDueDateSetterTask;
 
+    @Mock
+    private MarkJourneyAsServedByAlternativeMethodTask markJourneyAsServedByAlternativeMethodTask;
+
     @InjectMocks
     private ConfirmAlternativeServiceWorkflow confirmAlternativeServiceWorkflow;
 
     @Test
     public void whenConfirmAlternativeServiceWorkflowModifyDueDateTaskIsExecuted() throws Exception {
         HashMap<String, Object> caseData = new HashMap<>();
-        mockTasksExecution(caseData, alternativeServiceDueDateSetterTask);
+        mockTasksExecution(caseData, alternativeServiceDueDateSetterTask, markJourneyAsServedByAlternativeMethodTask);
 
         Map<String, Object> returned = confirmAlternativeServiceWorkflow.run(
             CaseDetails.builder()
@@ -37,6 +41,6 @@ public class ConfirmAlternativeServiceWorkflowTest {
         );
 
         assertThat(returned, is(caseData));
-        verifyTaskWasCalled(caseData, alternativeServiceDueDateSetterTask);
+        verifyTasksCalledInOrder(caseData, alternativeServiceDueDateSetterTask, markJourneyAsServedByAlternativeMethodTask);
     }
 }
