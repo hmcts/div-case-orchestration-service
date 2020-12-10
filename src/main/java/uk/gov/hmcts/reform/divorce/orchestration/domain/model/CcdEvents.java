8a2035b1bcaf06a105f352267443503efff955d5
Marc-steeven Eyeni-Kantsey <mseyeni@icloud.com>
2020-12-10 10:20:55 +0000

    Div 6653 Allow co-respondent to submit AOS from new states (#1168)
    * DIV-6653: Run startAos for new states
    
    * Allow Co-respondent to submit AOS from new states
    
    Co-authored-by: mseyeni@icloud.com <marc-steevene@Ilya.local>

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/CcdEvents.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/CcdEvents.java
@@ -7,4 +7,7 @@ import lombok.NoArgsConstructor;
 public class CcdEvents {
 
     public static final String AOS_START_FROM_SERVICE_APPLICATION_NOT_APPROVED = "startAosFromServiceAppRejected";
+    public static final String CO_RESP_SUBMISSION_AWAITING_ALTERNATIVE_SERVICE = "co-RespAOSReceivedFromAwaitingAlternativeService";
+    public static final String CO_RESP_SUBMISSION_AWAITING_PROCESS_SERVER_SERVICE = "co-RespAOSReceivedFromAwaitingProcessServerService";
+    public static final String CO_RESP_SUBMISSION_AWAITING_DWP_RESPONSE = "co-RespAOSReceivedFromAwaitingDWPResponse";
 }
