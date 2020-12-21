9bd0354e4c2513a7a051db81e7fd39f170bd7e12
Patryk Jar <jar.patryk@gmail.com>
2020-12-21 17:00:48 +0100

    more unit tests

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/util/DraftHelper.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/util/DraftHelper.java
@@ -1,13 +1,14 @@
 package uk.gov.hmcts.reform.divorce.orchestration.tasks.util;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 import java.util.Optional;
 
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.IS_DRAFT_KEY;
 
-@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DraftHelper {
 
     public static boolean isDraft(Map<String, Object> caseData) {
