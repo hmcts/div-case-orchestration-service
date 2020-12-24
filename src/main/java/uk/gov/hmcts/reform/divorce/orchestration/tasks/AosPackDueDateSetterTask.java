88927aee3058177c46b0fa064e514ae638050bde
Patryk Jar <jar.patryk@gmail.com>
2020-12-24 13:01:12 +0100

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/AosPackDueDateSetterTask.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/AosPackDueDateSetterTask.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.divorce.orchestration.tasks;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
 public class AosPackDueDateSetterTask extends DueDateSetterTask {
