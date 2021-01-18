7cd11a801c0f55d54e3ebb9ea992f6b4aff32b28
J. Victor Ferreira <victorf@kainos.com>
2021-01-18 14:37:34 +0000

    DIV-6734: Re-generating costs order document (#1196)
    * DIV-6734: Re-generating costs order document
    
    * DIV-6739: Re-generating Decree Nisi and Decree Absolute (#1202)

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/service/impl/CourtOrderDocumentsUpdateServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/service/impl/CourtOrderDocumentsUpdateServiceImpl.java
@@ -6,53 +6,94 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.divorce.model.ccd.CollectionMember;
 import uk.gov.hmcts.reform.divorce.model.ccd.Document;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CaseDetails;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.document.template.DocumentType;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.WorkflowException;
 import uk.gov.hmcts.reform.divorce.orchestration.service.CaseOrchestrationServiceException;
 import uk.gov.hmcts.reform.divorce.orchestration.service.CourtOrderDocumentsUpdateService;
 import uk.gov.hmcts.reform.divorce.orchestration.workflows.DocumentGenerationWorkflow;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CERTIFICATE_OF_ENTITLEMENT_FILENAME_PREFIX;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.COSTS_ORDER_DOCUMENT_TYPE;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D8DOCUMENTS_GENERATED;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_ABSOLUTE_DOCUMENT_TYPE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_ABSOLUTE_FILENAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_NISI_DOCUMENT_TYPE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_NISI_FILENAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_TYPE_COE;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.document.template.DocumentType.COE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.document.template.DocumentType.COSTS_ORDER;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.document.template.DocumentType.DECREE_ABSOLUTE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.document.template.DocumentType.DECREE_NISI;
 import static uk.gov.hmcts.reform.divorce.orchestration.util.CcdUtil.getCollectionMembersOrEmptyList;
 
 @RequiredArgsConstructor
 @Service
 public class CourtOrderDocumentsUpdateServiceImpl implements CourtOrderDocumentsUpdateService {
 
-    private static final String CERTIFICATE_OF_ENTITLEMENT_FILE_NAME = "certificateOfEntitlement";
-
     private final ObjectMapper objectMapper;
     private final DocumentGenerationWorkflow documentGenerationWorkflow;
 
     @Override
     public Map<String, Object> updateExistingCourtOrderDocuments(String authToken, CaseDetails caseDetails) throws CaseOrchestrationServiceException {
-        String caseId = caseDetails.getCaseId();
-        Map<String, Object> caseData = new HashMap<>(caseDetails.getCaseData());
+        List<CollectionMember<Document>> generatedDocuments =
+            getCollectionMembersOrEmptyList(objectMapper, caseDetails.getCaseData(), D8DOCUMENTS_GENERATED);
 
-        List<CollectionMember<Document>> generatedDocuments = getCollectionMembersOrEmptyList(objectMapper, caseData, D8DOCUMENTS_GENERATED);
-
-        boolean coeDocumentExists = generatedDocuments.stream()
-            .map(CollectionMember::getValue)
-            .map(Document::getDocumentType)
-            .anyMatch(DOCUMENT_TYPE_COE::equals);
-        if (coeDocumentExists) {
-            updateCertificateOfEntitlement(authToken, caseDetails, caseId, caseData);
+        if (isDocumentPresent(generatedDocuments, DOCUMENT_TYPE_COE)) {
+            updateCertificateOfEntitlement(authToken, caseDetails);
         }
 
-        return caseData;
+        if (isDocumentPresent(generatedDocuments, COSTS_ORDER_DOCUMENT_TYPE)) {
+            updateCostsOrder(authToken, caseDetails);
+        }
+
+        if (isDocumentPresent(generatedDocuments, DECREE_NISI_DOCUMENT_TYPE)) {
+            updateDecreeNisi(authToken, caseDetails);
+        }
+
+        if (isDocumentPresent(generatedDocuments, DECREE_ABSOLUTE_DOCUMENT_TYPE)) {
+            updateDecreeAbsolute(authToken, caseDetails);
+        }
+
+        return caseDetails.getCaseData();
     }
 
-    private void updateCertificateOfEntitlement(String authToken, CaseDetails caseDetails, String caseId, Map<String, Object> caseData)
-        throws CaseOrchestrationServiceException {
+    private boolean isDocumentPresent(List<CollectionMember<Document>> generatedDocuments, String ccdDocumentType) {
+        return generatedDocuments.stream()
+            .map(CollectionMember::getValue)
+            .map(Document::getDocumentType)
+            .anyMatch(ccdDocumentType::equals);
+    }
+
+    private void updateCertificateOfEntitlement(String authToken, CaseDetails caseDetails) throws CaseOrchestrationServiceException {
+        generateDocument(authToken, caseDetails, DOCUMENT_TYPE_COE, COE, CERTIFICATE_OF_ENTITLEMENT_FILENAME_PREFIX);
+    }
+
+    private void updateCostsOrder(String authToken, CaseDetails caseDetails) throws CaseOrchestrationServiceException {
+        generateDocument(authToken, caseDetails, COSTS_ORDER_DOCUMENT_TYPE, COSTS_ORDER, COSTS_ORDER_DOCUMENT_TYPE);
+    }
+
+    private void updateDecreeNisi(String authToken, CaseDetails caseDetails) throws CaseOrchestrationServiceException {
+        generateDocument(authToken, caseDetails, DECREE_NISI_DOCUMENT_TYPE, DECREE_NISI, DECREE_NISI_FILENAME);
+    }
+
+    private void updateDecreeAbsolute(String authToken, CaseDetails caseDetails) throws CaseOrchestrationServiceException {
+        generateDocument(authToken, caseDetails, DECREE_ABSOLUTE_DOCUMENT_TYPE, DECREE_ABSOLUTE, DECREE_ABSOLUTE_FILENAME);
+    }
+
+    private void generateDocument(String authToken,
+                                  CaseDetails caseDetails,
+                                  String documentTypeCoe,
+                                  DocumentType coe,
+                                  String certificateOfEntitlementFilenamePrefix) throws CaseOrchestrationServiceException {
+        String caseId = caseDetails.getCaseId();
+
         try {
-            Map<String, Object> returnedPayload =
-                documentGenerationWorkflow.run(caseDetails, authToken, DOCUMENT_TYPE_COE, COE, CERTIFICATE_OF_ENTITLEMENT_FILE_NAME);
-            caseData.putAll(returnedPayload);
+            Map<String, Object> returnedCaseData =
+                documentGenerationWorkflow.run(caseDetails, authToken, documentTypeCoe, coe, certificateOfEntitlementFilenamePrefix);
+            caseDetails.setCaseData(returnedCaseData);
         } catch (WorkflowException exception) {
             throw new CaseOrchestrationServiceException(exception, caseId);
         }
--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/service/impl/CourtOrderDocumentsUpdateServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/service/impl/CourtOrderDocumentsUpdateServiceImplTest.java
@@ -8,37 +8,49 @@ import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.divorce.model.ccd.CollectionMember;
 import uk.gov.hmcts.reform.divorce.model.ccd.Document;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CaseDetails;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.document.template.DocumentType;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.WorkflowException;
 import uk.gov.hmcts.reform.divorce.orchestration.service.CaseOrchestrationServiceException;
 import uk.gov.hmcts.reform.divorce.orchestration.service.CourtOrderDocumentsUpdateService;
 import uk.gov.hmcts.reform.divorce.orchestration.workflows.DocumentGenerationWorkflow;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import static java.util.function.Predicate.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isA;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.never;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.AUTH_TOKEN;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_INCOMING_CASE_DETAILS;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CERTIFICATE_OF_ENTITLEMENT_FILENAME_PREFIX;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.COSTS_ORDER_DOCUMENT_TYPE;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.D8DOCUMENTS_GENERATED;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_ABSOLUTE_DOCUMENT_TYPE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_ABSOLUTE_FILENAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_NISI_DOCUMENT_TYPE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_NISI_FILENAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_TYPE_COE;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.document.template.DocumentType.COE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.document.template.DocumentType.COSTS_ORDER;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.document.template.DocumentType.DECREE_ABSOLUTE;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.document.template.DocumentType.DECREE_NISI;
 import static uk.gov.hmcts.reform.divorce.orchestration.testutil.CaseDataTestHelper.createCollectionMemberDocument;
 import static uk.gov.hmcts.reform.divorce.orchestration.testutil.ObjectMapperTestUtil.getObjectMapperInstance;
+import static uk.gov.hmcts.reform.divorce.orchestration.util.CcdUtil.getCollectionMembersOrEmptyList;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CourtOrderDocumentsUpdateServiceImplTest {
-
-    private static final String COE_ENGLISH_TEMPLATE_ID = "FL-DIV-GNO-ENG-00020.docx";
-    private static final String COE_FILE_NAME = "certificateOfEntitlement";
 
     @Mock
     private DocumentGenerationWorkflow documentGenerationWorkflow;
@@ -52,44 +64,53 @@ public class CourtOrderDocumentsUpdateServiceImplTest {
         classUnderTest = new CourtOrderDocumentsUpdateServiceImpl(getObjectMapperInstance(), documentGenerationWorkflow);
 
         Map<String, Object> incomingCaseData = Map.of(D8DOCUMENTS_GENERATED, List.of(
-            createCollectionMemberDocument("oldUrl", DOCUMENT_TYPE_COE, COE_FILE_NAME)
+            createCollectionMemberDocument("oldUrl", DOCUMENT_TYPE_COE, CERTIFICATE_OF_ENTITLEMENT_FILENAME_PREFIX),
+            createCollectionMemberDocument("oldUrl", COSTS_ORDER_DOCUMENT_TYPE, COSTS_ORDER_DOCUMENT_TYPE),
+            createCollectionMemberDocument("oldUrl", DECREE_NISI_DOCUMENT_TYPE, DECREE_NISI_FILENAME),
+            createCollectionMemberDocument("oldUrl", DECREE_ABSOLUTE_DOCUMENT_TYPE, DECREE_ABSOLUTE_FILENAME)
         ));
         incomingCaseDetails = CaseDetails.builder().caseId(TEST_CASE_ID).caseData(incomingCaseData).build();
     }
 
     @Test
     public void shouldUpdateExistingCourtOrderDocuments() throws WorkflowException, CaseOrchestrationServiceException {
-        when(documentGenerationWorkflow.run(incomingCaseDetails, AUTH_TOKEN, DOCUMENT_TYPE_COE, COE, COE_FILE_NAME))
-            .thenReturn(
-                Map.of(D8DOCUMENTS_GENERATED, List.of(
-                    createCollectionMemberDocument("newUrl", DOCUMENT_TYPE_COE, COE_FILE_NAME)
-                ))
-            );
+        mockWorkflowToAddOrReplaceDocument(DOCUMENT_TYPE_COE, COE, CERTIFICATE_OF_ENTITLEMENT_FILENAME_PREFIX);
+        mockWorkflowToAddOrReplaceDocument(COSTS_ORDER_DOCUMENT_TYPE, COSTS_ORDER, COSTS_ORDER_DOCUMENT_TYPE);
+        mockWorkflowToAddOrReplaceDocument(DECREE_NISI_DOCUMENT_TYPE, DECREE_NISI, DECREE_NISI_FILENAME);
+        mockWorkflowToAddOrReplaceDocument(DECREE_ABSOLUTE_DOCUMENT_TYPE, DECREE_ABSOLUTE, DECREE_ABSOLUTE_FILENAME);
 
         Map<String, Object> returnedPayload = classUnderTest.updateExistingCourtOrderDocuments(AUTH_TOKEN, incomingCaseDetails);
 
-        List<CollectionMember<Document>> generatedDocuments = (List<CollectionMember<Document>>) returnedPayload.get(D8DOCUMENTS_GENERATED);
-        assertThat(generatedDocuments.size(), is(incomingCaseDetails.getCaseData().size()));
-        CollectionMember<Document> updatedCertificateOfEntitlement = generatedDocuments.get(0);
-        assertThat(updatedCertificateOfEntitlement.getValue().getDocumentType(), is(DOCUMENT_TYPE_COE));
-        assertThat(updatedCertificateOfEntitlement.getValue().getDocumentLink().getDocumentUrl(), is("newUrl"));
+        List<CollectionMember<Document>> generatedDocuments =
+            getCollectionMembersOrEmptyList(getObjectMapperInstance(), returnedPayload, D8DOCUMENTS_GENERATED);
+        assertThat(generatedDocuments.size(), is(4));
+        assertDocumentIsNew(generatedDocuments.get(0), DOCUMENT_TYPE_COE);
+        assertDocumentIsNew(generatedDocuments.get(1), COSTS_ORDER_DOCUMENT_TYPE);
+        assertDocumentIsNew(generatedDocuments.get(2), DECREE_NISI_DOCUMENT_TYPE);
+        assertDocumentIsNew(generatedDocuments.get(3), DECREE_ABSOLUTE_DOCUMENT_TYPE);
 
-        //Certificate of Entitlement
-        verify(documentGenerationWorkflow).run(incomingCaseDetails, AUTH_TOKEN, DOCUMENT_TYPE_COE, COE, COE_FILE_NAME);
+        verify(documentGenerationWorkflow)
+            .run(incomingCaseDetails, AUTH_TOKEN, DOCUMENT_TYPE_COE, COE, CERTIFICATE_OF_ENTITLEMENT_FILENAME_PREFIX);
+        verify(documentGenerationWorkflow)
+            .run(incomingCaseDetails, AUTH_TOKEN, COSTS_ORDER_DOCUMENT_TYPE, COSTS_ORDER, COSTS_ORDER_DOCUMENT_TYPE);
+        verify(documentGenerationWorkflow)
+            .run(incomingCaseDetails, AUTH_TOKEN, DECREE_NISI_DOCUMENT_TYPE, DECREE_NISI, DECREE_NISI_FILENAME);
+        verify(documentGenerationWorkflow)
+            .run(incomingCaseDetails, AUTH_TOKEN, DECREE_ABSOLUTE_DOCUMENT_TYPE, DECREE_ABSOLUTE, DECREE_ABSOLUTE_FILENAME);
     }
 
     @Test
-    public void shouldNotUpdateCourtOrderDocumentsIfTheyDoNotAlreadyExist() throws CaseOrchestrationServiceException, WorkflowException {
+    public void shouldNotUpdateCourtOrderDocumentsIfTheyDoNotAlreadyExist() throws CaseOrchestrationServiceException {
         Map<String, Object> returnedPayload = classUnderTest.updateExistingCourtOrderDocuments(AUTH_TOKEN, TEST_INCOMING_CASE_DETAILS);
 
         assertThat(returnedPayload, equalTo(TEST_INCOMING_CASE_DETAILS.getCaseData()));
 
-        verify(documentGenerationWorkflow, never()).run(any(), any(), any(), any(), any(), any());
+        verifyNoInteractions(documentGenerationWorkflow);
     }
 
     @Test
     public void shouldThrowCaseOrchestrationExceptionIfCertificateOfEntitlementGenerationFails() throws WorkflowException {
-        when(documentGenerationWorkflow.run(incomingCaseDetails, AUTH_TOKEN, DOCUMENT_TYPE_COE, COE, COE_FILE_NAME))
+        when(documentGenerationWorkflow.run(incomingCaseDetails, AUTH_TOKEN, DOCUMENT_TYPE_COE, COE, CERTIFICATE_OF_ENTITLEMENT_FILENAME_PREFIX))
             .thenThrow(WorkflowException.class);
 
         CaseOrchestrationServiceException serviceException = assertThrows(
@@ -99,6 +120,75 @@ public class CourtOrderDocumentsUpdateServiceImplTest {
 
         assertThat(serviceException.getCaseId().orElseThrow(), is(TEST_CASE_ID));
         assertThat(serviceException.getCause(), isA(WorkflowException.class));
+    }
+
+    @Test
+    public void shouldThrowCaseOrchestrationExceptionIfCostsOrderGenerationFails() throws WorkflowException {
+        when(documentGenerationWorkflow.run(incomingCaseDetails, AUTH_TOKEN, COSTS_ORDER_DOCUMENT_TYPE, COSTS_ORDER, COSTS_ORDER_DOCUMENT_TYPE))
+            .thenThrow(WorkflowException.class);
+
+        CaseOrchestrationServiceException serviceException = assertThrows(
+            CaseOrchestrationServiceException.class,
+            () -> classUnderTest.updateExistingCourtOrderDocuments(AUTH_TOKEN, incomingCaseDetails)
+        );
+
+        assertThat(serviceException.getCaseId().orElseThrow(), is(TEST_CASE_ID));
+        assertThat(serviceException.getCause(), isA(WorkflowException.class));
+    }
+
+    @Test
+    public void shouldThrowCaseOrchestrationExceptionIfDecreeNisiGenerationFails() throws WorkflowException {
+        when(documentGenerationWorkflow.run(incomingCaseDetails, AUTH_TOKEN, DECREE_NISI_DOCUMENT_TYPE, DECREE_NISI, DECREE_NISI_FILENAME))
+            .thenThrow(WorkflowException.class);
+
+        CaseOrchestrationServiceException serviceException = assertThrows(
+            CaseOrchestrationServiceException.class,
+            () -> classUnderTest.updateExistingCourtOrderDocuments(AUTH_TOKEN, incomingCaseDetails)
+        );
+
+        assertThat(serviceException.getCaseId().orElseThrow(), is(TEST_CASE_ID));
+        assertThat(serviceException.getCause(), isA(WorkflowException.class));
+    }
+
+    @Test
+    public void shouldThrowCaseOrchestrationExceptionIfDecreeAbsoluteGenerationFails() throws WorkflowException {
+        when(documentGenerationWorkflow.run(
+            incomingCaseDetails, AUTH_TOKEN, DECREE_ABSOLUTE_DOCUMENT_TYPE, DECREE_ABSOLUTE, DECREE_ABSOLUTE_FILENAME
+        )).thenThrow(WorkflowException.class);
+
+        CaseOrchestrationServiceException serviceException = assertThrows(
+            CaseOrchestrationServiceException.class,
+            () -> classUnderTest.updateExistingCourtOrderDocuments(AUTH_TOKEN, incomingCaseDetails)
+        );
+
+        assertThat(serviceException.getCaseId().orElseThrow(), is(TEST_CASE_ID));
+        assertThat(serviceException.getCause(), isA(WorkflowException.class));
+    }
+
+    private void mockWorkflowToAddOrReplaceDocument(String expectedCcdDocumentType,
+                                                    DocumentType expectedDocumentType,
+                                                    String expectedFilename) throws WorkflowException {
+        when(documentGenerationWorkflow.run(any(), eq(AUTH_TOKEN), eq(expectedCcdDocumentType), eq(expectedDocumentType), eq(expectedFilename)))
+            .thenAnswer(invocation -> {
+                CaseDetails caseDetails = invocation.getArgument(0, CaseDetails.class);
+                Map<String, Object> caseData = new HashMap<>(caseDetails.getCaseData());
+
+                List<CollectionMember<Document>> generatedDocuments =
+                    getCollectionMembersOrEmptyList(getObjectMapperInstance(), caseData, D8DOCUMENTS_GENERATED);
+                generatedDocuments = generatedDocuments.stream()
+                    .filter(not(documentCollectionMember -> expectedCcdDocumentType.equals(documentCollectionMember.getValue().getDocumentType())))
+                    .collect(Collectors.toList());
+                generatedDocuments.add(createCollectionMemberDocument("newUrl", expectedCcdDocumentType, expectedFilename));
+
+                caseData.put(D8DOCUMENTS_GENERATED, generatedDocuments);
+
+                return caseData;
+            });
+    }
+
+    private void assertDocumentIsNew(CollectionMember<Document> document, String expectedCcdDocumentType) {
+        assertThat(document.getValue().getDocumentType(), is(expectedCcdDocumentType));
+        assertThat(document.getValue().getDocumentLink().getDocumentUrl(), is("newUrl"));
     }
 
 }
