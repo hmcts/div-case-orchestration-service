7bbebdf0ffc9d9583c2d775a3063b6c917fe2f7b
Patryk Jar <jar.patryk@gmail.com>
2021-01-22 16:44:36 +0100

    refactoring, the beginning

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/service/AssignCaseAccessService.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/service/AssignCaseAccessService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.AssignCaseAccessRequest;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CaseDetails;
 import uk.gov.hmcts.reform.divorce.orchestration.util.mapper.AssignCaseAccessRequestMapper;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
 
 @Service
 @Slf4j
@@ -14,17 +15,19 @@ public class AssignCaseAccessService {
 
     private final AssignCaseAccessServiceConfiguration assignCaseAccessServiceConfiguration;
     private final AssignCaseAccessRequestMapper assignCaseAccessRequestMapper;
-    private final IdamService idamService;
+    private final IdamClient idamClient;
     private final RestService restService;
 
     public void assignCaseAccess(CaseDetails caseDetails, String authorisationToken) {
-        String userId = idamService.getIdamUserId(authorisationToken);
+        String userId = idamClient.getUserDetails(authorisationToken).getId();
+
         AssignCaseAccessRequest assignCaseAccessRequest = assignCaseAccessRequestMapper
             .mapToAssignCaseAccessRequest(caseDetails, userId);
 
         restService.restApiPostCall(
             authorisationToken,
             assignCaseAccessServiceConfiguration.getCaseAssignmentsUrl(),
-            assignCaseAccessRequest);
+            assignCaseAccessRequest
+        );
     }
 }
