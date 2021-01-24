1dc1dfb60800b0bf6cbd1c7876c7235072a7ccda
Patryk Jar <jar.patryk@gmail.com>
2021-01-24 19:55:11 +0100

    more unit tests

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/service/AssignCaseAccessServiceConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/service/AssignCaseAccessServiceConfiguration.java
@@ -1,12 +1,12 @@
 package uk.gov.hmcts.reform.divorce.orchestration.service;
 
-import lombok.Data;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Data
 @Component
 @ConfigurationProperties(prefix = "aca.api")
+@Getter
 public class AssignCaseAccessServiceConfiguration {
     private String caseAssignmentsUrl;
 }
