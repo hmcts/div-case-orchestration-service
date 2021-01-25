a8f8dd07291629940ea8a13fab58dc250536f4e4
Patryk Jar <jar.patryk@gmail.com>
2021-01-25 18:27:44 +0100

    spring functional tests

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/functionaltest/SolicitorCreateTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/functionaltest/SolicitorCreateTest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.divorce.orchestration.functionaltest;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CaseDetails;
@@ -19,18 +20,17 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.AUTH_TOKEN;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_SERVICE_AUTH_TOKEN;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.BulkCaseConstants.CREATE_EVENT;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CREATED_DATE_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DIVORCE_CENTRE_SITEID_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DIVORCE_UNIT_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_CASE_DETAILS_JSON_KEY;
 
-public class SolicitorCreateITest extends MockedFunctionalTest {
+public class SolicitorCreateTest extends IdamTestSupport {
 
     private static final String API_URL_CREATE = "/solicitor-create";
-
-    private static final String GENERATE_DOCUMENT_CONTEXT_PATH = "/version/1/generateDraftPDF";
-    private static final String FORMAT_ADD_DOCUMENTS_CONTEXT_PATH = "/caseformatter/version/1/add-documents";
     private static final String DRAFT_MINI_PETITION_TEMPLATE_NAME = "divorcedraftminipetition";
 
     @Autowired
@@ -41,32 +41,46 @@ public class SolicitorCreateITest extends MockedFunctionalTest {
 
     @Test
     public void givenCaseData_whenSolicitorCreate_thenReturnServiceCentreCourtAllocation() throws Exception {
-        final String caseId = "my-case-1";
+        CcdCallbackRequest ccdCallbackRequest = buildRequest();
+
+        stubDraftDocumentGeneratorService(
+            DRAFT_MINI_PETITION_TEMPLATE_NAME,
+            singletonMap(DOCUMENT_CASE_DETAILS_JSON_KEY, ccdCallbackRequest.getCaseDetails()),
+            AddMiniPetitionDraftTask.DOCUMENT_TYPE
+        );
+
+        stubUserDetailsEndpoint(HttpStatus.OK, AUTH_TOKEN, USER_DETAILS_PIN_USER_JSON);
+        stubServiceAuthProvider(HttpStatus.OK, TEST_SERVICE_AUTH_TOKEN);
+        stubRemoveCaseRoleServerEndpoint(AUTH_TOKEN, TEST_SERVICE_AUTH_TOKEN);
+        stubAssignCaseAccessServerEndpoint(AUTH_TOKEN, TEST_SERVICE_AUTH_TOKEN);
+
+        webClient.perform(post(API_URL_CREATE)
+            .header(AUTHORIZATION, AUTH_TOKEN)
+            .content(getBody(ccdCallbackRequest))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    private String getBody(CcdCallbackRequest ccdCallbackRequest) {
+        return ObjectMapperTestUtil.convertObjectToJsonString(ccdCallbackRequest);
+    }
+
+    private CcdCallbackRequest buildRequest() {
         Map<String, Object> expectedData = new HashMap<>();
         expectedData.put(CREATED_DATE_JSON_KEY, ccdUtil.getCurrentDateCcdFormat());
         expectedData.put(DIVORCE_UNIT_JSON_KEY, CourtEnum.SERVICE_CENTER.getId());
         expectedData.put(DIVORCE_CENTRE_SITEID_JSON_KEY, CourtEnum.SERVICE_CENTER.getSiteId());
 
         CaseDetails fullCase = CaseDetails.builder()
-            .caseId(caseId)
+            .caseId(TEST_CASE_ID)
             .caseData(expectedData)
             .build();
 
-        CcdCallbackRequest ccdCallbackRequest = CcdCallbackRequest.builder()
+        return CcdCallbackRequest.builder()
             .eventId(CREATE_EVENT)
             .caseDetails(fullCase)
             .build();
 
-        stubDraftDocumentGeneratorService(DRAFT_MINI_PETITION_TEMPLATE_NAME,
-            singletonMap(DOCUMENT_CASE_DETAILS_JSON_KEY, fullCase),
-            AddMiniPetitionDraftTask.DOCUMENT_TYPE);
-
-        webClient.perform(post(API_URL_CREATE)
-            .header(AUTHORIZATION, AUTH_TOKEN)
-            .content(ObjectMapperTestUtil.convertObjectToJsonString(ccdCallbackRequest))
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk());
     }
-
 }
