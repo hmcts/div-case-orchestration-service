47e4ec15528ed236bc52dedc6553b26b7c585a35
Jack <30896346+jacmurphy@users.noreply.github.com>
2021-02-17 09:46:37 +0000

    DIV-6769 Email pet solicitor on solicitor update (#1245)

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/SolicitorUpdateWorkflow.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/SolicitorUpdateWorkflow.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.divorce.orchestration.workflows;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.Features;
@@ -11,6 +12,7 @@ import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
 import uk.gov.hmcts.reform.divorce.orchestration.service.FeatureToggleService;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.AddMiniPetitionDraftTask;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.AddNewDocumentsToCaseDataTask;
+import uk.gov.hmcts.reform.divorce.orchestration.tasks.SendSolicitorApplicationSubmittedEmailTask;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.SetPetitionerSolicitorOrganisationPolicyReferenceTask;
 
 import java.util.ArrayList;
@@ -22,17 +24,23 @@ import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.Orchestrati
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_ID_JSON_KEY;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class SolicitorUpdateWorkflow extends DefaultWorkflow<Map<String, Object>> {
 
     private final AddMiniPetitionDraftTask addMiniPetitionDraftTask;
     private final AddNewDocumentsToCaseDataTask addNewDocumentsToCaseDataTask;
+    private final SendSolicitorApplicationSubmittedEmailTask sendSolicitorApplicationSubmittedEmailTask;
     private final SetPetitionerSolicitorOrganisationPolicyReferenceTask setPetitionerSolicitorOrganisationPolicyReferenceDetailTask;
     private final FeatureToggleService featureToggleService;
 
     public Map<String, Object> run(CaseDetails caseDetails, final String authToken) throws WorkflowException {
+        String caseId = caseDetails.getCaseId();
+
+        log.info("CaseID: {} SolicitorUpdateWorkflow workflow is going to be executed.", caseId);
+
         return this.execute(
-            getTasks(),
+            getTasks(caseId),
             caseDetails.getCaseData(),
             ImmutablePair.of(AUTH_TOKEN_JSON_KEY, authToken),
             ImmutablePair.of(CASE_DETAILS_JSON_KEY, caseDetails),
@@ -40,16 +48,38 @@ public class SolicitorUpdateWorkflow extends DefaultWorkflow<Map<String, Object>
         );
     }
 
-    private Task<Map<String, Object>>[] getTasks() {
+    private Task<Map<String, Object>>[] getTasks(String caseId) {
         List<Task<Map<String, Object>>> tasks = new ArrayList<>();
 
-        tasks.add(addMiniPetitionDraftTask);
-        tasks.add(addNewDocumentsToCaseDataTask);
+        tasks.add(getAddMiniPetitionDraftTask(caseId));
+        tasks.add(getAddNewDocumentsToCaseDataTask(caseId));
+        tasks.add(getSolicitorApplicationSubmittedEmailTask(caseId));
 
         if (featureToggleService.isFeatureEnabled(Features.REPRESENTED_RESPONDENT_JOURNEY)) {
-            tasks.add(setPetitionerSolicitorOrganisationPolicyReferenceDetailTask);
+            tasks.add(getSolicitorOrganisationPolicyDetailsTask(caseId));
         }
 
         return tasks.toArray(new Task[] {});
+    }
+
+    private Task<Map<String, Object>> getSolicitorApplicationSubmittedEmailTask(String caseId) {
+        log.info("CaseId: {} Adding task to send Application Submitted email to Petitioner solicitor.", caseId);
+        return sendSolicitorApplicationSubmittedEmailTask;
+    }
+
+    private Task<Map<String, Object>> getAddNewDocumentsToCaseDataTask(String caseId) {
+        log.info("CaseId: {} Adding task to Add new documents to case data.", caseId);
+        return addNewDocumentsToCaseDataTask;
+    }
+
+    private Task<Map<String, Object>> getAddMiniPetitionDraftTask(String caseId) {
+        log.info("CaseId: {} Adding task to Add Mini Petition Draft.", caseId);
+        return addMiniPetitionDraftTask;
+    }
+
+    private Task<Map<String, Object>> getSolicitorOrganisationPolicyDetailsTask(String caseId) {
+        log.info("CaseId: {} Feature Toggle {} is enabled. Adding petitioner solicitor organisation policy reference details task.",
+            caseId, Features.REPRESENTED_RESPONDENT_JOURNEY);
+        return setPetitionerSolicitorOrganisationPolicyReferenceDetailTask;
     }
 }
---
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/SendSolicitorApplicationSubmittedEmailTaskTest.java
@@ -0,0 +1,100 @@
+package uk.gov.hmcts.reform.divorce.orchestration.tasks;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.LanguagePreference;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.email.EmailTemplateNames;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.DefaultTaskContext;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
+import uk.gov.hmcts.reform.divorce.orchestration.service.EmailService;
+import uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.AddresseeDataExtractorTest;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_PETITIONER_FIRST_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_PETITIONER_LAST_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_RESPONDENT_FIRST_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_RESPONDENT_LAST_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_SOLICITOR_EMAIL;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_ID_JSON_KEY;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.email.EmailTemplateNames.SOL_APPLICANT_APPLICATION_SUBMITTED;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.EmailDataExtractor.CaseDataKeys.PETITIONER_SOLICITOR_EMAIL;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.FullNamesDataExtractor.CaseDataKeys.PETITIONER_FIRST_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.FullNamesDataExtractor.CaseDataKeys.PETITIONER_LAST_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.FullNamesDataExtractor.CaseDataKeys.RESPONDENT_FIRST_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.FullNamesDataExtractor.CaseDataKeys.RESPONDENT_LAST_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.FullNamesDataExtractor.getPetitionerFullName;
+import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.FullNamesDataExtractor.getRespondentFullName;
+import static uk.gov.hmcts.reform.divorce.orchestration.testutil.ServiceJourneyEmailTaskHelper.getExpectedNotificationTemplateVars;
+import static uk.gov.hmcts.reform.divorce.orchestration.testutil.ServiceJourneyEmailTaskHelper.getTaskContext;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SendSolicitorApplicationSubmittedEmailTaskTest {
+
+    @Mock
+    private EmailService emailService;
+
+    @InjectMocks
+    private SendSolicitorApplicationSubmittedEmailTask task;
+
+    private DefaultTaskContext testContext;
+    private static final String SUBJECT_CONTENT = "Application has been submitted";
+    private static final EmailTemplateNames EXPECTED_TEMPLATE = SOL_APPLICANT_APPLICATION_SUBMITTED;
+
+    @Before
+    public void setUp() {
+        testContext = new DefaultTaskContext();
+        testContext.setTransientObject(CASE_ID_JSON_KEY, TEST_CASE_ID);
+    }
+
+    @Test
+    public void shouldSendEmail_ToSolicitor_whenExecuteEmailNotificationTask() throws TaskException {
+        Map<String, Object> caseData = buildCaseData();
+
+        executeTask(caseData);
+
+        verifySolicitorEmailSent(caseData);
+    }
+
+    @Test
+    public void shouldReturnTemplate() {
+        EmailTemplateNames returnedTemplate = task.getTemplate();
+
+        assertEquals(EXPECTED_TEMPLATE, returnedTemplate);
+    }
+
+    private Map<String, Object> buildCaseData() {
+        Map<String, Object> caseData = AddresseeDataExtractorTest.buildCaseDataWithPetitionerSolicitor();
+
+        caseData.put(PETITIONER_FIRST_NAME, TEST_PETITIONER_FIRST_NAME);
+        caseData.put(PETITIONER_LAST_NAME, TEST_PETITIONER_LAST_NAME);
+        caseData.put(RESPONDENT_FIRST_NAME, TEST_RESPONDENT_FIRST_NAME);
+        caseData.put(RESPONDENT_LAST_NAME, TEST_RESPONDENT_LAST_NAME);
+
+        caseData.put(PETITIONER_SOLICITOR_EMAIL, TEST_SOLICITOR_EMAIL);
+
+        return caseData;
+    }
+
+    private void executeTask(Map<String, Object> caseData) {
+        Map<String, Object> returnPayload = task.execute(getTaskContext(), caseData);
+        assertEquals(caseData, returnPayload);
+    }
+
+    private void verifySolicitorEmailSent(Map<String, Object> caseData) {
+        verify(emailService).sendEmail(
+            TEST_SOLICITOR_EMAIL,
+            EXPECTED_TEMPLATE.name(),
+            getExpectedNotificationTemplateVars(true, testContext, caseData),
+            getPetitionerFullName(caseData) + " vs " + getRespondentFullName(caseData) + ": " + SUBJECT_CONTENT,
+            LanguagePreference.ENGLISH
+        );
+    }
+}
