299c5448eb933a8b172365d241f3792deb65d880
Patryk Jar <jar.patryk@gmail.com>
2021-02-17 13:18:10 +0100

    feature toggle added to NotifyRespondentOfDARequestedWorkflowTest

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/NotifyRespondentOfDARequestedWorkflowTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/NotifyRespondentOfDARequestedWorkflowTest.java
@@ -5,10 +5,10 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.Features;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CaseDetails;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CcdCallbackRequest;
-import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.WorkflowException;
-import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
+import uk.gov.hmcts.reform.divorce.orchestration.service.FeatureToggleService;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.SendDaRequestedNotifyRespondentEmailTask;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.decreeabsolute.DaRequestedPetitionerSolicitorEmailTask;
 
@@ -17,8 +17,10 @@ import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.reform.divorce.orchestration.testutil.Verificators.mockTasksExecution;
+import static uk.gov.hmcts.reform.divorce.orchestration.testutil.Verificators.verifyTaskWasNeverCalled;
 import static uk.gov.hmcts.reform.divorce.orchestration.testutil.Verificators.verifyTasksCalledInOrder;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -30,15 +32,20 @@ public class NotifyRespondentOfDARequestedWorkflowTest {
     @Mock
     private DaRequestedPetitionerSolicitorEmailTask daRequestedPetitionerSolicitorEmailTask;
 
+    @Mock
+    private FeatureToggleService featureToggleService;
+
     @InjectMocks
     private NotifyRespondentOfDARequestedWorkflow notifyRespondentOfDARequestedWorkflow;
 
-    @Test
-    public void callsTheRequiredTask() throws WorkflowException, TaskException {
+    private final Map<String, Object> payload = new HashMap<>();
 
-        final Map<String, Object> payload = new HashMap<>();
+    @Test
+    public void callsAllTasksWhenRespondentJourneyIsSwitchedOn() throws Exception {
+        when(featureToggleService.isFeatureEnabled(Features.REPRESENTED_RESPONDENT_JOURNEY)).thenReturn(true);
+
         final CcdCallbackRequest ccdCallbackRequest = CcdCallbackRequest.builder()
-            .caseDetails(CaseDetails.builder().caseId(TEST_CASE_ID).build())
+            .caseDetails(CaseDetails.builder().caseId(TEST_CASE_ID).caseData(payload).build())
             .build();
 
         mockTasksExecution(
@@ -55,4 +62,24 @@ public class NotifyRespondentOfDARequestedWorkflowTest {
         );
     }
 
+    @Test
+    public void callsOnlyOneTaskWhenRespondentJourneyIsSwitchedOff() throws Exception {
+        when(featureToggleService.isFeatureEnabled(Features.REPRESENTED_RESPONDENT_JOURNEY)).thenReturn(false);
+
+        final CcdCallbackRequest ccdCallbackRequest = CcdCallbackRequest.builder()
+            .caseDetails(CaseDetails.builder().caseId(TEST_CASE_ID).caseData(payload).build())
+            .build();
+
+        mockTasksExecution(
+            payload,
+            sendDaRequestedNotifyRespondentEmailTask
+        );
+
+        assertThat(notifyRespondentOfDARequestedWorkflow.run(ccdCallbackRequest), is(payload));
+        verifyTasksCalledInOrder(
+            payload,
+            sendDaRequestedNotifyRespondentEmailTask
+        );
+        verifyTaskWasNeverCalled(daRequestedPetitionerSolicitorEmailTask);
+    }
 }
