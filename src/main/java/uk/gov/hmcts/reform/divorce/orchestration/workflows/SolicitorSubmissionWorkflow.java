92e1ed33f11ced4723a705e4329dfc60e7a7e43f
J. Victor Ferreira <victorf@kainos.com>
2021-02-18 11:25:55 +0000

    DIV-6800: Improving logs (#1264)

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/SolicitorSubmissionWorkflow.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/SolicitorSubmissionWorkflow.java
@@ -1,8 +1,10 @@
 package uk.gov.hmcts.reform.divorce.orchestration.workflows;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CaseDetails;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CcdCallbackRequest;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.DefaultWorkflow;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.WorkflowException;
@@ -19,6 +21,7 @@ import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.Orchestrati
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class SolicitorSubmissionWorkflow extends DefaultWorkflow<Map<String, Object>> {
 
     private final ValidateSolicitorCaseDataTask validateSolicitorCaseDataTask;
@@ -27,6 +30,9 @@ public class SolicitorSubmissionWorkflow extends DefaultWorkflow<Map<String, Obj
     private final SendPetitionerSubmissionNotificationEmailTask sendPetitionerSubmissionNotificationEmailTask;
 
     public Map<String, Object> run(CcdCallbackRequest ccdCallbackRequest, String authToken) throws WorkflowException {
+        CaseDetails caseDetails = ccdCallbackRequest.getCaseDetails();
+        String caseId = caseDetails.getCaseId();
+        log.info("About to run {} workflow for case id {}", this.getClass().getSimpleName(), caseId);
 
         return this.execute(
             new Task[] {
@@ -35,9 +41,10 @@ public class SolicitorSubmissionWorkflow extends DefaultWorkflow<Map<String, Obj
                 removeMiniPetitionDraftDocumentsTask,
                 sendPetitionerSubmissionNotificationEmailTask
             },
-            ccdCallbackRequest.getCaseDetails().getCaseData(),
+            caseDetails.getCaseData(),
             ImmutablePair.of(AUTH_TOKEN_JSON_KEY, authToken),
-            ImmutablePair.of(CASE_ID_JSON_KEY, ccdCallbackRequest.getCaseDetails().getCaseId())
+            ImmutablePair.of(CASE_ID_JSON_KEY, caseId)
         );
     }
+
 }
