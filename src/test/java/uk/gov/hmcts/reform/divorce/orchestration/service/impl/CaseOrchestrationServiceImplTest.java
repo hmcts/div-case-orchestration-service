0ea1fdc61a31998400c14b96c8f1e0fb27838d20
J. Victor Ferreira <victorf@kainos.com>
2021-02-18 14:40:55 +0000

    DIV-6800: Throwing exception that helps us identify case id. (#1265)

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/service/impl/CaseOrchestrationServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/service/impl/CaseOrchestrationServiceImplTest.java
@@ -22,6 +22,7 @@ import uk.gov.hmcts.reform.divorce.orchestration.domain.model.payment.PaymentUpd
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.WorkflowException;
 import uk.gov.hmcts.reform.divorce.orchestration.service.CaseOrchestrationServiceException;
 import uk.gov.hmcts.reform.divorce.orchestration.util.AuthUtil;
+import uk.gov.hmcts.reform.divorce.orchestration.workflows.AllowShareACaseWorkflow;
 import uk.gov.hmcts.reform.divorce.orchestration.workflows.AmendPetitionForRefusalWorkflow;
 import uk.gov.hmcts.reform.divorce.orchestration.workflows.AmendPetitionWorkflow;
 import uk.gov.hmcts.reform.divorce.orchestration.workflows.AuthenticateRespondentWorkflow;
@@ -99,6 +100,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.isA;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.fail;
@@ -341,6 +343,9 @@ public class CaseOrchestrationServiceImplTest {
 
     @Mock
     private WelshSetPreviousStateWorkflow welshSetPreviousStateWorkflow;
+
+    @Mock
+    private AllowShareACaseWorkflow allowShareACaseWorkflow;
 
     @InjectMocks
     private CaseOrchestrationServiceImpl classUnderTest;
@@ -1728,6 +1733,17 @@ public class CaseOrchestrationServiceImplTest {
 
         List<String> errors = workflowErrors.values().stream().map(String.class::cast).collect(Collectors.toList());
         assertThat(ccdCallbackResponse.getErrors(), is(errors));
+    }
+
+    @Test
+    public void shouldThrowCaseOrchestrationException_IfWorkflowExceptionIsThrow_WhenAllowingShareACase() throws WorkflowException {
+        when(allowShareACaseWorkflow.run(ccdCallbackRequest.getCaseDetails(), AUTH_TOKEN)).thenThrow(WorkflowException.class);
+
+        CaseOrchestrationServiceException exception = assertThrows(CaseOrchestrationServiceException.class,
+            () -> classUnderTest.allowShareACase(ccdCallbackRequest, AUTH_TOKEN));
+
+        assertThat(exception.getCause(), isA(WorkflowException.class));
+        assertThat(exception.getCaseId().get(), is(TEST_CASE_ID));
     }
 
     private CcdCallbackRequest buildCcdCallbackRequest(Map<String, Object> requestPayload) {
