527a1594a4e31a3d71349d1c655d0cfd3bcc0db1
Patryk Jar <jar.patryk@gmail.com>
2021-02-19 18:26:39 +0100

    Added JsonIgnore to model methods

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/ccd/Organisation.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/ccd/Organisation.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Strings;
@@ -19,6 +20,7 @@ public class Organisation {
     @JsonProperty("OrganisationName")
     private String organisationName;
 
+    @JsonIgnore
     public boolean isPopulated() {
         return !Strings.isNullOrEmpty(organisationID);
     }
--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/ccd/OrganisationPolicy.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/domain/model/ccd/OrganisationPolicy.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
@@ -21,6 +22,7 @@ public class OrganisationPolicy {
     @JsonProperty("OrgPolicyReference")
     private String orgPolicyReference;
 
+    @JsonIgnore
     public boolean isPopulated() {
         return organisation.isPopulated();
     }
