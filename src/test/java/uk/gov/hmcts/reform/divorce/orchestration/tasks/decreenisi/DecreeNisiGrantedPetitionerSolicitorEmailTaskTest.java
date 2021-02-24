a88e7c5778f7ab7408e36b904483cdbde413e0d5
Patryk Jar <patryk.jar@hmcts.net>
2021-02-24 15:39:16 +0100

    DIV-6773: two more emails for DN pronounced (#1280)

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/decreenisi/DecreeNisiGrantedPetitionerSolicitorEmailTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/decreenisi/DecreeNisiGrantedPetitionerSolicitorEmailTaskTest.java
@@ -1,48 +1,52 @@
-package uk.gov.hmcts.reform.divorce.orchestration.tasks.notification;
+package uk.gov.hmcts.reform.divorce.orchestration.tasks.decreenisi;
 
+import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.LanguagePreference;
-import uk.gov.hmcts.reform.divorce.orchestration.domain.model.email.EmailTemplateNames;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
 import uk.gov.hmcts.reform.divorce.orchestration.service.EmailService;
 import uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.AddresseeDataExtractorTest;
 
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_PETITIONER_FIRST_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_PETITIONER_FULL_NAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_PETITIONER_LAST_NAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_RESPONDENT_FIRST_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_RESPONDENT_FULL_NAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_RESPONDENT_LAST_NAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_SOLICITOR_EMAIL;
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.CcdFields.RESPONDENT_SOLICITOR_ORGANISATION_POLICY;
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.email.EmailTemplateNames.SOL_RESPONDENT_NOTICE_OF_PROCEEDINGS;
-import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.EmailDataExtractor.CaseDataKeys.RESPONDENT_SOLICITOR_EMAIL;
+import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_SOLICITOR_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.NOTIFICATION_CCD_REFERENCE_KEY;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.NOTIFICATION_PET_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.NOTIFICATION_RESP_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.NOTIFICATION_SOLICITOR_NAME;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.email.EmailTemplateNames.SOL_PETITIONER_DECREE_NISI_GRANTED;
 import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.FullNamesDataExtractor.CaseDataKeys.PETITIONER_FIRST_NAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.FullNamesDataExtractor.CaseDataKeys.PETITIONER_LAST_NAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.FullNamesDataExtractor.CaseDataKeys.RESPONDENT_FIRST_NAME;
 import static uk.gov.hmcts.reform.divorce.orchestration.service.bulk.print.dataextractor.FullNamesDataExtractor.CaseDataKeys.RESPONDENT_LAST_NAME;
-import static uk.gov.hmcts.reform.divorce.orchestration.testutil.CaseDataTestHelper.buildOrganisationPolicy;
-import static uk.gov.hmcts.reform.divorce.orchestration.testutil.ServiceJourneyEmailTaskHelper.getExpectedRespondentSolicitorNotificationTemplateVars;
-import static uk.gov.hmcts.reform.divorce.orchestration.testutil.ServiceJourneyEmailTaskHelper.getTaskContext;
-import static uk.gov.hmcts.reform.divorce.orchestration.testutil.TaskContextHelper.context;
+import static uk.gov.hmcts.reform.divorce.orchestration.testutil.TaskContextHelper.contextWithToken;
 
 @RunWith(MockitoJUnitRunner.class)
-public class SendRespondentNoticeOfProceedingsEmailTaskTest {
+public class DecreeNisiGrantedPetitionerSolicitorEmailTaskTest {
 
     @Mock
     private EmailService emailService;
 
     @InjectMocks
-    private SendRespondentNoticeOfProceedingsEmailTask task;
+    private DecreeNisiGrantedPetitionerSolicitorEmailTask task;
 
     @Test
-    public void shouldSendEmail_ToSolicitor_whenExecuteEmailNotificationTask() throws TaskException {
+    public void shouldSendEmail_ToPetitionerSolicitor_whenExecuted() throws TaskException {
         Map<String, Object> caseData = buildCaseData();
 
         executeTask(caseData);
@@ -52,36 +56,36 @@ public class SendRespondentNoticeOfProceedingsEmailTaskTest {
 
     @Test
     public void shouldReturnTemplate() {
-        EmailTemplateNames returnedTemplate = task.getTemplate();
-
-        assertEquals(SOL_RESPONDENT_NOTICE_OF_PROCEEDINGS, returnedTemplate);
+        assertThat(task.getTemplate(), is(SOL_PETITIONER_DECREE_NISI_GRANTED));
     }
 
     private Map<String, Object> buildCaseData() {
-        Map<String, Object> caseData = AddresseeDataExtractorTest.buildCaseDataWithRespondentSolicitor();
+        Map<String, Object> caseData = AddresseeDataExtractorTest.buildCaseDataWithPetitionerSolicitor();
 
         caseData.put(PETITIONER_FIRST_NAME, TEST_PETITIONER_FIRST_NAME);
         caseData.put(PETITIONER_LAST_NAME, TEST_PETITIONER_LAST_NAME);
         caseData.put(RESPONDENT_FIRST_NAME, TEST_RESPONDENT_FIRST_NAME);
         caseData.put(RESPONDENT_LAST_NAME, TEST_RESPONDENT_LAST_NAME);
 
-        caseData.put(RESPONDENT_SOLICITOR_EMAIL, TEST_SOLICITOR_EMAIL);
-        caseData.put(RESPONDENT_SOLICITOR_ORGANISATION_POLICY, buildOrganisationPolicy());
-
         return caseData;
     }
 
     private void executeTask(Map<String, Object> caseData) {
-        Map<String, Object> returnPayload = task.execute(getTaskContext(), caseData);
-        assertEquals(caseData, returnPayload);
+        Map<String, Object> returnPayload = task.execute(contextWithToken(), caseData);
+        assertThat(caseData, is(returnPayload));
     }
 
     private void verifySolicitorEmailSent(Map<String, Object> caseData) {
         verify(emailService).sendEmail(
             TEST_SOLICITOR_EMAIL,
-            SOL_RESPONDENT_NOTICE_OF_PROCEEDINGS.name(),
-            getExpectedRespondentSolicitorNotificationTemplateVars(context(), caseData),
-            "CaseId: test.case.id, email template SOL_RESPONDENT_NOTICE_OF_PROCEEDINGS",
+            SOL_PETITIONER_DECREE_NISI_GRANTED.name(),
+            ImmutableMap.of(
+                NOTIFICATION_PET_NAME, TEST_PETITIONER_FULL_NAME,
+                NOTIFICATION_RESP_NAME, TEST_RESPONDENT_FULL_NAME,
+                NOTIFICATION_CCD_REFERENCE_KEY, TEST_CASE_ID,
+                NOTIFICATION_SOLICITOR_NAME, TEST_SOLICITOR_NAME
+            ),
+            "CaseId: test.case.id, email template SOL_PETITIONER_DECREE_NISI_GRANTED",
             LanguagePreference.ENGLISH
         );
     }
