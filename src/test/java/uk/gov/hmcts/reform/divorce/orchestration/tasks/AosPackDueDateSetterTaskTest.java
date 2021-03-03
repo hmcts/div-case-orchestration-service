2b00f3132b995ba775dc289c0764db3dbbec7d61
Ravinder Singh <ravinderpal96@live.it>
2021-03-03 10:13:01 +0000

    test refactoring

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/AosPackDueDateSetterTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/AosPackDueDateSetterTaskTest.java
@@ -1,6 +1,18 @@
 package uk.gov.hmcts.reform.divorce.orchestration.tasks;
 
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.CcdFields;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
+import uk.gov.hmcts.reform.divorce.orchestration.testutil.DateCalculator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.core.Is.is;
+import static uk.gov.hmcts.reform.divorce.orchestration.testutil.TaskContextHelper.context;
 
 public class AosPackDueDateSetterTaskTest extends DueDateSetterTaskTest {
 
@@ -17,5 +29,18 @@ public class AosPackDueDateSetterTaskTest extends DueDateSetterTaskTest {
     @Override
     protected DueDateSetterTask getDueDateSetterTaskInstance() {
         return aosPackDueDateSetterTask;
+    }
+
+    @Test
+    public void executeShouldAddPopulatedFieldWithExpectedValue() throws TaskException {
+        Map<String, Object> caseData = new HashMap<>();
+        Map<String, Object> returnedCaseData = aosPackDueDateSetterTask.execute(context(), caseData);
+
+        assertThat(returnedCaseData.isEmpty(), is(false));
+        assertThat(caseData, hasEntry(CcdFields.DUE_DATE, getDatePlusOffsetInCcdFormat()));
+    }
+
+    private String getDatePlusOffsetInCcdFormat() {
+        return DateCalculator.getDateWithOffset(DUE_DATE_OFFSET);
     }
 }
