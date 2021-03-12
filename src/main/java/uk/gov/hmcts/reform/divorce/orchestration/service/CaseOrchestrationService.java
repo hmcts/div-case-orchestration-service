c50ccbf1d17407a296e6e0005d1fd04757017aa3
Jakub Kozlowski <jakub.kozlowski@gmail.com>
2021-03-12 16:59:00 +0000

    DIV-6630: endpoint for judge costs decision

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/service/CaseOrchestrationService.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/service/CaseOrchestrationService.java
@@ -177,4 +177,6 @@ public interface CaseOrchestrationService {
     Map<String, Object> sendAmendApplicationEmail(CcdCallbackRequest ccdCallbackRequest) throws WorkflowException;
 
     CcdCallbackResponse welshSetPreviousState(CcdCallbackRequest ccdCallbackRequest) throws WorkflowException;
+
+    Map<String, Object> judgeCostsDecision(CcdCallbackRequest ccdCallbackRequest);
 }
