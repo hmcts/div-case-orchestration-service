a8af85a553476a8a8ee339a61509903af34e3aac
Shaun Ganley <shaung@Fare.local>
2021-03-23 11:32:08 +0000

    functional and unit tests added

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/functionaltest/solicitor/dn/rejectandamend/GetPetitionIssueFeesTestShareCaseFeatureSwitchedOffTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/functionaltest/solicitor/dn/rejectandamend/GetPetitionIssueFeesTestShareCaseFeatureSwitchedOffTest.java
@@ -9,6 +9,8 @@ import uk.gov.hmcts.reform.divorce.model.ccd.CaseLink;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CcdCallbackResponse;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.fees.OrderSummary;
 
+import java.util.Collections;
+
 import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasNoJsonPath;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -16,23 +18,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.AUTH_TOKEN;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_CASE_ID;
-import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_SERVICE_AUTH_TOKEN;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.PETITION_ISSUE_ORDER_SUMMARY_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.PREVIOUS_CASE_ID_CCD_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.SOL_APPLICATION_FEE_IN_POUNDS_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.testutil.ObjectMapperTestUtil.convertObjectToJsonString;
 
-@SpringBootTest(properties = {"feature-toggle.toggle.solicitor_dn_reject_and_amend=false"})
-public class GetPetitionIssueFeesTestFeatureSwitchedOffTest extends GetPetitionIssueFeesAbstractTest {
+@SpringBootTest(properties = {"feature-toggle.toggle.share_a_case=false"})
+public class GetPetitionIssueFeesTestShareCaseFeatureSwitchedOffTest extends GetPetitionIssueFeesAbstractTest {
 
     @Test
     public void givenCaseData_whenGetPetitionIssueFee_thenReturnUpdatedResponseWithFees() throws Exception {
         callbackRequest.getCaseDetails().setCaseId(TEST_CASE_ID);
-
-        stubUserDetailsEndpoint(HttpStatus.OK, AUTH_TOKEN, USER_DETAILS_PIN_USER_JSON);
-        stubServiceAuthProvider(HttpStatus.OK, TEST_SERVICE_AUTH_TOKEN);
-        stubRemoveCaseRoleServerEndpoint(AUTH_TOKEN, TEST_SERVICE_AUTH_TOKEN, HttpStatus.OK);
-        stubAssignCaseAccessServerEndpoint(AUTH_TOKEN, TEST_SERVICE_AUTH_TOKEN, HttpStatus.OK);
+        stubMaintenanceServerEndpointForAddPetitionerSolicitorRole(HttpStatus.OK);
 
         OrderSummary orderSummary = new OrderSummary();
         orderSummary.add(issueFeeResponse);
@@ -54,11 +51,7 @@ public class GetPetitionIssueFeesTestFeatureSwitchedOffTest extends GetPetitionI
     @Test
     public void givenCaseAmendment_whenGetPetitionIssueFee_thenReturnUpdatedResponseWithFees() throws Exception {
         callbackRequest.getCaseDetails().getCaseData().put(PREVIOUS_CASE_ID_CCD_KEY, CaseLink.builder().caseReference("1234567890123456").build());
-
-        stubUserDetailsEndpoint(HttpStatus.OK, AUTH_TOKEN, USER_DETAILS_PIN_USER_JSON);
-        stubServiceAuthProvider(HttpStatus.OK, TEST_SERVICE_AUTH_TOKEN);
-        stubRemoveCaseRoleServerEndpoint(AUTH_TOKEN, TEST_SERVICE_AUTH_TOKEN, HttpStatus.OK);
-        stubAssignCaseAccessServerEndpoint(AUTH_TOKEN, TEST_SERVICE_AUTH_TOKEN, HttpStatus.OK);
+        stubMaintenanceServerEndpointForAddPetitionerSolicitorRole(HttpStatus.OK);
 
         OrderSummary orderSummary = new OrderSummary();
         orderSummary.add(issueFeeResponse);
@@ -75,5 +68,39 @@ public class GetPetitionIssueFeesTestFeatureSwitchedOffTest extends GetPetitionI
             .andExpect(status().isOk())
             .andExpect(content().json(convertObjectToJsonString(expectedResponse)))
             .andExpect(content().string(hasNoJsonPath("data." + SOL_APPLICATION_FEE_IN_POUNDS_JSON_KEY)));
+    }
+
+    @Test
+    public void givenCaseData_whenAddingCaseRoleFails_thenReturnErrorResponse() throws Exception {
+        stubMaintenanceServerEndpointForAddPetitionerSolicitorRole(HttpStatus.BAD_GATEWAY);
+
+        CcdCallbackResponse expected = CcdCallbackResponse.builder()
+            .errors(Collections.singletonList("Problem setting the [PETSOLICITOR] role to the case"))
+            .build();
+
+        webClient.perform(post(API_URL)
+            .header(AUTHORIZATION, AUTH_TOKEN)
+            .content(convertObjectToJsonString(callbackRequest))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().json(convertObjectToJsonString(expected)));
+    }
+
+    @Test
+    public void givenUnauthorizedRequest_whenGetPetitionIssueFees_thenReturnErrorData() throws Exception {
+        stubMaintenanceServerEndpointForAddPetitionerSolicitorRole(HttpStatus.FORBIDDEN);
+
+        CcdCallbackResponse expected = CcdCallbackResponse.builder()
+            .errors(Collections.singletonList("Problem setting the [PETSOLICITOR] role to the case"))
+            .build();
+
+        webClient.perform(post(API_URL)
+            .header(AUTHORIZATION, AUTH_TOKEN)
+            .content(convertObjectToJsonString(callbackRequest))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().json(convertObjectToJsonString(expected)));
     }
 }
