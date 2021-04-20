41ed55a00ffa399965e08893e003c0aee60185c7
Patryk Jar <jar.patryk@gmail.com>
2021-04-20 14:48:49 +0200

    remove ignore

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/dataextraction/DataExtractionEmailClientTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/dataextraction/DataExtractionEmailClientTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.divorce.orchestration.tasks.dataextraction;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
