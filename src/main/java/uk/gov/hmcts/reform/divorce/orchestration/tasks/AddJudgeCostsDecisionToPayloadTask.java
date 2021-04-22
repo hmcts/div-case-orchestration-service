d9aed73492bf9d9dc3e036b7ef3ebbf0f0634c44
Ravinder Singh <ravinderpal96@live.it>
2021-04-22 14:57:53 +0100

    updated log

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/AddJudgeCostsDecisionToPayloadTask.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/AddJudgeCostsDecisionToPayloadTask.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.divorce.orchestration.tasks;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
@@ -10,7 +11,9 @@ import java.util.Map;
 
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.CcdFields.JUDGE_COSTS_DECISION;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.YES_VALUE;
+import static uk.gov.hmcts.reform.divorce.orchestration.tasks.util.TaskUtils.getCaseId;
 
+@Slf4j
 @Component
 public class AddJudgeCostsDecisionToPayloadTask implements Task<Map<String, Object>> {
 
@@ -18,6 +21,7 @@ public class AddJudgeCostsDecisionToPayloadTask implements Task<Map<String, Obje
     public Map<String, Object> execute(TaskContext context, Map<String, Object> payload) throws TaskException {
         Map<String, Object> payloadToReturn = new HashMap<>(payload);
 
+        log.info("CaseID: {} Adding field {} to payload with value {}.", getCaseId(context), JUDGE_COSTS_DECISION, YES_VALUE);
         payloadToReturn.put(JUDGE_COSTS_DECISION, YES_VALUE);
 
         return payloadToReturn;
