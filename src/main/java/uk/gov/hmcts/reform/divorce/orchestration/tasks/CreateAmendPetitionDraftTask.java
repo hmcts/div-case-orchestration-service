5535cc84be8c33297128e4421416679ef2f20d55
TSeelig <thomas.seelig@HMCTS.NET>
2021-04-23 11:54:37 +0100

    add logging

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/CreateAmendPetitionDraftTask.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/CreateAmendPetitionDraftTask.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.divorce.orchestration.tasks;
 
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.orchestration.client.CaseMaintenanceClient;
@@ -18,6 +19,7 @@ import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.Orchestrati
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CreateAmendPetitionDraftTask implements Task<Map<String, Object>> {
 
     private final CaseMaintenanceClient caseMaintenanceClient;
@@ -25,19 +27,25 @@ public class CreateAmendPetitionDraftTask implements Task<Map<String, Object>> {
     @Override
     public Map<String, Object> execute(TaskContext context,
                                        Map<String, Object> draft) {
+        String caseId = context.getTransientObject(CASE_ID_JSON_KEY);
         try {
+            log.info("amendPetition: Calling CMS to amend petition for case ID: {}", caseId);
             final Map<String, Object> amendDraft = caseMaintenanceClient.amendPetition(context.getTransientObject(AUTH_TOKEN_JSON_KEY).toString());
 
             context.setTransientObject(NEW_AMENDED_PETITION_DRAFT_KEY, amendDraft);
         } catch (FeignException exception) {
             if (exception.status() == HttpStatus.NOT_FOUND.value()) {
-                String caseId = context.getTransientObject(CASE_ID_JSON_KEY);
-                throw new TaskException("AmendPetitionDraft: Case not found - please check the case ID: " + caseId, exception);
+                throw new TaskException("amendPetition: Case not found - please check the case ID: " + caseId, exception);
             } else if (exception.status() == HttpStatus.MULTIPLE_CHOICES.value()) {
-                throw new TaskException("AmendPetitionDraft: Multiple cases found", exception);
+                throw new TaskException("amendPetition: Multiple cases found", exception);
             }
-            throw exception;
+             else if (exception.status() == HttpStatus.UNAUTHORIZED.value()) {
+                    throw new TaskException("amendPetition: Unauthorized (invalid user or case) - caseID:" + caseId, exception);
+            } else {
+                throw new TaskException("amendPetition: An error occurred", exception);
+            }
         }
+        log.info("amendPetition: Draft created for old case ID: {}", caseId);
 
         // return empty as next step (update case state AmendPetition) needs no data (empty)
         return new HashMap<>();
