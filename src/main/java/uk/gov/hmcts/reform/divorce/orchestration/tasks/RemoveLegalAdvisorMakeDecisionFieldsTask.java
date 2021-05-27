5619d66dde5c17bd80b47afd0810258ca7fd9fe0
Patryk Jar <patryk.jar@hmcts.net>
2021-05-27 17:40:34 +0200

    RPET-601: more tests for /remove-la-make-decision-fields  (#1440)

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/RemoveLegalAdvisorMakeDecisionFieldsTask.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/RemoveLegalAdvisorMakeDecisionFieldsTask.java
@@ -1,13 +1,11 @@
 package uk.gov.hmcts.reform.divorce.orchestration.tasks;
 
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
-import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
-import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
+import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.generics.FieldsRemovalTask;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
+import static java.util.Arrays.asList;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.COSTS_ORDER_ADDITIONAL_INFO_CCD_FIELD;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DECREE_NISI_GRANTED_CCD_FIELD;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DIVORCE_COSTS_CLAIM_GRANTED_CCD_FIELD;
@@ -15,16 +13,16 @@ import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.Orchestrati
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.WHO_PAYS_COSTS_CCD_FIELD;
 
 @Component
-public class RemoveLegalAdvisorMakeDecisionFieldsTask implements Task<Map<String, Object>> {
+public class RemoveLegalAdvisorMakeDecisionFieldsTask extends FieldsRemovalTask {
 
     @Override
-    public Map<String, Object> execute(TaskContext context, final Map<String, Object> payload) throws TaskException {
-        Map<String, Object> response = new HashMap<>(payload);
-        response.remove(DECREE_NISI_GRANTED_CCD_FIELD);
-        response.remove(DIVORCE_COSTS_CLAIM_GRANTED_CCD_FIELD);
-        response.remove(WHO_PAYS_COSTS_CCD_FIELD);
-        response.remove(TYPE_COSTS_DECISION_CCD_FIELD);
-        response.remove(COSTS_ORDER_ADDITIONAL_INFO_CCD_FIELD);
-        return response;
+    protected List<String> getFieldsToRemove() {
+        return asList(
+            DECREE_NISI_GRANTED_CCD_FIELD,
+            DIVORCE_COSTS_CLAIM_GRANTED_CCD_FIELD,
+            WHO_PAYS_COSTS_CCD_FIELD,
+            TYPE_COSTS_DECISION_CCD_FIELD,
+            COSTS_ORDER_ADDITIONAL_INFO_CCD_FIELD
+        );
     }
 }
