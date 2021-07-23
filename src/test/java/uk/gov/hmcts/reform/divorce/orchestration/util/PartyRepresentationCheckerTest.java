a522fd55c454d8646d76dcf6c2c894dc21ad99c3
Ruban <ruban72@hotmail.com>
2021-07-23 10:33:22 +0100

    DFR-60: Tidy up tests again

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/util/PartyRepresentationCheckerTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/util/PartyRepresentationCheckerTest.java
@@ -193,17 +193,15 @@ public class PartyRepresentationCheckerTest {
     }
 
     @Test
-    public void isRespondentWhenRespondentEmailIsPopulatedAndCoRespondentDigitalReturnsTrue() {
+    public void isRespondentDigitalWhenRespondentEmailIsPopulatedReturnsTrue() {
         Map<String, Object> caseData = createCaseData(RESPONDENT_EMAIL_ADDRESS, "respondent@email.com");
         assertThat(isRespondentDigital(caseData), is(true));
-        assertThat(isCoRespondentDigital(caseData), is(true));
     }
 
     @Test
     public void isRespondentDigitalReturnsFalseWhenRespondentEmailIsNotPopulated() {
         Map<String, Object> caseData = emptyMap();
         assertThat(isRespondentDigital(caseData), is(false));
-        assertThat(isCoRespondentDigital(caseData), is(true));
     }
 
     @Test
