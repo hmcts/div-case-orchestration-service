dfa19a85e784b432b86a34dae9d19564294dbf40
Damon Green <damon.green@btinternet.com>
2023-07-28 11:21:38 +0100

    add cft_vnt

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,6 +14,29 @@ locals {
     prod = "8999dec3-0104-4a27-94ee-6588559729d1"
     nonprod = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
   }
+  cft_vnet = {
+    sbox = {
+      subscription = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
+    }
+    perftest = {
+      subscription = "8a07fdcd-6abd-48b3-ad88-ff737a4b9e3c"
+    }
+    aat = {
+      subscription = "96c274ce-846d-4e48-89a7-d528432298a7"
+    }
+    ithc = {
+      subscription = "62864d44-5da9-4ae9-89e7-0cf33942fa09"
+    }
+    preview = {
+      subscription = "8b6ea922-0862-443e-af15-6056e1c9b9a4"
+    }
+    prod = {
+      subscription = "8cbc6f36-7c56-4963-9d36-739db5d00b27"
+    }
+    demo = {
+      subscription = "d025fece-ce99-4df2-b7a9-b649d3ff2060"
+    }
+  }
 }
 
 module "div-scheduler-db" {
@@ -29,6 +52,31 @@ module "div-scheduler-db" {
   common_tags        = var.common_tags
   subscription       = var.subscription
 }
+
+module "postgresql-14" {
+
+  providers = {
+    azurerm.postgres_network = azurerm.cft_vnet
+  }
+
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  env    = var.env
+
+  product       = var.product
+  component     = var.component
+  business_area = "cft"
+
+  pgsql_databases = [
+    {
+      name : "div_scheduler_14"
+    }
+  ]
+
+  pgsql_version = "14"
+  admin_user_object_id = var.jenkins_AAD_objectId
+  common_tags = var.common_tags
+}
+
 
 data "azurerm_key_vault" "div_key_vault" {
   name                = local.vaultName
