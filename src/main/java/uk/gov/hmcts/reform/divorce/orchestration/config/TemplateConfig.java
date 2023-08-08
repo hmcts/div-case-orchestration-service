936926aab8fc5ba668c11b535d311ed660dff9f7
srochani <sunil.rochani@hotmail.com>
2023-08-08 11:53:33 +0100

    Fixed java 17 Out of memory

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/config/TemplateConfig.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/config/TemplateConfig.java
@@ -8,15 +8,15 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.LanguagePreference;
 
+import java.util.HashMap;
 import java.util.Map;
 import javax.validation.Valid;
 
 @Getter
-@Setter
 @Component
 @Validated
-@ConfigurationProperties(prefix = "uk.gov.notify")
+@ConfigurationProperties()
 public class TemplateConfig {
     @Valid
-    private Map<String, Map<LanguagePreference,  Map<String, String>>> template;
+    private Map<String, Map<LanguagePreference,  Map<String, String>>> template = new HashMap<>();
 }
