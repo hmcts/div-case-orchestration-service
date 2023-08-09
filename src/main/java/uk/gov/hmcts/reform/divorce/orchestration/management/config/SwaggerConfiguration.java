6c95f11a81eee8e7e4e813a10540d9680d12efcc
srochani <sunil.rochani@hotmail.com>
2023-08-09 09:54:26 +0100

    Fixed java 17 Out of memory

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/management/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/management/config/SwaggerConfiguration.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.divorce.orchestration.management.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
 import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
 import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
@@ -17,38 +19,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.util.StringUtils;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import uk.gov.hmcts.reform.divorce.orchestration.OrchestrationServiceApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 @Configuration
-@EnableSwagger2
 @ConditionalOnProperty(value = "documentation.swagger.enabled", havingValue = "true")
-public class SwaggerConfiguration {
+public class SwaggerConfiguration implements WebMvcConfigurer {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-            .select()
-            .apis(RequestHandlerSelectors.basePackage(OrchestrationServiceApplication.class.getPackage().getName()))
-            .build()
-            .useDefaultResponseMessages(true)
-            .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-            .title("Case Orchestration Service")
-            .description("Given a case data this service will validate against the business rules ")
-            .build();
+    public OpenAPI springOpenAPI() {
+        return new OpenAPI()
+            .info(new Info().title("Case Orchestration Service")
+                .description("Given a case data this service will validate against the business rules "));
     }
 
     @Bean
