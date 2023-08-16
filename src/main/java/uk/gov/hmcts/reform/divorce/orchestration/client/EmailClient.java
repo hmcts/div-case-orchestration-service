c5c72c1bda48d18058f56cfd4164c061842a116f
srochani <sunil.rochani@hotmail.com>
2023-08-16 13:46:53 +0100

    Java 17 upgrade fix

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/client/EmailClient.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/client/EmailClient.java
@@ -1,18 +1,15 @@
 package uk.gov.hmcts.reform.divorce.orchestration.client;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.service.notify.NotificationClient;
 
-@Slf4j
 @Component
 public class EmailClient extends NotificationClient {
 
     @Autowired
     public EmailClient(@Value("${uk.gov.notify.api.key}") String apiKey, @Value("${uk.gov.notify.api.baseUrl}") final String baseUrl) {
         super(apiKey, baseUrl);
-        log.info("apikey------------------------- {}", apiKey);
     }
 }
