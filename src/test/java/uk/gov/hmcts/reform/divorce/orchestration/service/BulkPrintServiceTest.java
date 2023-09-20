64ef3f77654e3d7ef3160a1b6b2b33ccc05f64ac
srochani <sunil.rochani@hotmail.com>
2023-09-20 09:49:46 +0100

    Fix sendLetter issues

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/service/BulkPrintServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/service/BulkPrintServiceTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.divorce.model.documentupdate.GeneratedDocumentInfo;
+import uk.gov.hmcts.reform.divorce.orchestration.domain.model.Features;
 import uk.gov.hmcts.reform.sendletter.api.LetterWithPdfsRequest;
 import uk.gov.hmcts.reform.sendletter.api.SendLetterApi;
 import uk.gov.hmcts.reform.sendletter.api.SendLetterResponse;
@@ -52,7 +53,7 @@ public class BulkPrintServiceTest {
     }
 
     @Test
-    public void happyPath() {
+    public void happyPathWhenSendLetterRecipientCheckDisabled() {
         classUnderTest = new BulkPrintService(true, authTokenGenerator, sendLetterApi, featureToggleService);
 
         final String authToken = "auth-token";
@@ -83,6 +84,48 @@ public class BulkPrintServiceTest {
             "caseIdentifier",
             "caseReferenceNumber",
             "recipients");
+
+        final String xeroxType = "DIV001";
+        assertThat(letterRequestCaptor.getValue(), allOf(
+            hasProperty("type", equalTo(xeroxType)),
+            hasProperty("documents", equalTo(asList(stringifiedDocument1, stringifiedDocument2)))));
+
+        assertThat("Should match", letterRequestCaptor.getValue().getAdditionalData().keySet().equals(expectedAdditionalParameters));
+    }
+
+    @Test
+    public void happyPathWhenSendLetterRecipientCheckEnabled() {
+        classUnderTest = new BulkPrintService(true, authTokenGenerator, sendLetterApi, featureToggleService);
+
+        final String authToken = "auth-token";
+        when(authTokenGenerator.generate()).thenReturn(authToken);
+
+
+        final GeneratedDocumentInfo document1 = mock(GeneratedDocumentInfo.class);
+        final String document1Data = "foo";
+        when(document1.getBytes()).thenReturn(document1Data.getBytes());
+
+        final GeneratedDocumentInfo document2 = mock(GeneratedDocumentInfo.class);
+        final String document2Data = "bar";
+        when(document2.getBytes()).thenReturn(document2Data.getBytes());
+
+        when(featureToggleService.isFeatureEnabled(Features.SEND_LETTER_RECIPIENT_CHECK)).thenReturn(true);
+        when(sendLetterApi.sendLetter(eq(authToken), any(LetterWithPdfsRequest.class))).thenReturn(new SendLetterResponse(randomUUID()));
+
+        final String caseId = "case-id";
+        final String letterType = "letter-type";
+        classUnderTest.send(caseId, letterType, asList(document1, document2));
+
+        final String stringifiedDocument1 = getEncoder().encodeToString(document1Data.getBytes());
+        final String stringifiedDocument2 = getEncoder().encodeToString(document2Data.getBytes());
+        final ArgumentCaptor<LetterWithPdfsRequest> letterRequestCaptor = ArgumentCaptor.forClass(LetterWithPdfsRequest.class);
+        verify(sendLetterApi, times(1)).sendLetter(eq(authToken), letterRequestCaptor.capture());
+
+        final ImmutableSet<String> expectedAdditionalParameters = ImmutableSet.of(
+            "letterType",
+            "caseIdentifier",
+            "caseReferenceNumber",
+            "party");
 
         final String xeroxType = "DIV001";
         assertThat(letterRequestCaptor.getValue(), allOf(
