75202390e896abb30a6b3a9800acdb2706bcc776
damongreen123 <damon.green@btinternet.com>
2023-10-24 12:54:24 +0100

    Clean up old PG database
    https://tools.hmcts.net/jira/browse/DTSSE-2451
    As part of postgres update, remove old DB and creds from vault.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -46,20 +46,6 @@ locals {
   }
 }
 
-module "div-scheduler-db" {
-  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product            = "${var.product}-${var.component}"
-  location           = var.location_db
-  env                = var.env
-  database_name      = "div_scheduler"
-  postgresql_user    = "div_scheduler"
-  postgresql_version = "10"
-  sku_name           = "GP_Gen5_2"
-  sku_tier           = "GeneralPurpose"
-  common_tags        = var.common_tags
-  subscription       = var.subscription
-}
-
 module "postgresql-14" {
 
   providers = {
@@ -87,18 +73,6 @@ module "postgresql-14" {
 data "azurerm_key_vault" "div_key_vault" {
   name                = local.vaultName
   resource_group_name = local.vaultName
-}
-
-resource "azurerm_key_vault_secret" "postgresql-user" {
-  name         = "${var.component}-postgresql-user"
-  value        = module.div-scheduler-db.user_name
-  key_vault_id = data.azurerm_key_vault.div_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "postgresql-password" {
-  name         = "${var.component}-postgresql-password"
-  value        = module.div-scheduler-db.postgresql_password
-  key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
 
 data "azurerm_key_vault" "sendgrid" {
