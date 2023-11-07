7c6e8e08c7d3d7edb576b152148a23a8845c1658
damongreen123 <damon.green@btinternet.com>
2023-11-07 11:35:12 +0000

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,6 +15,14 @@ provider "azurerm" {
   alias = "cft_vnet"
 }
 
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
 locals {
   vaultName = "${var.product}-${var.env}"
   sendgrid_subscription = {
@@ -46,20 +54,6 @@ locals {
   }
 }
 
-module "div-scheduler-db" {
-  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product            = "${var.product}-${var.component}"
-  location           = var.location_db
-  env                = var.env
-  database_name      = "div_scheduler"
-  postgresql_user    = "div_scheduler"
-  postgresql_version = "10"
-  sku_name           = "GP_Gen5_2"
-  sku_tier           = "GeneralPurpose"
-  common_tags        = var.common_tags
-  subscription       = var.subscription
-}
-
 module "postgresql-14" {
 
   providers = {
@@ -87,18 +81,6 @@ module "postgresql-14" {
 data "azurerm_key_vault" "div_key_vault" {
   name                = local.vaultName
   resource_group_name = local.vaultName
-}
-
-resource "azurerm_key_vault_secret" "postgresql-user" {
-  name         = "${var.component}-postgresql-user"
-  value        = module.div-scheduler-db.user_name
-  key_vault_id = data.azurerm_key_vault.div_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "postgresql-password" {
-  name         = "${var.component}-postgresql-password"
-  value        = module.div-scheduler-db.postgresql_password
-  key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
 
 data "azurerm_key_vault" "sendgrid" {
