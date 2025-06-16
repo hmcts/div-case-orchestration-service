a72a8d9dd1b06cec64f2ddd12a4fddd1e3c939bd
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2025-06-16 10:56:12 +0100

    Update azurerm provider to version 4.32.0 (#1916)

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.80.0"
+      version = "4.32.0"
     }
     random = {
       source = "hashicorp/random"
